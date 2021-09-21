53d6d84b29f32f835454d627b9aa628fbb8c05f5
Oscar Coniel <oscar@coniel.com>
2021-09-21 19:21:23 +0300

    Create package: storybook-native

---
+++ packages/storybook-native/src/stories.ts
@@ -0,0 +1,6 @@
+import { storiesOf } from '@storybook/react-native';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { StoriesOf } from '@braindrop/test-utils-native';
+import { createStories } from '@braindrop/ui-native/src/createStories';
+
+createStories(storiesOf as StoriesOf);
