07dd018e94d81603bd32e7f0b497e6c1978cfb91
Oscar Coniel <oscar@coniel.com>
2021-09-21 15:00:34 +0300

    Move storybook to storybook-dom package

--- .storybook/main.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  stories: [
-    '../packages/**/*.stories.mdx',
-    '../packages/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-};
