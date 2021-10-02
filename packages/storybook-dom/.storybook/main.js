70d27a14988d4fd8547b57a29c395e174549839d
Oscar Coniel <oscar@coniel.com>
2021-10-02 22:30:23 +0300

    Remove -dom suffix from packages

--- packages/storybook-dom/.storybook/main.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  stories: [
-    '../../ui-dom/**/*.stories.mdx',
-    '../../ui-dom/**/*.stories.@(ts|tsx)',
-  ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-};
---
+++ packages/storybook/.storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stories: ['../../ui/**/*.stories.mdx', '../../ui/**/*.stories.@(ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+};
