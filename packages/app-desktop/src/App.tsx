97d1cfe77cac7d8525a103f337adc6b11a0ed5f8
Oscar Coniel <oscar@coniel.com>
2021-11-03 13:58:03 +0200

    Set up styling in desktop-app

--- packages/app-desktop/src/App.tsx
+++ packages/app-desktop/src/App.tsx
@@ -1,8 +1,43 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { initializeI18n } from '@minddrop/i18n';
+import '@minddrop/theme/reset.css';
+import '@minddrop/theme/light.css';
+import '@minddrop/theme/dark.css';
+import '@minddrop/theme/base.css';
+import '@minddrop/ui/dist/styles.css';
+import { Button } from '@minddrop/ui';
+import { useEffect, useState } from 'react';
+
+initializeI18n();
+
+const App: React.FC = () => {
+  const [theme, setTheme] = useState<'light-theme' | 'dark-theme'>(
+    'light-theme',
+  );
+
+  useEffect(() => {
+    document.getElementsByTagName('body').item(0).setAttribute('class', theme);
+  }, [theme]);
+
+  return (
+    <>
+      <h2>Hello from React!</h2>
+      <Button
+        label="test"
+        onClick={() =>
+          setTheme((theme) =>
+            theme === 'light-theme' ? 'dark-theme' : 'light-theme',
+          )
+        }
+      />
+      <Button label="test" />
+    </>
+  );
+};
 
 function render() {
-  ReactDOM.render(<h2>Hello from React!</h2>, document.getElementById('root'));
+  ReactDOM.render(<App />, document.getElementById('root'));
 }
 
 render();
