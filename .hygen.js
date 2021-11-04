c06326bcf953a0434215b9ab3885ee8eaf2b94be
Oscar Coniel <oscar@coniel.com>
2021-11-04 14:05:32 +0200

    Remove hygen generator template

--- .hygen.js
+++ .hygen.js
@@ -1,7 +1,6 @@
 const toKebabCase =
   require('@minddrop/utils/dist/toKebabCase/toKebabCase').toKebabCase;
 
-console.log(toKebabCase);
 module.exports = {
   helpers: {
     toKebabCase: (string) => toKebabCase(string),
