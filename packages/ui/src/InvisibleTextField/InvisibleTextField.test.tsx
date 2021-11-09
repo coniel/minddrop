f12bf101e0560942d30ae9dc076454889e406661
Oscar Coniel <oscar@coniel.com>
2021-11-09 23:55:28 +0200

    Fix tests

--- packages/ui/src/InvisibleTextField/InvisibleTextField.test.tsx
+++ packages/ui/src/InvisibleTextField/InvisibleTextField.test.tsx
@@ -6,31 +6,47 @@ describe('<InvisibleTextField />', () => {
   afterEach(cleanup);
 
   it('renders the className', () => {
-    render(<InvisibleTextField label="Title" className="my-class" />);
+    render(
+      <InvisibleTextField
+        placeholder="Untitled"
+        label="Title"
+        className="my-class"
+      />,
+    );
 
     expect(screen.getByRole('textbox')).toHaveClass('my-class');
   });
 
   it('renders the accessability label', () => {
-    render(<InvisibleTextField label="Title" />);
+    render(<InvisibleTextField placeholder="Untitled" label="Title" />);
 
     expect(screen.getByRole('textbox')).toHaveAccessibleName('Title');
   });
 
   it('supports sizes', () => {
-    render(<InvisibleTextField label="Title" size="title" />);
+    render(
+      <InvisibleTextField placeholder="Untitled" label="Title" size="title" />,
+    );
 
     expect(screen.getByRole('textbox')).toHaveClass('size-title');
   });
 
   it('supports colors', () => {
-    render(<InvisibleTextField label="Title" color="light" />);
+    render(
+      <InvisibleTextField placeholder="Untitled" label="Title" color="light" />,
+    );
 
     expect(screen.getByRole('textbox')).toHaveClass('color-light');
   });
 
   it('supports weights', () => {
-    render(<InvisibleTextField label="Title" weight="semibold" />);
+    render(
+      <InvisibleTextField
+        placeholder="Untitled"
+        label="Title"
+        weight="semibold"
+      />,
+    );
 
     expect(screen.getByRole('textbox')).toHaveClass('weight-semibold');
   });
