cbd5c09b57f9ad1c59f3f973bfce1fe546df3328
Oscar Coniel <oscar@coniel.com>
2021-11-13 22:55:19 +0200

    Add content colors
    Rename UI color gray to neutral

---
+++ packages/ui/src/types/Content.types.ts
@@ -0,0 +1,12 @@
+export type ContentBackgroundColor =
+  | 'none'
+  | 'blue'
+  | 'cyan'
+  | 'red'
+  | 'pink'
+  | 'violet'
+  | 'green'
+  | 'orange'
+  | 'yellow'
+  | 'brown'
+  | 'gray';
--- packages/ui/src/types/index.ts
+++ packages/ui/src/types/index.ts
@@ -1,2 +1,3 @@
+export * from './Content.types';
 export * from './Text.types';
 export * from './Menu.types';
