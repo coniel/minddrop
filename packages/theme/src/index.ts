e8a368f510af82c17ebd53a2e846b2eb647c0860
Oscar Coniel <oscar@coniel.com>
2021-11-20 04:08:33 +0200

    Fix docs theme import

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,5 +1,2 @@
-import './reset.css';
-import './animations.css';
-import './light.css';
-import './dark.css';
-import './base.css';
+import './reset';
+import './theme';
---
+++ packages/theme/src/theme.ts
@@ -0,0 +1,4 @@
+import './light.css';
+import './dark.css';
+import './animations.css';
+import './base.css';
