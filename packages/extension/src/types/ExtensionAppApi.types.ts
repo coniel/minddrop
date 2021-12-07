1fd8eccc672204ffaed697f04b54ef465fe9f39d
Oscar Coniel <oscar@coniel.com>
2021-12-07 03:01:45 +0200

    Remove types moved to separate packages

--- packages/extension/src/types/ExtensionAppApi.types.ts
+++ packages/extension/src/types/ExtensionAppApi.types.ts
@@ -1,16 +1,4 @@
 import { EventListenerCallback } from '@minddrop/core';
-import {
-  View,
-  ResourceView,
-  LoadTopicsEvent,
-  LoadTopicsEventData,
-  LoadDropsEvent,
-  LoadDropsEventData,
-  InsertDataEvent,
-  InsertDataEventData,
-} from '@minddrop/app';
-import { ExtensionTopicApi } from './ExtensionTopicApi';
-import { ExtensionDropApi } from './ExtensionDropApi';
 
 export interface Api {
   /**
@@ -19,25 +7,4 @@ export interface Api {
    * @param view The view to open.
    */
   openView(view: View | ResourceView): void;
-
-  // App event listeners
-  // Load topics
-  addEventListener(
-    type: LoadTopicsEvent,
-    callback: EventListenerCallback<LoadTopicsEvent, LoadTopicsEventData>,
-  ): void;
-
-  // Load drops
-  addEventListener(
-    type: LoadDropsEvent,
-    callback: EventListenerCallback<LoadDropsEvent, LoadDropsEventData>,
-  ): void;
-
-  // Insert data
-  addEventListener(
-    type: InsertDataEvent,
-    callback: EventListenerCallback<InsertDataEvent, InsertDataEventData>,
-  ): void;
 }
-
-export type ExtensionAppApi = Api & ExtensionTopicApi & ExtensionDropApi;
