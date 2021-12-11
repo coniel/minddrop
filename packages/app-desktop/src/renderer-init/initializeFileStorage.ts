8aaafdf612c5af23dc18d7ac52976d3848570560
Oscar Coniel <oscar@coniel.com>
2021-12-12 01:56:12 +0200

    Rename main/ to main-init/ for consistency

--- packages/app-desktop/src/renderer-init/initializeFileStorage.ts
+++ packages/app-desktop/src/renderer-init/initializeFileStorage.ts
@@ -6,7 +6,6 @@ const core = initializeCore('app');
 export function initializeFileStorage() {
   Files.addEventListener(core, 'files:create', (payload) => {
     const { file, reference } = payload.data;
-    console.log(window.files);
     window.files.create(file, reference.id);
   });
 }
