e1dca242ebd08e6df0673a2bdc32067d9e1dfc8f
Oscar Coniel <oscar@coniel.com>
2021-12-11 22:52:12 +0200

    Clean up app-desktop

--- packages/app-desktop/src/App.tsx
+++ packages/app-desktop/src/App.tsx
@@ -3,26 +3,11 @@ import * as ReactDOM from 'react-dom';
 import './App.css';
 import '@minddrop/theme';
 import { initializeI18n } from '@minddrop/i18n';
-import { IconsProvider } from '@minddrop/icons';
-import { useEffect, useState } from 'react';
-import { AppSidebar } from './components';
 
 initializeI18n();
 
 const App: React.FC = () => {
-  const [theme] = useState<'light-theme' | 'dark-theme'>('light-theme');
-
-  useEffect(() => {
-    document.getElementsByTagName('body').item(0).setAttribute('class', theme);
-  }, [theme]);
-
-  return (
-    <IconsProvider>
-      <div style={{ display: 'flex', height: '100%' }}>
-        <AppSidebar />
-      </div>
-    </IconsProvider>
-  );
+  return <div />;
 };
 
 function render() {
