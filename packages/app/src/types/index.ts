67b18f5437ac1fd98313b8a21f160f75905349e4
Oscar Coniel <oscar@coniel.com>
2021-12-13 13:52:16 +0200

    Fix export statement

--- packages/app/src/types/index.ts
+++ packages/app/src/types/index.ts
@@ -1,6 +1,6 @@
 export * from './AppApi.types';
 export * from './AppEvents.types';
 export * from './AppStore.types';
-export * from './UIComponentConfig.types';
+export * from './UiComponentConfig.types';
 export * from './UiExtension.types';
 export * from './View.types';
