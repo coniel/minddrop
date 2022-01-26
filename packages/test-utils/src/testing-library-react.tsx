0fb8a0c0e7543954f16ca1252427fc8a859d33ab
Oscar Coniel <oscar@coniel.com>
2022-01-27 00:39:35 +0200

    Wrap tests in CoreProvider

--- packages/test-utils/src/testing-library-react.tsx
+++ packages/test-utils/src/testing-library-react.tsx
@@ -2,12 +2,17 @@ import React, { FC, ReactElement } from 'react';
 import { render, RenderOptions } from '@testing-library/react';
 import { initializeI18n } from '@minddrop/i18n';
 import { IconsProvider } from '@minddrop/icons';
+import { CoreProvider } from '@minddrop/core';
 import '@testing-library/jest-dom';
 
 initializeI18n();
 
 const WithProviders: FC = ({ children }) => {
-  return <IconsProvider>{children}</IconsProvider>;
+  return (
+    <IconsProvider>
+      <CoreProvider appId="app">{children}</CoreProvider>
+    </IconsProvider>
+  );
 };
 
 const customRender = (
