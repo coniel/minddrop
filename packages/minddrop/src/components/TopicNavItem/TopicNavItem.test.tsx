4eb199d2c1d89bca6b26cba7e5610f977d160e30
Oscar Coniel <oscar@coniel.com>
2022-01-28 18:13:13 +0200

    Add ability to rename topics from context menu

--- packages/minddrop/src/components/TopicNavItem/TopicNavItem.test.tsx
+++ packages/minddrop/src/components/TopicNavItem/TopicNavItem.test.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { render, cleanup, screen } from '@minddrop/test-utils';
+import {
+  render,
+  cleanup,
+  screen,
+  act,
+  fireEvent,
+  waitFor,
+} from '@minddrop/test-utils';
 import { TopicNavItem } from './TopicNavItem';
 import {
   tSailing,
@@ -7,10 +14,14 @@ import {
   tBoats,
   tAnchoring,
 } from '../../tests/topics.data';
-import '../../tests/initialize-app';
+import { reInitialize } from '../../tests/initialize-app';
+import { i18n } from '@minddrop/i18n';
 
 describe('<TopicNavItem />', () => {
-  afterEach(cleanup);
+  afterEach(() => {
+    cleanup();
+    reInitialize();
+  });
 
   it('renders the title', () => {
     render(<TopicNavItem id={tSailing.id} />);
@@ -18,11 +29,50 @@ describe('<TopicNavItem />', () => {
     screen.getByText(tSailing.title);
   });
 
-  it('renders subtopics', () => {
-    render(<TopicNavItem defaultExpanded id={tSailing.id} />);
+  it('renders subtopics when expanded', () => {
+    render(<TopicNavItem id={tSailing.id} />);
+
+    act(() => {
+      // Expand topic by clicking on toggle button
+      const label = i18n.t('expandSubtopics');
+      fireEvent.click(screen.getByLabelText(label));
+    });
 
     screen.getByText(tNavigation.title);
     screen.getByText(tBoats.title);
     screen.getByText(tAnchoring.title);
+  });
+
+  it('expands subtopics when adding a subtopic', async () => {
+    render(<TopicNavItem id={tSailing.id} />);
+
+    // Open context menu
+    fireEvent.contextMenu(screen.getByText(tSailing.title));
+
+    // Wait for menu to open
+    const addSubtopic = i18n.t('addSubtopic');
+    await waitFor(() => screen.getAllByText(addSubtopic));
+    // Click 'Add subtopic'
+    act(() => {
+      fireEvent.click(screen.getByText(addSubtopic));
+    });
+
+    screen.getByText(tNavigation.title);
+  });
+
+  it('opens the rename popover when clicking on Rename menu option', async () => {
+    render(<TopicNavItem id={tSailing.id} />);
+
+    // Open context menu
+    fireEvent.contextMenu(screen.getByText(tSailing.title));
+
+    // Click "Rename" menu item
+    const rename = i18n.t('rename');
+    await waitFor(() => screen.getAllByText(rename));
+    fireEvent.click(screen.getByText(rename));
+
+    // Check for rename popover input
+    const input = i18n.t('topicName');
+    screen.getByLabelText(input);
   });
 });
--- packages/minddrop/src/components/TopicNavItem/TopicNavItem.tsx
+++ packages/minddrop/src/components/TopicNavItem/TopicNavItem.tsx
@@ -1,14 +1,22 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import {
   TopicNavItem as TopicNavItemPrimitive,
   TopicNavItemProps as TopicNavItemPrimitiveProps,
   ContextMenu,
   ContextMenuTrigger,
   ContextMenuContent,
+  Popover,
+  PopoverAnchor,
 } from '@minddrop/ui';
 import { useTopic } from '@minddrop/topics';
 import { generateTopicMenu } from '../../menus';
 import { useAppCore } from '../../utils';
+import {
+  PersistentStore,
+  useLocalPersistentStoreValue,
+} from '@minddrop/persistent-store';
+import { FieldValue } from '@minddrop/utils';
+import { RenameTopicPopover } from '../RenameTopicPopover';
 
 export interface TopicNavItemProps
   extends Omit<TopicNavItemPrimitiveProps, 'label'> {
@@ -21,20 +29,61 @@ export interface TopicNavItemProps
 export const TopicNavItem: FC<TopicNavItemProps> = ({ id, ...other }) => {
   const core = useAppCore();
   const topic = useTopic(id);
+  const expandedTopics = useLocalPersistentStoreValue<string[]>(
+    core,
+    'expandedTopics',
+  );
+  const [renamePopoverOpen, setRenamePopoverOpen] = useState(false);
+
+  const handleExpandedChange = useCallback(
+    (expanded) => {
+      PersistentStore.setLocalValue(
+        core,
+        'expandedTopics',
+        expanded ? FieldValue.arrayUnion(id) : FieldValue.arrayRemove(id),
+      );
+    },
+    [id],
+  );
+
+  function expand() {
+    handleExpandedChange(true);
+  }
+
+  function openRenamePopover() {
+    setRenamePopoverOpen(true);
+  }
+
+  function closeRenamePopover() {
+    setRenamePopoverOpen(false);
+  }
 
   return (
-    <ContextMenu>
-      <ContextMenuTrigger asChild>
-        <TopicNavItemPrimitive label={topic.title} {...other}>
-          {topic.subtopics.map((subtopicId) => (
-            <TopicNavItem key={subtopicId} id={subtopicId} />
-          ))}
-        </TopicNavItemPrimitive>
-      </ContextMenuTrigger>
-      <ContextMenuContent
-        className="topic-menu-content"
-        content={generateTopicMenu(core, topic)}
-      />
-    </ContextMenu>
+    <Popover open={renamePopoverOpen} onOpenChange={null}>
+      <ContextMenu>
+        <ContextMenuTrigger asChild>
+          <PopoverAnchor asChild>
+            <TopicNavItemPrimitive
+              label={topic.title}
+              expanded={expandedTopics.includes(id)}
+              onExpandedChange={handleExpandedChange}
+              {...other}
+            >
+              {topic.subtopics.map((subtopicId) => (
+                <TopicNavItem key={subtopicId} id={subtopicId} />
+              ))}
+            </TopicNavItemPrimitive>
+          </PopoverAnchor>
+        </ContextMenuTrigger>
+        <ContextMenuContent
+          className="topic-menu-content"
+          content={generateTopicMenu(core, topic, {
+            onRename: openRenamePopover,
+            onAddSubtopic: expand,
+          })}
+        />
+      </ContextMenu>
+      <RenameTopicPopover topic={topic} onClose={closeRenamePopover} />
+    </Popover>
   );
 };
