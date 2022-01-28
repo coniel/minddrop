6687ed6043072af96c6e500df13dfbc1ad4fea00
Oscar Coniel <oscar@coniel.com>
2022-01-28 18:10:14 +0200

    Add window.DOMRect to tests initialisation

--- packages/test-utils/src/testing-library-react.tsx
+++ packages/test-utils/src/testing-library-react.tsx
@@ -1,9 +1,23 @@
+/* eslint-disable no-empty-function */
+/* eslint-disable class-methods-use-this */
 import React, { FC, ReactElement } from 'react';
 import { render, RenderOptions } from '@testing-library/react';
 import { initializeI18n } from '@minddrop/i18n';
 import { IconsProvider } from '@minddrop/icons';
 import { CoreProvider } from '@minddrop/core';
 import '@testing-library/jest-dom';
+
+class ResizeObserver {
+  observe() {}
+
+  unobserve() {}
+}
+
+// Needed for popovers and tooltips
+// @ts-ignore
+window.DOMRect = { fromRect: () => ({}) };
+// @ts-ignore
+window.ResizeObserver = ResizeObserver;
 
 initializeI18n();
 
