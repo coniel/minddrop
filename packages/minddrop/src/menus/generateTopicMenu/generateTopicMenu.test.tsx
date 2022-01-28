5acde3a4fad5cdef07dc7ce17b8b088ab675e7cd
Oscar Coniel <oscar@coniel.com>
2022-01-28 18:12:28 +0200

    Add callbacks to topic menus options

--- packages/minddrop/src/menus/generateTopicMenu/generateTopicMenu.test.tsx
+++ packages/minddrop/src/menus/generateTopicMenu/generateTopicMenu.test.tsx
@@ -5,7 +5,7 @@ import { i18n } from '@minddrop/i18n';
 import { core } from '../../tests/initialize-app';
 import { DropdownMenuContent, DropdownMenu } from '@minddrop/ui';
 import { Topic, Topics } from '@minddrop/topics';
-import { generateTopicMenu } from './generateTopicMenu';
+import { generateTopicMenu, TopicMenuOptions } from './generateTopicMenu';
 
 class ResizeObserver {
   observe() {}
@@ -13,20 +13,31 @@ class ResizeObserver {
   unobserve() {}
 }
 
-describe('<TopicNavItem />', () => {
+const options: TopicMenuOptions = {
+  onAddSubtopic: jest.fn(),
+  onDelete: jest.fn(),
+  onRename: jest.fn(),
+};
+
+describe('generateTopicMenu', () => {
   let Menu;
   let topic: Topic;
 
   afterEach(() => {
     cleanup();
     Topics.clear(core);
+    Object.values(options).forEach((mock) => {
+      mock.mockClear();
+    });
   });
 
   beforeEach(() => {
     topic = Topics.create(core);
     Menu = () => (
       <DropdownMenu defaultOpen>
-        <DropdownMenuContent content={generateTopicMenu(core, topic)} />
+        <DropdownMenuContent
+          content={generateTopicMenu(core, topic, options)}
+        />
       </DropdownMenu>
     );
   });
@@ -47,8 +58,13 @@ describe('<TopicNavItem />', () => {
     });
 
     const updatedTopic = Topics.get(topic.id);
+    const subtopic = Topics.get(updatedTopic.subtopics[0]);
+    // Adds subtopic to topic
     expect(updatedTopic.subtopics.length).toBe(1);
-    expect(Topics.get(updatedTopic.subtopics[0])).toBeDefined();
+    // Creates the subtopic
+    expect(subtopic).toBeDefined();
+    // Calls onAddSubtopic callback
+    expect(options.onAddSubtopic).toHaveBeenCalledWith(updatedTopic, subtopic);
   });
 
   it('allows deleting topics', () => {
@@ -59,6 +75,22 @@ describe('<TopicNavItem />', () => {
       fireEvent.click(screen.getByText(label));
     });
 
-    expect(Topics.get(topic.id).deleted).toBe(true);
+    const deletedTopic = Topics.get(topic.id);
+
+    // Deletes to the topic
+    expect(deletedTopic.deleted).toBe(true);
+    // Calls the onDelete callback
+    expect(options.onDelete).toHaveBeenCalledWith(deletedTopic);
+  });
+
+  it('calls options.onRename when clicking rename option', () => {
+    render(<Menu />);
+    const label = i18n.t('rename');
+
+    act(() => {
+      fireEvent.click(screen.getByText(label));
+    });
+
+    expect(options.onRename).toHaveBeenCalledWith(topic);
   });
 });
--- packages/minddrop/src/menus/generateTopicMenu/generateTopicMenu.tsx
+++ packages/minddrop/src/menus/generateTopicMenu/generateTopicMenu.tsx
@@ -4,28 +4,89 @@ import { Topic, Topics } from '@minddrop/topics';
 import { MenuContents } from '@minddrop/ui';
 import { Core } from '@minddrop/core';
 import { i18n } from '@minddrop/i18n';
+import { PersistentStore } from '@minddrop/persistent-store';
+import { FieldValue } from '@minddrop/utils';
 
-function addSubtopic(core: Core, topic: Topic) {
+export interface TopicMenuOptions {
+  /**
+   * Callback fired after a subtopic is added using the
+   * 'Add subtopic' menu item.
+   *
+   * @param topic The topic to which the new subtopic was added.
+   * @param subtopic The newly created subtopic.
+   */
+  onAddSubtopic?(topic: Topic, subtopic: Topic): void;
+
+  /**
+   * Callback fired when the topic is deleted using the
+   * 'Delete' option.
+   *
+   * @param topic The deleted topic.
+   */
+  onDelete?(topic: Topic): void;
+
+  /**
+   * Callback fired when the 'Rename' menu item is clicked.
+   *
+   * @param topic The topic to rename.
+   */
+  onRename(topic: Topic): void;
+}
+
+function handleAddSubtopic(
+  core: Core,
+  topic: Topic,
+  callback?: TopicMenuOptions['onAddSubtopic'],
+) {
   // Create the subtopic
   const subtopic = Topics.create(core);
 
   // Add the new subtopic to the topic
-  Topics.addSubtopics(core, topic.id, [subtopic.id]);
+  const updatedTopic = Topics.addSubtopics(core, topic.id, [subtopic.id]);
+
+  // Call the onAddSubtopic callback
+  if (callback) {
+    callback(updatedTopic, subtopic);
+  }
 }
 
-export function generateTopicMenu(core: Core, topic: Topic): MenuContents {
+function handleDelete(
+  core: Core,
+  topic: Topic,
+  callback?: TopicMenuOptions['onDelete'],
+) {
+  // Delete the topic
+  const deletedTopic = Topics.delete(core, topic.id);
+
+  // Call the onDelete callback
+  if (callback) {
+    callback(deletedTopic);
+  }
+}
+
+export function generateTopicMenu(
+  core: Core,
+  topic: Topic,
+  options: TopicMenuOptions,
+): MenuContents {
   return [
     {
       type: 'menu-item',
       label: 'Add subtopic',
-      onSelect: () => addSubtopic(core, topic),
+      onSelect: () => handleAddSubtopic(core, topic, options.onAddSubtopic),
       icon: 'inside',
     },
     {
       type: 'menu-item',
       label: i18n.t('delete'),
-      onSelect: () => Topics.delete(core, topic.id),
+      onSelect: () => handleDelete(core, topic, options.onDelete),
       icon: 'trash',
+    },
+    {
+      type: 'menu-item',
+      label: i18n.t('rename'),
+      onSelect: () => options.onRename(topic),
+      icon: 'edit',
     },
   ];
 }
