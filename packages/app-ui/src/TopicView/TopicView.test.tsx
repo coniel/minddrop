63346086eba01060bc73a50d35bcc764fa84c2b6
Oscar Coniel <oscar@coniel.com>
2022-02-23 04:39:23 +0200

    Add TopicView keyboard shortcut handlers

--- packages/app-ui/src/TopicView/TopicView.test.tsx
+++ packages/app-ui/src/TopicView/TopicView.test.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { render, cleanup as cleanupRender } from '@minddrop/test-utils';
-import { setup as setupApp, cleanup } from '../test-utils';
+import {
+  render,
+  cleanup as cleanupRender,
+  act,
+  fireEvent,
+} from '@minddrop/test-utils';
+import { setup as setupApp, cleanup, core } from '../test-utils';
 import { TopicView } from './TopicView';
-import { TOPICS_TEST_DATA } from '@minddrop/topics';
+import { Topics, TOPICS_TEST_DATA } from '@minddrop/topics';
+import { App } from '@minddrop/app';
+import { Drops, DROPS_TEST_DATA } from '@minddrop/drops';
+import {
+  createDataInsertFromDataTransfer,
+  setDataTransferData,
+} from '@minddrop/utils';
 
-const { tCoastalNavigationView } = TOPICS_TEST_DATA;
+const { tSixDropsView } = TOPICS_TEST_DATA;
+const { textDrop1 } = DROPS_TEST_DATA;
 
 describe('<TopicView />', () => {
   beforeEach(() => {
@@ -18,7 +30,7 @@ describe('<TopicView />', () => {
 
   const setup = () => {
     const utils = render(
-      <TopicView {...tCoastalNavigationView}>
+      <TopicView {...tSixDropsView}>
         <div>children</div>
       </TopicView>,
     );
@@ -30,5 +42,226 @@ describe('<TopicView />', () => {
     const { getByText } = setup();
 
     getByText('children');
+  });
+
+  it('deletes selected drops on Delete keydown', () => {
+    setup();
+
+    act(() => {
+      // Select a drop
+      App.selectDrops(core, [textDrop1.id]);
+    });
+
+    act(() => {
+      // Fire Delete keydown
+      fireEvent.keyDown(document, { key: 'Delete' });
+    });
+
+    // Get the drop
+    const drop = Drops.get(textDrop1.id);
+    // Should be deleted
+    expect(drop.deleted).toBe(true);
+  });
+
+  it('deletes selected drops on Backspace keydown', () => {
+    setup();
+
+    act(() => {
+      // Select a drop
+      App.selectDrops(core, [textDrop1.id]);
+    });
+
+    act(() => {
+      // Fire Delete keydown
+      fireEvent.keyDown(document, { key: 'Backspace' });
+    });
+
+    // Get the drop
+    const drop = Drops.get(textDrop1.id);
+    // Should be deleted
+    expect(drop.deleted).toBe(true);
+  });
+
+  it('archives selected drops on Shift+Delete keydown', () => {
+    setup();
+
+    act(() => {
+      // Select a drop
+      App.selectDrops(core, [textDrop1.id]);
+    });
+
+    act(() => {
+      // Fire Shift+Delete keydown
+      fireEvent.keyDown(document, { key: 'Delete', shiftKey: true });
+    });
+
+    // Get the drop
+    const drop = Drops.get(textDrop1.id);
+    // Should be archived
+    expect(drop.archived).toBe(true);
+    // Should not be deleted
+    expect(drop.deleted).toBeFalsy();
+  });
+
+  it('archives selected drops on Shift+Backspace keydown', () => {
+    setup();
+
+    act(() => {
+      // Select a drop
+      App.selectDrops(core, [textDrop1.id]);
+    });
+
+    act(() => {
+      // Fire Shift+Delete keydown
+      fireEvent.keyDown(document, { key: 'Backspace', shiftKey: true });
+    });
+
+    // Get the drop
+    const drop = Drops.get(textDrop1.id);
+    // Should be archived
+    expect(drop.archived).toBe(true);
+    // Should not be deleted
+    expect(drop.deleted).toBeFalsy();
+  });
+
+  it('adds selected drops and action to clipboardData on copy', () => {
+    setup();
+
+    const clipboardData = {
+      types: [],
+      data: {},
+      getData: (key) => clipboardData.data[key],
+    };
+    const event = {
+      clipboardData: {
+        clearData: jest.fn(),
+        setData: (key, value) => {
+          clipboardData.data[key] = value;
+          clipboardData.types.push(key);
+        },
+      },
+    };
+
+    act(() => {
+      // Select a drop
+      App.selectDrops(core, [textDrop1.id]);
+    });
+
+    act(() => {
+      // Fire copy event
+      fireEvent.copy(document, event);
+    });
+
+    const dataInsert = createDataInsertFromDataTransfer(
+      clipboardData as unknown as DataTransfer,
+    );
+
+    // Should set action
+    expect(dataInsert.action).toEqual('copy');
+    // Should set drops
+    expect(dataInsert.drops).toEqual([textDrop1.id]);
+  });
+
+  it('adds selected drops and action to clipboardData and removes drops from topic on cut', () => {
+    setup();
+
+    const event = {
+      clipboardData: {
+        types: [],
+        data: {},
+        getData: (key) => event.clipboardData.data[key],
+        clearData: jest.fn(),
+        setData: (key, value) => {
+          event.clipboardData.data[key] = value;
+          event.clipboardData.types.push(key);
+        },
+      },
+    };
+
+    act(() => {
+      // Select a drop
+      App.selectDrops(core, [textDrop1.id]);
+    });
+
+    act(() => {
+      // Fire cut event
+      fireEvent.cut(document, event);
+    });
+
+    const dataInsert = createDataInsertFromDataTransfer(
+      event.clipboardData as unknown as DataTransfer,
+    );
+
+    // Should set action
+    expect(dataInsert.action).toEqual('cut');
+    // Should set drops
+    expect(dataInsert.drops).toEqual([textDrop1.id]);
+    // Should remove drops from topic
+    const topic = Topics.get(tSixDropsView.topic);
+    expect(topic.drops.includes(textDrop1.id)).toBe(false);
+  });
+
+  it('inserts data on paste', () => {
+    jest.spyOn(App, 'insertDataIntoTopic');
+    setup();
+
+    const clipboardData = {
+      types: [],
+      data: {},
+      getData: (key) => clipboardData.data[key],
+    };
+    const event = {
+      target: {
+        tagName: 'BODY',
+      },
+      clipboardData: {
+        types: [],
+        data: {},
+        getData: (key) => event.clipboardData.data[key],
+        clearData: jest.fn(),
+        setData: (key, value) => {
+          event.clipboardData.data[key] = value;
+          event.clipboardData.types.push(key);
+        },
+      },
+    };
+
+    act(() => {
+      // Add the new drop to the clipboard event data
+      setDataTransferData(event as unknown as ClipboardEvent, {
+        action: 'copy',
+        drops: [textDrop1.id],
+      });
+      // Fire a paste event using the clipboard event
+      fireEvent.paste(document, event);
+    });
+
+    // Should insert data into topic
+    expect(App.insertDataIntoTopic).toHaveBeenCalled();
+    // @ts-ignore
+    const params = App.insertDataIntoTopic.mock.calls[0];
+    expect(params[1]).toBe(tSixDropsView.topic);
+    expect(params[2].drops).toEqual([textDrop1.id]);
+  });
+
+  it('duplicates selected drops on metaKey+D keypress', () => {
+    setup();
+
+    act(() => {
+      // Select a drop
+      App.selectDrops(core, [textDrop1.id]);
+    });
+
+    act(() => {
+      // Fire metaKey+D keydown
+      fireEvent.keyDown(document, { key: 'D', metaKey: true });
+    });
+
+    // Should have duplicated the drop
+    const topic = Topics.get(tSixDropsView.topic);
+    expect(topic.drops.length).toEqual(7);
+    const dropId = topic.drops[6];
+    const drop = Drops.get(dropId);
+    expect(drop.duplicatedFrom).toBe(textDrop1.id);
   });
 });
--- packages/app-ui/src/TopicView/TopicView.tsx
+++ packages/app-ui/src/TopicView/TopicView.tsx
@@ -1,13 +1,18 @@
-import React, { FC, useRef } from 'react';
-import { useTopic } from '@minddrop/topics';
+import React, { FC, useEffect, useRef } from 'react';
+import { Topics, useTopic } from '@minddrop/topics';
 import { InstanceViewProps } from '@minddrop/views';
 import { IconButton, Toolbar } from '@minddrop/ui';
-import { useAppCore } from '@minddrop/app';
+import { App, useAppCore } from '@minddrop/app';
 import { TopicTitle } from '../TopicTitle';
 import { useLocalPersistentStoreValue } from '@minddrop/persistent-store';
 import { TopicBreadcrumbs } from '../TopicBreadcrumbs';
 import './TopicView.css';
 import { TopicViewOptionsMenu } from '../TopicViewOptionsMenu';
+import { Drops } from '@minddrop/drops';
+import {
+  createDataInsertFromDataTransfer,
+  setDataTransferData,
+} from '@minddrop/utils';
 
 export interface TopicViewBaseProps {
   topic: string;
@@ -20,6 +25,97 @@ export const TopicView: FC<TopicViewProps> = ({ topic: topicId, children }) => {
   const core = useAppCore();
   const topic = useTopic(topicId);
   const trail = useLocalPersistentStoreValue(core, 'topicTrail', [topicId]);
+
+  useEffect(() => {
+    const keydownCallback = (event: KeyboardEvent) => {
+      // Get selected drops
+      const selectedDrops = App.getSelectedDrops();
+
+      if (event.key === 'Delete' || event.key === 'Backspace') {
+        if (event.shiftKey) {
+          // Archive selected drops is Shift key was pressed
+          Object.keys(selectedDrops).forEach((dropId) => {
+            Drops.archive(core, dropId);
+          });
+        } else {
+          // Delete selected drops
+          Object.keys(selectedDrops).forEach((dropId) => {
+            Drops.delete(core, dropId);
+          });
+        }
+      } else if (event.key === 'd' && event.metaKey) {
+        event.preventDefault();
+
+        // Duplicate the drops
+        const drops = Drops.duplicate(core, Object.keys(selectedDrops));
+
+        // Add duplicate drops to the topic
+        Topics.addDrops(core, topicId, Object.keys(drops));
+      }
+    };
+
+    const copyCallback = (event: ClipboardEvent) => {
+      // Get selected drops
+      const selectedDrops = App.getSelectedDrops();
+
+      if (Object.keys(selectedDrops).length) {
+        // Clear default clipboard data
+        event.clipboardData.clearData();
+
+        // Set custom data
+        setDataTransferData(event, {
+          drops: selectedDrops,
+          action: 'copy',
+        });
+
+        // Needed when setting custom data
+        event.preventDefault();
+      }
+    };
+
+    const cutCallback = (event: ClipboardEvent) => {
+      // Get selected drops
+      const selectedDrops = App.getSelectedDrops();
+
+      if (Object.keys(selectedDrops).length) {
+        // Clear default clipboard data
+        event.clipboardData.clearData();
+
+        // Set custom data
+        setDataTransferData(event, {
+          drops: selectedDrops,
+          action: 'cut',
+        });
+
+        // Needed when setting custom data
+        event.preventDefault();
+
+        // Remove selected drops from the topic
+        Topics.removeDrops(core, topicId, Object.keys(selectedDrops));
+      }
+    };
+
+    const pasteCallback = (event: ClipboardEvent) => {
+      const target = event.target as HTMLElement;
+      if (target.tagName === 'INPUT' || target.tagName === 'SPAN') {
+        return;
+      }
+      const dataInsert = createDataInsertFromDataTransfer(event.clipboardData);
+      App.insertDataIntoTopic(core, topicId, dataInsert);
+    };
+
+    document.addEventListener('keydown', keydownCallback);
+    document.addEventListener('copy', copyCallback);
+    document.addEventListener('cut', cutCallback);
+    document.addEventListener('paste', pasteCallback);
+
+    return () => {
+      document.removeEventListener('keydown', keydownCallback);
+      document.removeEventListener('copy', copyCallback);
+      document.removeEventListener('cut', cutCallback);
+      document.removeEventListener('paste', pasteCallback);
+    };
+  }, [core, topicId]);
 
   return (
     <div className="topic-view">
