c210cf0f74ce6db133343259d2f12a704540b2e3
Oscar Coniel <oscar@coniel.com>
2022-02-26 19:30:12 +0200

    Support basic objects in isEqual utility function

--- packages/utils/src/isEqual/isEqual.test.ts
+++ packages/utils/src/isEqual/isEqual.test.ts
@@ -3,6 +3,12 @@ import { isEqual } from './isEqual';
 describe('isEqual', () => {
   it('returns true if arrays contain the same elements', () => {
     expect(isEqual([1, 2, 3], [3, 1, 2])).toBe(true);
+    expect(
+      isEqual(
+        [{ string: 'a', number: 1, null: null, boolean: true }],
+        [{ string: 'a', number: 1, null: null, boolean: true }],
+      ),
+    ).toBe(true);
   });
 
   it('returns false if the arrays do not contain the same elements', () => {
@@ -11,5 +17,29 @@ describe('isEqual', () => {
     expect(isEqual([1, 2, 3, 4], [3, 1, 2])).toBe(false);
     expect(isEqual([1, 2, 3], [3, 4, 1, 2])).toBe(false);
     expect(isEqual([1, 2, 3], [1, 2, 4])).toBe(false);
+    expect(
+      isEqual(
+        [{ string: 'a', number: 1, null: null, boolean: true }],
+        [{ string: 'b', number: 1, null: null, boolean: true }],
+      ),
+    ).toBe(false);
+    expect(
+      isEqual(
+        [{ string: 'a', number: 1, null: null, boolean: true }],
+        [{ string: 'a', number: 2, null: null, boolean: true }],
+      ),
+    ).toBe(false);
+    expect(
+      isEqual(
+        [{ string: 'a', number: 1, null: null, boolean: true }],
+        [{ string: 'a', number: 1, null: null, boolean: false }],
+      ),
+    ).toBe(false);
+    expect(
+      isEqual(
+        [{ string: 'a', number: 1, null: null, boolean: true }],
+        [{ string: 'a', number: 1, null: 'null', boolean: true }],
+      ),
+    ).toBe(false);
   });
 });
--- packages/utils/src/isEqual/isEqual.ts
+++ packages/utils/src/isEqual/isEqual.ts
@@ -1,15 +1,29 @@
+import { contains } from '../contains';
+
+type ComparisonItem =
+  | string
+  | number
+  | null
+  | boolean
+  | Record<any, string | number | null | boolean>;
+
 /**
- * Checks whether two arrays contain the same elements,
- * ignoring the order of the elements.
+ * Checks whether two arrays contain the same content,
+ * ignoring the order of the elements. Supports items
+ * of type: string, number, null, boolean, and objects
+ * containing the above types.
  *
  * @param array1 The array to check.
  * @param array2 The array to check against.
  * @returns Whether or not the arrays are equal.
  */
-export function isEqual(array1, array2): boolean {
+export function isEqual(
+  array1: ComparisonItem[],
+  array2: ComparisonItem[],
+): boolean {
   if (array1.length === array2.length) {
     return array1.every((element) => {
-      if (array2.includes(element)) {
+      if (contains(array2, [element])) {
         return true;
       }
 
