dcf6464dda58d0affe8588d5ae7e83237a93bc1f
Oscar Coniel <oscar@coniel.com>
2022-03-11 22:21:13 +0200

    Add ability to exclude topics in generateTopicSelectionMenu

--- packages/app-ui/src/utils/generateTopicSelectionMenu/generateTopicSelectionMenu.test.ts
+++ packages/app-ui/src/utils/generateTopicSelectionMenu/generateTopicSelectionMenu.test.ts
@@ -2,7 +2,7 @@ import { Topics, TOPICS_TEST_DATA } from '@minddrop/topics';
 import { setup, cleanup } from '../../test-utils';
 import { generateTopicSelectionMenu } from './generateTopicSelectionMenu';
 
-const { tSailing } = TOPICS_TEST_DATA;
+const { tSailing, tNavigation, tUntitled } = TOPICS_TEST_DATA;
 
 describe('generateTopicSelectionMenu', () => {
   beforeAll(setup);
@@ -33,5 +33,26 @@ describe('generateTopicSelectionMenu', () => {
     expect(onSelect).toHaveBeenCalled();
     expect(onSelect.mock.calls[0][1]).toBe(nested2.id);
     onSelect.mockClear();
+  });
+
+  it('ommits exluded topics', () => {
+    const onSelect = jest.fn();
+    const result = generateTopicSelectionMenu(
+      [tSailing.id, tUntitled.id],
+      onSelect,
+      [tNavigation.id, tUntitled.id],
+    );
+
+    // Should only contain 'Sailing' topic at the root level
+    expect(result.length).toBe(1);
+    expect(result[0].label).toBe(tSailing.title);
+
+    // Should ommit 'Navigation' subtopic
+    expect(result[0].subtopics.length).toBe(tSailing.subtopics.length - 1);
+    expect(
+      result[0].subtopics.find(
+        (subtopic) => subtopic.label === tNavigation.title,
+      ),
+    ).not.toBeDefined();
   });
 });
