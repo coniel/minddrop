3f607696e8c108c8c879fefa8ab2b00034132c95
Oscar Coniel <oscar@coniel.com>
2022-03-21 04:16:46 +0200

    Export extensions extension

--- packages/extensions/src/index.ts
+++ packages/extensions/src/index.ts
@@ -1,4 +1,5 @@
 export * from './errors';
 export * from './types';
 export * from './Extensions';
+export * from './extensions-extension';
 export * as EXTENSIONS_TEST_DATA from './test-utils/extensions.data';
