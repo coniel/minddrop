93f930e9b89d0f40ac25265fef21302979147d73
Oscar Coniel <oscar@coniel.com>
2022-03-27 00:36:35 +0200

    Add core ParentReference type

---
+++ packages/core/src/types/ParentReference.types.ts
@@ -0,0 +1,11 @@
+export interface ParentReference {
+  /**
+   * The type of the parent.
+   */
+  type: string;
+
+  /**
+   * The ID of the parent.
+   */
+  id: string;
+}
--- packages/core/src/types/index.ts
+++ packages/core/src/types/index.ts
@@ -2,5 +2,6 @@ export * from './Color.types';
 export * from './Core.types';
 export * from './DataInsert.types';
 export * from './EventListener.types';
+export * from './ParentReference.types';
 export * from './ResourceChange.types';
 export * from './ResourceConnector.types';
