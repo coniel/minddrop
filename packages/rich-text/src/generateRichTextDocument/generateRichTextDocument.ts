f5ea3e369a3c0611f3258a40626b88b9c7afb0fc
Oscar Coniel <oscar@coniel.com>
2022-03-27 00:50:31 +0200

    Update generateRichTextDocument

--- packages/rich-text/src/generateRichTextDocument/generateRichTextDocument.ts
+++ packages/rich-text/src/generateRichTextDocument/generateRichTextDocument.ts
@@ -1,18 +1,22 @@
 import { generateId } from '@minddrop/utils';
-import { RichTextContent, RichTextDocument } from '../types';
+import { RichTextDocument } from '../types';
 
 /**
  * Generates a new rich text document with the
  * given content.
  *
- * @param content The content of the rich text document.
+ * @param children The content of the rich text document.
  * @returns A new rich text document.
  */
 export function generateRichTextDocument(
-  content: RichTextContent,
+  children: string[] = [],
 ): RichTextDocument {
   return {
+    id: generateId(),
     revision: generateId(),
-    content,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    parents: [],
+    children,
   };
 }
