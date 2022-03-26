9ec89be570577ccbcd0b7eacbd6b26aecd6a5863
Oscar Coniel <oscar@coniel.com>
2022-03-27 01:25:48 +0200

    Add mock database to stories

--- packages/minddrop/src/MindDrop/MindDrop.stories.tsx
+++ packages/minddrop/src/MindDrop/MindDrop.stories.tsx
@@ -1,3 +1,8 @@
+import {
+  DBResourceDocument,
+  initializePouchDB,
+  ResourceDB,
+} from '@minddrop/pouchdb';
 import React from 'react';
 import { MindDrop } from './MindDrop';
 
@@ -6,12 +11,30 @@ export default {
   component: MindDrop,
 };
 
+const database: Record<string, DBResourceDocument> = {};
+
+const dbApi: ResourceDB = {
+  // @ts-ignore
+  allDocs: async () => ({
+    rows: Object.values(database).map((doc) => ({ doc })),
+  }),
+  // @ts-ignore
+  put: async (doc: DBResourceDocument) => {
+    // eslint-disable-next-line no-underscore-dangle
+    database[doc._id] = doc;
+  },
+  // @ts-ignore
+  get: async (id: string) => database[id],
+};
+
+const api = initializePouchDB(dbApi);
+
 export const App: React.FC = () => {
   return (
     <div
       style={{ height: '100%', width: '100%', marginTop: -16, marginLeft: -16 }}
     >
-      <MindDrop appId="app" />
+      <MindDrop appId="app" extensions={[]} dbApi={api} />
     </div>
   );
 };
