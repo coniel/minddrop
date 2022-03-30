c58ae8770228a0923a985f7b89554cb5df652ff5
Oscar Coniel <oscar@coniel.com>
2022-03-31 01:32:35 +0300

    Add additional test files references

--- packages/files/src/test-utils/files.data.ts
+++ packages/files/src/test-utils/files.data.ts
@@ -1,9 +1,28 @@
+import { generateId } from '@minddrop/utils';
 import { FileReference } from '../types';
 
 export const imageFileRef: FileReference = {
-  id: 'image-file-ref',
+  id: generateId(),
   name: 'image.png',
   type: 'image/png',
   size: 2405,
   attachedTo: [],
 };
+
+export const imageFileRef2: FileReference = {
+  id: generateId(),
+  name: 'image-2.png',
+  type: 'image/png',
+  size: 4028,
+  attachedTo: [],
+};
+
+export const imageFileRef3: FileReference = {
+  id: generateId(),
+  name: 'image-3.jpg',
+  type: 'image/jpg',
+  size: 1043,
+  attachedTo: [],
+};
+
+export const fileReferences = [imageFileRef, imageFileRef2, imageFileRef3];
