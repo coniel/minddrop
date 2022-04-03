bf7b53d970942ed2b00e320cb68504809ad3cd98
Oscar Coniel <oscar@coniel.com>
2022-04-03 23:27:43 +0300

    Add document and store revision IDs to rich text store

--- packages/rich-text-editor/src/useRichTextEditorStore/useRichTextEditorStore.test.ts
+++ packages/rich-text-editor/src/useRichTextEditorStore/useRichTextEditorStore.test.ts
@@ -7,113 +7,284 @@ import { useRichTextEditorStore } from './useRichTextEditorStore';
 const { paragraphElement1, paragraphElement2 } = RICH_TEXT_TEST_DATA;
 
 describe('useRichTextEditorStore', () => {
-  it('adds a created element', () => {
-    // Add a created element
-    useRichTextEditorStore.getState().addCreatedElement(paragraphElement1);
-    // Add a second created element
-    useRichTextEditorStore.getState().addCreatedElement(paragraphElement2);
-
-    // Element should be added to `createdElements`
-    expect(
-      useRichTextEditorStore.getState().createdElements[paragraphElement1.id],
-    ).toEqual(paragraphElement1);
-    // Adds the element ID to the end of `creationOrder`
-    expect(useRichTextEditorStore.getState().creationOrder).toEqual([
-      paragraphElement1.id,
-      paragraphElement2.id,
-    ]);
+  afterEach(() => {
+    // Clear the store data
+    useRichTextEditorStore.getState().clear();
+    // Clear the document revisions
+    useRichTextEditorStore.getState().clearDocumentRevisions();
   });
 
-  it('removes a created element', () => {
-    // Add a created element
-    useRichTextEditorStore.getState().addCreatedElement(paragraphElement1);
-    // Remove the created element
-    useRichTextEditorStore
-      .getState()
-      .removeCreatedElement(paragraphElement1.id);
+  it('adds a revision', () => {
+    // Add a revision
+    useRichTextEditorStore.getState().addDocumentRevision('revision-id');
 
-    // Element should no longer be in `createdElements`
-    expect(
-      useRichTextEditorStore.getState().createdElements[paragraphElement1.id],
-    ).toBeUndefined();
-    // Removes the element ID from `creationOrder`
-    expect(useRichTextEditorStore.getState().creationOrder).not.toContain(
-      paragraphElement1.id,
+    // Adds the revision to `revisions`
+    expect(useRichTextEditorStore.getState().documentRevisions).toContain(
+      'revision-id',
     );
   });
 
-  it('adds an updated element', () => {
-    // The update data
-    const data: UpdateRichTextElementData = {
-      children: [{ text: 'Hello world' }],
-    };
+  describe('addCreatedElement', () => {
+    it('adds the element to `createdElements`', () => {
+      // Add a created element
+      useRichTextEditorStore.getState().addCreatedElement(paragraphElement1);
 
-    // Add an updated element
-    useRichTextEditorStore
-      .getState()
-      .addUpdatedElement(paragraphElement1.id, data);
+      // Element should be added to `createdElements`
+      expect(
+        useRichTextEditorStore.getState().createdElements[paragraphElement1.id],
+      ).toEqual(paragraphElement1);
+    });
 
-    // Data should be added to `updatedElements`
-    expect(
-      useRichTextEditorStore.getState().updatedElements[paragraphElement1.id],
-    ).toEqual(data);
+    it('adds the created element ID to the end of `creationOrder`', () => {
+      // Add a created element
+      useRichTextEditorStore.getState().addCreatedElement(paragraphElement1);
+      // Add a second created element
+      useRichTextEditorStore.getState().addCreatedElement(paragraphElement2);
+
+      // Adds the element ID to the end of `creationOrder`
+      expect(useRichTextEditorStore.getState().creationOrder).toEqual([
+        paragraphElement1.id,
+        paragraphElement2.id,
+      ]);
+    });
+
+    it('updates the store revision', () => {
+      // Get the current revision ID
+      const originalRevision = useRichTextEditorStore.getState().storeRevision;
+
+      // Add a created element
+      useRichTextEditorStore.getState().addCreatedElement(paragraphElement1);
+
+      // Should chnage the `storeRevision`
+      expect(useRichTextEditorStore.getState().storeRevision).not.toBe(
+        originalRevision,
+      );
+    });
   });
 
-  it('removes an updated element', () => {
-    // Add an updated element
-    useRichTextEditorStore
-      .getState()
-      .addUpdatedElement(paragraphElement1.id, { children: [{ text: '' }] });
-    // Remove the updated element
-    useRichTextEditorStore
-      .getState()
-      .removeUpdatedElement(paragraphElement1.id);
+  describe('removeCreatedElement', () => {
+    it('removes the element from `createdElements`', () => {
+      // Add a created element
+      useRichTextEditorStore.getState().addCreatedElement(paragraphElement1);
+      // Remove the created element
+      useRichTextEditorStore
+        .getState()
+        .removeCreatedElement(paragraphElement1.id);
 
-    // Element should no longer be in `updatedElements`
-    expect(
-      useRichTextEditorStore.getState().updatedElements[paragraphElement1.id],
-    ).toBeUndefined();
+      // Element should no longer be in `createdElements`
+      expect(
+        useRichTextEditorStore.getState().createdElements[paragraphElement1.id],
+      ).toBeUndefined();
+    });
+
+    it('removes the element ID from `creationOrder`', () => {
+      // Add a created element
+      useRichTextEditorStore.getState().addCreatedElement(paragraphElement1);
+      // Remove the created element
+      useRichTextEditorStore
+        .getState()
+        .removeCreatedElement(paragraphElement1.id);
+
+      // Removes the element ID from `creationOrder`
+      expect(useRichTextEditorStore.getState().creationOrder).not.toContain(
+        paragraphElement1.id,
+      );
+    });
+
+    it('updates the store revision', () => {
+      // Add a created element
+      useRichTextEditorStore.getState().addCreatedElement(paragraphElement1);
+
+      // Get the current revision ID
+      const originalRevision = useRichTextEditorStore.getState().storeRevision;
+
+      // Remove the created element
+      useRichTextEditorStore
+        .getState()
+        .removeCreatedElement(paragraphElement1.id);
+
+      // Should chnage the `storeRevision`
+      expect(useRichTextEditorStore.getState().storeRevision).not.toBe(
+        originalRevision,
+      );
+    });
   });
 
-  it('adds a deleted element', () => {
-    // Add a deleted element
-    useRichTextEditorStore.getState().addDeletedElement(paragraphElement1.id);
+  describe('addUpdatedElement', () => {
+    it('adds the updated element data to `updatedElements`', () => {
+      // The update data
+      const data: UpdateRichTextElementData = {
+        children: [{ text: 'Hello world' }],
+      };
 
-    // Element ID should be in `deletedElements`
-    expect(useRichTextEditorStore.getState().deletedElements).toContain(
-      paragraphElement1.id,
-    );
+      // Add an updated element
+      useRichTextEditorStore
+        .getState()
+        .addUpdatedElement(paragraphElement1.id, data);
+
+      // Data should be added to `updatedElements`
+      expect(
+        useRichTextEditorStore.getState().updatedElements[paragraphElement1.id],
+      ).toEqual(data);
+    });
+
+    it('updates the store revision', () => {
+      // Get the current revision ID
+      const originalRevision = useRichTextEditorStore.getState().storeRevision;
+
+      // Add an updated element
+      useRichTextEditorStore
+        .getState()
+        .addUpdatedElement(paragraphElement1.id, { children: [{ text: '' }] });
+
+      // Should chnage the `storeRevision`
+      expect(useRichTextEditorStore.getState().storeRevision).not.toBe(
+        originalRevision,
+      );
+    });
   });
 
-  it('removes a deleted element', () => {
-    // Add a deleted element
-    useRichTextEditorStore.getState().addDeletedElement(paragraphElement1.id);
-    // Remove the deleted element
-    useRichTextEditorStore
-      .getState()
-      .removeDeletedElement(paragraphElement1.id);
+  describe('removeUpdatedElement', () => {
+    it('removes the updated element data from `updatedElements`', () => {
+      // Add an updated element
+      useRichTextEditorStore
+        .getState()
+        .addUpdatedElement(paragraphElement1.id, { children: [{ text: '' }] });
+      // Remove the updated element
+      useRichTextEditorStore
+        .getState()
+        .removeUpdatedElement(paragraphElement1.id);
 
-    // Element ID should no longer be in `deletedElements`
-    expect(useRichTextEditorStore.getState().deletedElements).not.toContain(
-      paragraphElement1.id,
-    );
+      // Element should no longer be in `updatedElements`
+      expect(
+        useRichTextEditorStore.getState().updatedElements[paragraphElement1.id],
+      ).toBeUndefined();
+    });
+
+    it('updates the store revision', () => {
+      // Add an updated element
+      useRichTextEditorStore
+        .getState()
+        .addUpdatedElement(paragraphElement1.id, { children: [{ text: '' }] });
+
+      // Get the current revision ID
+      const originalRevision = useRichTextEditorStore.getState().storeRevision;
+
+      // Remove the updated element
+      useRichTextEditorStore
+        .getState()
+        .removeUpdatedElement(paragraphElement1.id);
+
+      // Should chnage the `storeRevision`
+      expect(useRichTextEditorStore.getState().storeRevision).not.toBe(
+        originalRevision,
+      );
+    });
   });
 
-  it('sets document children', () => {
-    // Set document children
-    useRichTextEditorStore
-      .getState()
-      .setDocumentChildren([paragraphElement1.id, paragraphElement2.id]);
+  describe('addDeletedElement', () => {
+    it('adds the deleted element ID to `deletedElements`', () => {
+      // Add a deleted element
+      useRichTextEditorStore.getState().addDeletedElement(paragraphElement1.id);
 
-    // Children should be set to the new value
-    expect(useRichTextEditorStore.getState().documentChildren).toEqual([
-      paragraphElement1.id,
-      paragraphElement2.id,
-    ]);
+      // Element ID should be in `deletedElements`
+      expect(useRichTextEditorStore.getState().deletedElements).toContain(
+        paragraphElement1.id,
+      );
+    });
+
+    it('updates the store revision', () => {
+      // Get the current revision ID
+      const originalRevision = useRichTextEditorStore.getState().storeRevision;
+
+      // Add a deleted element
+      useRichTextEditorStore.getState().addDeletedElement(paragraphElement1.id);
+
+      // Should chnage the `storeRevision`
+      expect(useRichTextEditorStore.getState().storeRevision).not.toBe(
+        originalRevision,
+      );
+    });
   });
 
-  it('clears the store', () => {
+  describe('removeDeletedElement', () => {
+    it('removes the element ID from `deletedElements`', () => {
+      // Add a deleted element
+      useRichTextEditorStore.getState().addDeletedElement(paragraphElement1.id);
+      // Remove the deleted element
+      useRichTextEditorStore
+        .getState()
+        .removeDeletedElement(paragraphElement1.id);
+
+      // Element ID should no longer be in `deletedElements`
+      expect(useRichTextEditorStore.getState().deletedElements).not.toContain(
+        paragraphElement1.id,
+      );
+    });
+
+    it('updates the store revision', () => {
+      // Add a deleted element
+      useRichTextEditorStore.getState().addDeletedElement(paragraphElement1.id);
+
+      // Get the current revision ID
+      const originalRevision = useRichTextEditorStore.getState().storeRevision;
+
+      // Remove the deleted element
+      useRichTextEditorStore
+        .getState()
+        .removeDeletedElement(paragraphElement1.id);
+
+      // Should chnage the `storeRevision`
+      expect(useRichTextEditorStore.getState().storeRevision).not.toBe(
+        originalRevision,
+      );
+    });
+  });
+
+  describe('setDocumentChildren', () => {
+    it('sets the child IDs to `documentChildren`', () => {
+      // Set document children
+      useRichTextEditorStore
+        .getState()
+        .setDocumentChildren([paragraphElement1.id, paragraphElement2.id]);
+
+      // Children should be set to the new value
+      expect(useRichTextEditorStore.getState().documentChildren).toEqual([
+        paragraphElement1.id,
+        paragraphElement2.id,
+      ]);
+    });
+
+    it('updates the store revision', () => {
+      // Get the current revision ID
+      const originalRevision = useRichTextEditorStore.getState().storeRevision;
+
+      // Set document children
+      useRichTextEditorStore
+        .getState()
+        .setDocumentChildren([paragraphElement1.id, paragraphElement2.id]);
+
+      // Should chnage the `storeRevision`
+      expect(useRichTextEditorStore.getState().storeRevision).not.toBe(
+        originalRevision,
+      );
+    });
+  });
+
+  it('clears revisions', () => {
+    // Add a revision
+    useRichTextEditorStore.getState().addDocumentRevision('revision-id');
+
+    // Clear revisions
+    useRichTextEditorStore.getState().clearDocumentRevisions();
+
+    // Should clear revisions
+    expect(useRichTextEditorStore.getState().documentRevisions).toEqual([]);
+  });
+
+  it('clears all store data except revisions', () => {
     // Set data in the store
+    useRichTextEditorStore.getState().addDocumentRevision('revision-id');
     useRichTextEditorStore.getState().addCreatedElement(paragraphElement1);
     useRichTextEditorStore.getState().addUpdatedElement(paragraphElement1.id, {
       children: [{ text: 'Hello world' }],
@@ -133,6 +304,7 @@ describe('useRichTextEditorStore', () => {
       updatedElements,
       deletedElements,
       documentChildren,
+      documentRevisions,
     } = useRichTextEditorStore.getState();
 
     // Should clear `createdElements`
@@ -145,5 +317,7 @@ describe('useRichTextEditorStore', () => {
     expect(deletedElements).toEqual([]);
     // Should clear `documentChildren`
     expect(documentChildren).toEqual([]);
+    // Should not clear revisions
+    expect(documentRevisions).toEqual(['revision-id']);
   });
 });
--- packages/rich-text-editor/src/useRichTextEditorStore/useRichTextEditorStore.ts
+++ packages/rich-text-editor/src/useRichTextEditorStore/useRichTextEditorStore.ts
@@ -3,9 +3,18 @@ import {
   RichTextElementMap,
   UpdateRichTextElementData,
 } from '@minddrop/rich-text';
+import { generateId } from '@minddrop/utils';
 import createStore from 'zustand';
 
 export interface RichTextEditorStore {
+  /**
+   * A UUID changed every time store data (except
+   * `documentRevisions`) is modified.
+   * Allows the store to be watched for changes without
+   * needing to do deep comparisons of the data.
+   */
+  storeRevision: string;
+
   /**
    * An `{ [id]: RichTextElement }` map of created rich
    * text elements.
@@ -30,10 +39,22 @@ export interface RichTextEditorStore {
   deletedElements: string[];
 
   /**
+   * The document revisions created during this editing session.
+   */
+  documentRevisions: string[];
+
+  /**
    * The document's children. Empty if no changes have been
    * made to them.
    */
   documentChildren: string[];
+
+  /**
+   * Adds a new document revision ID to the store.
+   *
+   * @param revision The revision ID.
+   */
+  addDocumentRevision(revision: string): void;
 
   /**
    * Adds a created element to the store.
@@ -86,22 +107,33 @@ export interface RichTextEditorStore {
   setDocumentChildren(children: string[]): void;
 
   /**
-   * Clears all data from the store.
+   * Clears the revisions.
+   */
+  clearDocumentRevisions(): void;
+
+  /**
+   * Clears all data apart except revisions from the store.
    */
   clear(): void;
 }
 
 export const useRichTextEditorStore = createStore<RichTextEditorStore>(
   (set) => ({
+    storeRevision: generateId(),
     createdElements: {},
     creationOrder: [],
     updatedElements: {},
     deletedElements: [],
+    documentRevisions: [],
     documentChildren: [],
 
     addCreatedElement: (element) =>
       set((state) => ({
+        // Set a new store revision ID
+        storeRevision: generateId(),
+        // Add the element to `createdElements`
         createdElements: { ...state.createdElements, [element.id]: element },
+        // Add the element ID to the end of `creationOrder`
         creationOrder: [...state.creationOrder, element.id],
       })),
 
@@ -113,10 +145,12 @@ export const useRichTextEditorStore = createStore<RichTextEditorStore>(
         // Remove the element
         delete createdElements[id];
 
-        // Return state the element removed from `createdElements` and
-        // the element ID removed from `creationOrder`.
         return {
+          // Set a new store revision ID
+          storeRevision: generateId(),
+          // Set `createdElements` to the updated value
           createdElements,
+          // Remove the element ID from `creationOrder`
           creationOrder: state.creationOrder.filter(
             (elementId) => elementId !== id,
           ),
@@ -125,6 +159,9 @@ export const useRichTextEditorStore = createStore<RichTextEditorStore>(
 
     addUpdatedElement: (id, data) =>
       set((state) => ({
+        // Set a new store revision ID
+        storeRevision: generateId(),
+        // Add the updated element data to `updatedElements`
         updatedElements: { ...state.updatedElements, [id]: data },
       })),
 
@@ -136,25 +173,56 @@ export const useRichTextEditorStore = createStore<RichTextEditorStore>(
         // Remove the element update data
         delete updatedElements[id];
 
-        // Return `updatedElements` with the data removed
-        return { updatedElements };
+        return {
+          // Set a new store revision ID
+          storeRevision: generateId(),
+          // Set `updatedElements` to the updated value
+          updatedElements,
+        };
       }),
 
     addDeletedElement: (id) =>
-      set((state) => ({ deletedElements: [...state.deletedElements, id] })),
+      set((state) => ({
+        // Set a new store revision ID
+        storeRevision: generateId(),
+        // Add the element ID to `deletedElements`
+        deletedElements: [...state.deletedElements, id],
+      })),
 
     removeDeletedElement: (id) =>
       set((state) => ({
+        // Set a new store revision ID
+        storeRevision: generateId(),
+        // Remove the element ID from `deletedElements`
         deletedElements: state.deletedElements.filter(
           (elementId) => elementId !== id,
         ),
       })),
 
+    addDocumentRevision: (revision) =>
+      set((state) => ({
+        // Add the revision ID to `documentRevisions`
+        documentRevisions: [...state.documentRevisions, revision],
+      })),
+
     setDocumentChildren: (children) =>
-      set(() => ({ documentChildren: children })),
+      set(() => ({
+        // Set a new store revision ID
+        storeRevision: generateId(),
+        // Set the child IDs as `documentChildren`
+        documentChildren: children,
+      })),
+
+    clearDocumentRevisions: () =>
+      set({
+        // Set `documentRevisions` back to an empty array
+        documentRevisions: [],
+      }),
 
     clear: () =>
       set({
+        // Set all non revision related data fields
+        // back to empty values.
         createdElements: {},
         creationOrder: [],
         updatedElements: {},
