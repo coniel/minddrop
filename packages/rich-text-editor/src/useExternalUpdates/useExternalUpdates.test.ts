869094522048f20eaa7caeee260390feefab9ad3
Oscar Coniel <oscar@coniel.com>
2022-04-05 02:16:22 +0300

    Add use external updates hook

---
+++ packages/rich-text-editor/src/useExternalUpdates/useExternalUpdates.test.ts
@@ -0,0 +1,113 @@
+import {
+  RichTextDocuments,
+  RichTextElements,
+  RICH_TEXT_TEST_DATA,
+} from '@minddrop/rich-text';
+import { act, renderHook } from '@minddrop/test-utils';
+import { generateId } from '@minddrop/utils';
+import { setup, cleanup, core } from '../test-utils';
+import { useEditorSession } from '../useEditorSession';
+import { useRichTextEditorStore } from '../useRichTextEditorStore';
+import { useExternalUpdates } from './useExternalUpdates';
+
+const { richTextDocument1, paragraphElement2 } = RICH_TEXT_TEST_DATA;
+
+function createEditorSession() {
+  const { result } = renderHook(() => useEditorSession(richTextDocument1.id));
+
+  return result.current;
+}
+
+describe('useExternalUpdates', () => {
+  beforeEach(setup);
+
+  afterEach(cleanup);
+
+  it('resets the editor value if it has changed externally', () => {
+    // Create an editor session
+    const [editor, sessionId] = createEditorSession();
+
+    // Run the hook for the editor session
+    renderHook(() =>
+      useExternalUpdates(editor, richTextDocument1.id, sessionId),
+    );
+
+    act(() => {
+      // Modify the value of the document as well a its revision
+      RichTextDocuments.setChildren(
+        core,
+        richTextDocument1.id,
+        [paragraphElement2.id],
+        generateId(),
+      );
+    });
+
+    // Get the inserted udpated version of the inserted element
+    const element = RichTextElements.get(paragraphElement2.id);
+
+    // Editor children should be reset to the updated value
+    expect(editor.children).toEqual([element]);
+  });
+
+  it('does not reset if the value has changed within the editor', () => {
+    // Create an editor session
+    const [editor, sessionId] = createEditorSession();
+
+    // Run the hook for the editor session
+    renderHook(() =>
+      useExternalUpdates(editor, richTextDocument1.id, sessionId),
+    );
+
+    // Create a revision ID
+    const revision = generateId();
+
+    // Add the revision ID to the session's `documentRevisions`
+    // simulating that the document update was caused by an edit
+    // within the editor.
+    useRichTextEditorStore.getState().addDocumentRevision(sessionId, revision);
+
+    act(() => {
+      // Modify the value of the document as well a its revision,
+      // using the revision ID from above.
+      RichTextDocuments.setChildren(
+        core,
+        richTextDocument1.id,
+        [paragraphElement2.id],
+        revision,
+      );
+    });
+
+    // Editor children should not be reset (initial value was empty)
+    expect(editor.children).toEqual([]);
+  });
+
+  it('resets the session changes', () => {
+    // Create an editor session
+    const [editor, sessionId] = createEditorSession();
+
+    // Run the hook for the editor session
+    renderHook(() =>
+      useExternalUpdates(editor, richTextDocument1.id, sessionId),
+    );
+
+    act(() => {
+      // Modify the value of the document as well a its revision
+      RichTextDocuments.setChildren(
+        core,
+        richTextDocument1.id,
+        [paragraphElement2.id],
+        generateId(),
+      );
+    });
+
+    // Get the editor session data
+    const session = useRichTextEditorStore.getState().sessions[sessionId];
+
+    // Session should not contain any changes
+    expect(session.createdElements).toEqual({});
+    expect(session.creationOrder).toEqual([]);
+    expect(session.updatedElements).toEqual({});
+    expect(session.deletedElements).toEqual([]);
+    expect(session.documentChildren).toEqual([]);
+  });
+});
---
+++ packages/rich-text-editor/src/useExternalUpdates/useExternalUpdates.ts
@@ -0,0 +1,51 @@
+import { RichTextElements, useRichTextDocument } from '@minddrop/rich-text';
+import { useEffect, useRef } from 'react';
+import { Descendant } from 'slate';
+import { Transforms } from '../Transforms';
+import { Editor } from '../types';
+import { useRichTextEditorStore } from '../useRichTextEditorStore';
+
+/**
+ * Resets the editor children to the latest value when
+ * the document's revision changes, unless the changes
+ * originate from the current editor session.
+ *
+ * @param editor The editor instance.
+ * @param documentId The ID of the document being edited.
+ * @param sessionId The editor session ID.
+ */
+export function useExternalUpdates(
+  editor: Editor,
+  documentId: string,
+  sessionId: string,
+): void {
+  // Watch the document for changes
+  const document = useRichTextDocument(documentId);
+
+  useEffect(() => {
+    // Get the session's document revisions
+    const { documentRevisions } =
+      useRichTextEditorStore.getState().sessions[sessionId];
+
+    // If the new document revision is not included in this editor
+    // session's document revisions, it means that the document's
+    // value has been changed externally.
+    if (!documentRevisions.includes(document.revision)) {
+      // Get the rich text elements making up the document's children
+      const elements = RichTextElements.get(document.children);
+
+      // Create the editor value by aranging the rich text elements
+      // in the order they are listed in the document's children.
+      const value = document.children.map((childId) => elements[childId]);
+
+      // Reset the editor content to the new value
+      Transforms.resetNodes(editor, { nodes: value });
+
+      // Reseting nodes removes the current nodes and inserts the updated ones,
+      // this causes the changes to be added to the editor session in the rich
+      // text editor store. Reset the editor session to clear the changes as
+      // they aren't actually changes.
+      useRichTextEditorStore.getState().resetSessionChanges(sessionId);
+    }
+  }, [document.revision, sessionId]);
+}
