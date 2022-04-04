5cf285fe7f222e0106896efdd0116fb5c62fc049
Oscar Coniel <oscar@coniel.com>
2022-04-04 16:20:56 +0300

    Add useRichTextElementConfigs hook

--- packages/rich-text/src/index.ts
+++ packages/rich-text/src/index.ts
@@ -5,4 +5,5 @@ export * from './RichTextDocuments';
 export * from './useRichTextDocument';
 export * from './useRichTextElement';
 export * from './useRichTextElements';
+export * from './useRichTextElementConfigs';
 export * as RICH_TEXT_TEST_DATA from './test-utils/rich-text.data';
