881ea9993792b026220e723e53935493db08318a
Oscar Coniel <oscar@coniel.com>
2022-04-05 15:59:48 +0300

    Fix build issue

--- packages/rich-text-editor/src/types/Editor.types.ts
+++ packages/rich-text-editor/src/types/Editor.types.ts
@@ -1,3 +1,13 @@
-import { Editor as SlateEditor } from 'slate';
+import { BaseEditor, Editor as SlateEditor } from 'slate';
+import { ReactEditor } from 'slate-react';
+import { RichTextNode, RichTextElement } from '@minddrop/rich-text';
+
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor;
+    Element: RichTextElement;
+    Text: RichTextNode;
+  }
+}
 
 export type Editor = SlateEditor;
--- packages/rich-text-editor/src/types/index.ts
+++ packages/rich-text-editor/src/types/index.ts
@@ -2,4 +2,3 @@ export * from './Editor.types';
 export * from './RichTextEditorPluginConfig.types';
 export * from './RichTextElementConfig.types';
 export * from './RichTextElementProps.types';
-import './Slate.d';
