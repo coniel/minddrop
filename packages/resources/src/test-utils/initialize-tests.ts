bae662bc15aa30735222a851b174319305f3a307
Oscar Coniel <oscar@coniel.com>
2022-04-12 00:27:58 +0300

    Add initialize-tests

---
+++ packages/resources/src/test-utils/initialize-tests.ts
@@ -0,0 +1,11 @@
+import { initializeCore } from '@minddrop/core';
+
+export const core = initializeCore({ appId: 'app', extensionId: 'app' });
+
+export function setup() {
+  //
+}
+
+export function cleanup() {
+  //
+}
