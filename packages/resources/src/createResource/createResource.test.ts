77e230c0c103fb14588fd16def1e51536c704bfb
Oscar Coniel <oscar@coniel.com>
2022-04-13 00:55:36 +0300

    Implement createResource function

--- packages/resources/src/createResource/createResource.test.ts
+++ packages/resources/src/createResource/createResource.test.ts
@@ -1,7 +1,518 @@
+import { mapById } from '@minddrop/utils';
+import {
+  ResourceDocumentNotFoundError,
+  ResourceValidationError,
+} from '../errors';
+import { generateResourceDocument } from '../generateResourceDocument';
+import { cleanup, core } from '../test-utils';
+import { ResourceApi, ResourceDataSchema, ResourceDocument } from '../types';
 import { createResource } from './createResource';
 
-describe('createResource', () => {
-  it('does something useful', () => {
-    //
+interface Data {
+  foo: string;
+  bar: string;
+  baz: string;
+  qux: string;
+}
+
+interface CreateData {
+  foo?: string;
+  qux?: string;
+}
+
+interface UpdateData {
+  foo?: string;
+  bar?: string;
+}
+
+const dataSchema: ResourceDataSchema<Data> = {
+  foo: {
+    type: 'string',
+    required: true,
+  },
+  bar: {
+    type: 'string',
+    required: true,
+  },
+  baz: {
+    type: 'string',
+    required: true,
+  },
+  qux: {
+    type: 'string',
+    static: true,
+  },
+};
+
+const onClear = jest.fn();
+
+const ResourceApi = createResource<Data, CreateData, UpdateData>({
+  resource: 'tests:test',
+  dataSchema,
+  defaultData: {
+    foo: 'foo',
+    bar: 'bar',
+    baz: 'baz',
+    qux: 'qux',
+  },
+  onClear,
+  onCreate: (core, document) => ({
+    ...document,
+    baz: 'onCreate baz',
+    qux: 'onCreate qux',
+  }),
+  onUpdate: (core, update) => ({ ...update.changes, bar: 'onUpdate bar' }),
+  onGet: (document) => ({ ...document, qux: 'onGet qux' }),
+});
+
+describe('resource API', () => {
+  let existingDoc1: ResourceDocument<Data>;
+  let existingDoc2: ResourceDocument<Data>;
+
+  afterEach(() => {
+    ResourceApi.store.clear();
+    cleanup();
+  });
+
+  beforeEach(() => {
+    // Create a couple of test documents
+    existingDoc1 = ResourceApi.create(core, {});
+    existingDoc2 = ResourceApi.create(core, {});
+  });
+
+  describe('create', () => {
+    it('creates a document using the default data', () => {
+      // Create a new document
+      const document = ResourceApi.create(core, {});
+
+      // Document should contain default data
+      expect(document.bar).toBe('bar');
+    });
+
+    it('merges in the provided data', () => {
+      // Create a new document
+      const document = ResourceApi.create(core, { foo: 'data foo' });
+
+      // Document should contain provided data which overwrites
+      // default data.
+      expect(document.foo).toBe('data foo');
+    });
+
+    it("merges in the `onCreate` callback's data", () => {
+      // Create a new document
+      const document = ResourceApi.create(core, {
+        qux: 'data qux',
+      });
+
+      // Document should contain `onCreate` callback data which
+      // overwrites default and provided data.
+      expect(document.baz).toBe('onCreate baz');
+      expect(document.qux).toBe('onCreate qux');
+    });
+
+    it('adds the document to the store', () => {
+      // Create a new document
+      const document = ResourceApi.create(core, {});
+
+      // Document should be in the store
+      expect(ResourceApi.store.get(document.id)).toEqual(document);
+    });
+
+    it('validates the created document', () => {
+      // Attempt to create an invalid document. Should throw a
+      // `ResourceDocumentValidationError`.
+      // @ts-ignore
+      expect(() => ResourceApi.create(core, { foo: 1234 })).toThrowError(
+        ResourceValidationError,
+      );
+    });
+
+    it('dispatches a `tests:test:create` event', (done) => {
+      // Listen to 'tests:test:create' events
+      core.addEventListener('tests:test:create', (payload) => {
+        // Get the created document
+        const document = ResourceApi.store.get(payload.data.id);
+
+        // Payload data should be the created document
+        expect(payload.data).toEqual(document);
+        done();
+      });
+
+      // Create a new document
+      ResourceApi.create(core, { foo: 'provided foo' });
+    });
+  });
+
+  describe('update', () => {
+    it('updates a document using the provided data', () => {
+      // Update a document
+      const updated = ResourceApi.update(core, existingDoc1.id, {
+        foo: 'updated foo',
+      });
+
+      // Document should be updated
+      expect(updated.foo).toBe('updated foo');
+    });
+
+    it("uses the config's `onUpdate` data", () => {
+      // Update a document
+      const updated = ResourceApi.update(core, existingDoc1.id, {
+        bar: 'updated bar',
+      });
+
+      // Document should contain `onUpdate` data
+      expect(updated.bar).toBe('onUpdate bar');
+    });
+
+    it('sets the updated document in the store', () => {
+      // Update a document
+      ResourceApi.update(core, existingDoc1.id, {
+        foo: 'updated foo',
+      });
+
+      // Get the document from the store
+      const storeDoc = ResourceApi.get(existingDoc1.id);
+
+      // Store document should be updated
+      expect(storeDoc.foo).toBe('updated foo');
+    });
+
+    it('dispatches a `tests:test:update` event', (done) => {
+      // Listen to 'tests:test:update' events
+      core.addEventListener('tests:test:update', (payload) => {
+        // Get the updated document
+        const updatedDocument = ResourceApi.store.get(existingDoc1.id);
+
+        // Payload data should be an update object
+        expect(payload.data).toEqual({
+          before: existingDoc1,
+          after: updatedDocument,
+          changes: {
+            foo: 'updated foo',
+            bar: 'onUpdate bar',
+            revision: updatedDocument.revision,
+            updatedAt: updatedDocument.updatedAt,
+          },
+        });
+        done();
+      });
+
+      // Update a document
+      ResourceApi.update(core, existingDoc1.id, { foo: 'updated foo' });
+    });
+  });
+
+  describe('delete', () => {
+    it('soft-deletes the document', () => {
+      // Delete a document
+      const document = ResourceApi.delete(core, existingDoc1.id);
+
+      // Document should be deleted
+      expect(document.deleted).toBe(true);
+    });
+
+    it('sets the deleted document in the store', () => {
+      // Delete a document
+      ResourceApi.delete(core, existingDoc1.id);
+
+      // Get the document from the store
+      const document = ResourceApi.get(existingDoc1.id);
+
+      // Document should be deleted
+      expect(document.deleted).toBe(true);
+    });
+
+    it('dispatches a `tests:test:delete` event', (done) => {
+      // Listen to 'tests:test:delete' events
+      core.addEventListener('tests:test:delete', (payload) => {
+        // Get the deleted document
+        const deletedDocument = ResourceApi.store.get(existingDoc1.id);
+
+        // Payload data should be the deleted document
+        expect(payload.data).toEqual(deletedDocument);
+        done();
+      });
+
+      // Delete a document
+      ResourceApi.delete(core, existingDoc1.id);
+    });
+  });
+
+  describe('restore', () => {
+    beforeEach(() => {
+      // Delete the document
+      ResourceApi.delete(core, existingDoc1.id);
+    });
+
+    it('restores the document', () => {
+      // Restore a document
+      const document = ResourceApi.restore(core, existingDoc1.id);
+
+      // Document should be restored
+      expect(document.deleted).toBeUndefined();
+    });
+
+    it('sets the restored document in the store', () => {
+      // Restore a document
+      ResourceApi.restore(core, existingDoc1.id);
+
+      // Get the document from the store
+      const document = ResourceApi.get(existingDoc1.id);
+
+      // Document should be restored
+      expect(document.deleted).toBeUndefined();
+    });
+
+    it('dispatches a `tests:test:restore` event', (done) => {
+      // Listen to 'tests:test:restore' events
+      core.addEventListener('tests:test:restore', (payload) => {
+        // Get the restored document
+        const restoredDocument = ResourceApi.store.get(existingDoc1.id);
+
+        // Payload data should be the restored document
+        expect(payload.data).toEqual(restoredDocument);
+        done();
+      });
+
+      // Restore a document
+      ResourceApi.restore(core, existingDoc1.id);
+    });
+  });
+
+  describe('deletePermanently', () => {
+    it('removes the document from the store', () => {
+      // Permanently delete a document
+      ResourceApi.deletePermanently(core, existingDoc1.id);
+
+      // Document should no longer be in the store
+      expect(() => ResourceApi.get(existingDoc1.id)).toThrowError(
+        ResourceDocumentNotFoundError,
+      );
+    });
+
+    it('dispatches a `tests:test:delete-permanently` event', (done) => {
+      // Listen to 'tests:test:delete-permanently' events
+      core.addEventListener('tests:test:delete-permanently', (payload) => {
+        // Payload data should be the deleted document
+        expect(payload.data).toEqual(existingDoc1);
+        done();
+      });
+
+      // Permanently delete a document
+      ResourceApi.deletePermanently(core, existingDoc1.id);
+    });
+
+    it('returns the deleted document', () => {
+      // Permanently delete a document
+      const document = ResourceApi.deletePermanently(core, existingDoc1.id);
+
+      // Should return the document
+      expect(document).toEqual(existingDoc1);
+    });
+  });
+
+  describe('get', () => {
+    describe('single document', () => {
+      it('returns the requested document given a single ID', () => {
+        // Get a document
+        const document = ResourceApi.get(existingDoc1.id);
+
+        // Should return the document
+        expect(document).toBeDefined();
+        expect(document.id).toBe(existingDoc1.id);
+      });
+
+      it("merges in the config's `onGet` data", () => {
+        // Get a document
+        const document = ResourceApi.get(existingDoc2.id);
+
+        // Should merge the data returned by the config's `onGet`
+        // callback into the returned document.
+        expect(document.qux).toBe('onGet qux');
+      });
+    });
+
+    describe('multiple documents', () => {
+      it('returns the requested documents given multiple IDs', () => {
+        // Get some documents
+        const documents = ResourceApi.get([existingDoc1.id, existingDoc2.id]);
+
+        // Should return the documents mapped by ID
+        expect(documents[existingDoc1.id]).toBeDefined();
+        expect(documents[existingDoc2.id]).toBeDefined();
+      });
+
+      it("merges in the config's `onGet` data", () => {
+        // Get some documents
+        const documents = ResourceApi.get([existingDoc1.id, existingDoc2.id]);
+
+        // Should merge the data returned by the config's `onGet`
+        // callback into the returned documents.
+        expect(documents[existingDoc2.id].qux).toBe('onGet qux');
+      });
+    });
+  });
+
+  describe('getAll', () => {
+    it('returns all the documents from the store', () => {
+      // Get some documents
+      const documents = ResourceApi.getAll();
+
+      // Should return the documents mapped by ID
+      expect(documents[existingDoc1.id]).toBeDefined();
+      expect(documents[existingDoc2.id]).toBeDefined();
+    });
+
+    it("merges in the config's `onGet` data", () => {
+      // Get some documents
+      const documents = ResourceApi.get([existingDoc1.id, existingDoc2.id]);
+
+      // Should merge the data returned by the config's `onGet`
+      // callback into the returned documents.
+      expect(documents[existingDoc2.id].qux).toBe('onGet qux');
+    });
+  });
+
+  describe('store', () => {
+    // Generate a test document that is not in the store
+    const document = generateResourceDocument<Data>({
+      foo: 'foo',
+      bar: 'bar',
+      baz: 'baz',
+      qux: 'qux',
+    });
+
+    describe('get', () => {
+      describe('single document', () => {
+        it('returns the requested document given a single ID', () => {
+          // Get a document
+          const document = ResourceApi.store.get(existingDoc1.id);
+
+          // Should return the document
+          expect(document).toBe(existingDoc1);
+        });
+      });
+
+      describe('multiple documents', () => {
+        it('returns the requested documents given multiple IDs', () => {
+          // Get some documents
+          const documents = ResourceApi.store.get([
+            existingDoc1.id,
+            existingDoc2.id,
+          ]);
+
+          // Should return the documents mapped by ID
+          expect(documents).toEqual(mapById([existingDoc1, existingDoc2]));
+        });
+      });
+    });
+
+    describe('getAll', () => {
+      it('returns all the documents from the store', () => {
+        // Get some documents
+        const documents = ResourceApi.store.getAll();
+
+        // Should return all store documents mapped by ID
+        expect(documents).toEqual(mapById([existingDoc1, existingDoc2]));
+      });
+    });
+
+    describe('load', () => {
+      it('loads documents into the store', () => {
+        // Load a document
+        ResourceApi.store.load(core, [document]);
+
+        // Document should be in the store
+        expect(ResourceApi.store.get(document.id)).toEqual(document);
+      });
+
+      it('dispatches a `tests:test:load` event', (done) => {
+        // Listen to 'tests:test:load' events
+        core.addEventListener('tests:test:load', (payload) => {
+          // Payload data should be the loaded documents
+          expect(payload.data).toEqual([document]);
+          done();
+        });
+
+        // Load a document
+        ResourceApi.store.load(core, [document]);
+      });
+    });
+
+    describe('add', () => {
+      it('adds a document to the store', () => {
+        // Add a document
+        ResourceApi.store.add(core, document);
+
+        // Document should be in the store
+        expect(ResourceApi.store.get(document.id)).toEqual(document);
+      });
+
+      it('dispatches a `tests:test:add` event', (done) => {
+        // Listen to 'tests:test:add' events
+        core.addEventListener('tests:test:add', (payload) => {
+          // Payload data should be the added document
+          expect(payload.data).toEqual(document);
+          done();
+        });
+
+        // Add a document
+        ResourceApi.store.add(core, document);
+      });
+    });
+
+    describe('set', () => {
+      it('sets a document in the store', () => {
+        // Set a document
+        ResourceApi.store.set(core, document);
+
+        // Document should be in the store
+        expect(ResourceApi.store.get(document.id)).toEqual(document);
+      });
+
+      it('dispatches a `tests:test:set` event', (done) => {
+        // Listen to 'tests:test:set' events
+        core.addEventListener('tests:test:set', (payload) => {
+          // Payload data should be the set document
+          expect(payload.data).toEqual(document);
+          done();
+        });
+
+        // Set a document
+        ResourceApi.store.set(core, document);
+      });
+    });
+
+    describe('remove', () => {
+      it('removes a document from the store', () => {
+        // Remove a document
+        ResourceApi.store.remove(core, existingDoc1.id);
+
+        // Document should no longer be in the store
+        expect(ResourceApi.store.get(existingDoc1.id)).toBeUndefined();
+      });
+
+      it('dispatches a `tests:test:remove` event', (done) => {
+        // Listen to 'tests:test:remove' events
+        core.addEventListener('tests:test:remove', (payload) => {
+          // Payload data should be the removed document
+          expect(payload.data).toEqual(existingDoc1);
+          done();
+        });
+
+        // Remove a document
+        ResourceApi.store.remove(core, existingDoc1.id);
+      });
+    });
+
+    describe('clear', () => {
+      it('clears the store', () => {
+        // Clear the store
+        ResourceApi.store.clear();
+
+        // Store should be empty
+        expect(ResourceApi.store.getAll()).toEqual({});
+      });
+    });
   });
 });
--- packages/resources/src/createResource/createResource.ts
+++ packages/resources/src/createResource/createResource.ts
@@ -1,8 +1,75 @@
-import { ResourceApi, ResourceConfig } from '../types';
+import {
+  ResourceApi,
+  ResourceConfig,
+  ResourceDocument,
+  ResourceDocumentCustomData,
+} from '../types';
+import { clearResourceDocuments } from '../clearResourceDocuments';
+import { createResourceDocument } from '../createResourceDocument';
+import { createResourceStore } from '../createResourceStore';
+import { deleteResourceDocument } from '../deleteResourceDocument';
+import { getAllResourceDocuments } from '../getAllResourceDocuments';
+import { getResourceDocument } from '../getResourceDocument';
+import { getResourceDocuments } from '../getResourceDocuments';
+import { loadResourceDocuments } from '../loadResourceDocuments';
+import { permanentlyDeleteResourceDocument } from '../permanentlyDeleteResourceDocument';
+import { restoreResourceDocument } from '../restoreResourceDocument';
+import { updateResourceDocument } from '../updateResourceDocument';
+import { addResourceDocument } from '../addResourceDocument';
+import { setResourceDocument } from '../setResourceDocument';
+import { removeResourceDocument } from '../removeResourceDocument';
 
 /**
- * Does something useful.
+ * Creates a new resource type, returning its API.
+ *
+ * @param config The resource configuration.
+ * @returns A resource API.
  */
-export function createResource(config: ResourceConfig): ResourceApi {
-  return {};
+export function createResource<
+  TData extends ResourceDocumentCustomData,
+  TCreateData extends Partial<TData> & ResourceDocumentCustomData,
+  TUpdateData extends Partial<TData> & ResourceDocumentCustomData,
+>(config: ResourceConfig<TData>): ResourceApi<TData, TCreateData, TUpdateData> {
+  // Create the resource document store
+  const store = createResourceStore<TData>();
+
+  // Create the `get` function which returns one or multiple documents
+  // based on the whether `documentId` argument is a string or an array.
+  function get(documentId: string): ResourceDocument<TData>;
+  function get(documentIds: string[]): Record<string, ResourceDocument<TData>>;
+  function get(documentId: string | string[]) {
+    if (Array.isArray(documentId)) {
+      return getResourceDocuments(store, config, documentId);
+    }
+
+    return getResourceDocument(store, config, documentId);
+  }
+
+  // Return the resource API
+  return {
+    get,
+    getAll: () => getAllResourceDocuments(store, config),
+    create: (core, data) => createResourceDocument(core, store, config, data),
+    update: (core, documentId, data) =>
+      updateResourceDocument(core, store, config, documentId, data),
+    delete: (core, documentId) =>
+      deleteResourceDocument(core, store, config, documentId),
+    restore: (core, documentId) =>
+      restoreResourceDocument(core, store, config, documentId),
+    deletePermanently: (core, documentId) =>
+      permanentlyDeleteResourceDocument(core, store, config, documentId),
+    store: {
+      get: store.get,
+      getAll: store.getAll,
+      add: (core, document) =>
+        addResourceDocument(core, store, config, document),
+      set: (core, document) =>
+        setResourceDocument(core, store, config, document),
+      remove: (core, documentId) =>
+        removeResourceDocument(core, store, config, documentId),
+      load: (core, documents) =>
+        loadResourceDocuments(core, store, config, documents),
+      clear: () => clearResourceDocuments(store, config),
+    },
+  };
 }
