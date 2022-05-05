fec7040ec37f2bfb950daf2acc262fec7c1f0335
Oscar Coniel <oscar@coniel.com>
2022-05-05 14:49:49 +0300

    Add package exports

---
+++ packages/resources/src/index.ts
@@ -0,0 +1,6 @@
+export * from './createConfigsStore';
+export * from './createResource';
+export * from './createResourceStore';
+export * from './createTypedResource';
+export * from './errors';
+export * from './types';
