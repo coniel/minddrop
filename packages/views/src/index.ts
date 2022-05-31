a5b173079811c9d5f9f66d242473693de8118266
coniel <oscar@coniel.com>
2022-05-31 05:10:34 +0300

    Export ViewInstances

--- packages/views/src/index.ts
+++ packages/views/src/index.ts
@@ -1,6 +1,7 @@
 export * from './types';
 export * from './views-extension';
 export * from './Views';
+export * from './ViewInstances';
 export * from './ViewInstancesResource';
 export * from './errors';
 export * as VIEWS_TEST_DATA from './test-utils/views.data';
