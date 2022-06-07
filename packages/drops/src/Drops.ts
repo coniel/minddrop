040a46283a5dab22b3489cd2490ae1ee5e7b1b15
coniel <oscar@coniel.com>
2022-06-07 05:50:16 +0300

    Add useDrop and useDrops hooks

--- packages/drops/src/Drops.ts
+++ packages/drops/src/Drops.ts
@@ -1,4 +1,4 @@
-import { DropsApi } from './types';
+import { DropsApi, DropTypeData } from './types';
 import { createFromDataInsert } from './createFromDataInsert';
 import { duplicateDrops } from './duplicateDrops';
 import { DropsResource } from './DropsResource';
@@ -12,3 +12,24 @@ export const Drops: DropsApi = {
   removeEventListener: (core, event, callback) =>
     core.removeEventListener(event, callback),
 };
+
+/**
+ * Returns a drop by ID or `null` if the
+ * drop does not exist.
+ *
+ * @param dropId - The ID of the drop to retrieve.
+ * @returns A drop or `null` if it does not exist.
+ */
+export const useDrop = <TTypeData extends DropTypeData = {}>(dropId: string) =>
+  DropsResource.hooks.useDocument<TTypeData>(dropId);
+
+/**
+ * Returns a `{ [id]: Drop }` drops by ID,
+ * omitting any drops that do not exist.
+ *
+ * @param dropIds - The IDs of the drops to retrieve.
+ * @returns A `{ [id]: Drop }` maps of the requested drops.
+ */
+export const useDrops = <TTypeData extends DropTypeData = {}>(
+  dropIds: string[],
+) => DropsResource.hooks.useDocuments<TTypeData>(dropIds);
