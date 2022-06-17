66e43fa3338d4fd7fd66afb1e39455e6a54838a6
coniel <oscar@coniel.com>
2022-06-17 18:21:42 +0300

    Switch metadata fetching library
    Replace metadata-scraper with @devmehq/open-graph-extractor

--- packages/backend-utils/src/getWebpageMetadata/getWebpageMetadata.test.ts
+++ packages/backend-utils/src/getWebpageMetadata/getWebpageMetadata.test.ts
@@ -1,22 +1,54 @@
-import getMetadata from 'metadata-scraper';
+import { extractOpenGraph } from '@devmehq/open-graph-extractor';
+import axios from 'axios';
 import { getWebpageMetadata } from './getWebpageMetadata';
 
-jest.mock('metadata-scraper', () => jest.fn().mockReturnValue('metadata'));
+jest.mock('axios', () => ({
+  get: jest.fn().mockReturnValue({ data: 'HTML' }),
+}));
+
+jest.mock('@devmehq/open-graph-extractor', () => ({
+  extractOpenGraph: jest.fn().mockReturnValue({
+    ogTitle: 'IB Guides',
+    ogDescription: 'OG description',
+    description: 'Description',
+  }),
+}));
 
 describe('getWebpageMetadata', () => {
-  it('calls getMetadata with the URL', () => {
+  it('gets the webpage HTML', () => {
+    // Get metadata for a URL
+    getWebpageMetadata('https://ibguides.com');
+
+    // Should call 'axios.get' with the URL
+    expect(axios.get).toHaveBeenCalledWith(
+      'https://ibguides.com',
+      // Options passes to the call
+      expect.anything(),
+    );
+  });
+
+  it('calls extractOpenGraph with the webpage data', () => {
     // Get metadata for a URL
     getWebpageMetadata('https://ibguides.com');
 
     // Should call getMetadata with the URL
-    expect(getMetadata).toHaveBeenCalledWith('https://ibguides.com');
+    expect(extractOpenGraph).toHaveBeenCalledWith(
+      'HTML',
+      // Options passes to the call
+      expect.anything(),
+    );
   });
 
-  it('returns a promise resolving to the result of getMetadata', async () => {
+  it('returns the metadata', async () => {
     // Get metadata for a URL
     const metadata = await getWebpageMetadata('https://ibguides.com');
 
-    // Should call getMetadata with the URL
-    expect(metadata).toBe('metadata');
+    // Should return metadata
+    expect(metadata).toEqual({
+      domain: 'ibguides.com',
+      title: 'IB Guides',
+      // Should default to description over ogDescription
+      description: 'Description',
+    });
   });
 });
--- packages/backend-utils/src/getWebpageMetadata/getWebpageMetadata.ts
+++ packages/backend-utils/src/getWebpageMetadata/getWebpageMetadata.ts
@@ -1,5 +1,7 @@
-import getMetadata from 'metadata-scraper';
 import { WebpageMetadata } from '@minddrop/utils';
+import axios from 'axios';
+import { extractOpenGraph } from '@devmehq/open-graph-extractor';
+import { getDomain } from 'tldts';
 
 /**
  * Retrieves metada data for a webpage.
@@ -7,6 +9,43 @@ import { WebpageMetadata } from '@minddrop/utils';
  * @param url - The webpage URL.
  * @returns The webpage metadata.
  */
-export function getWebpageMetadata(url: string): Promise<WebpageMetadata> {
-  return getMetadata(url);
+export async function getWebpageMetadata(
+  url: string,
+): Promise<WebpageMetadata> {
+  // Get the URL's domain
+  const domain = getDomain(url);
+
+  // Default metadata consists of the domain and the
+  // title, which defaults to the domain.
+  const defaultMetadata = {
+    domain,
+    title: domain,
+  };
+
+  try {
+    // Get the webpage HTML
+    const { data: html } = await axios.get(url, { timeout: 5000 });
+
+    // Extract metadata from the HTML
+    const metadata = await extractOpenGraph(html, {
+      customMetaTags: [
+        { multiple: false, property: 'description', fieldName: 'description' },
+      ],
+    });
+
+    // Return the metadata or default metadata if metadata
+    // could not be extracted.
+    return metadata
+      ? {
+          domain,
+          title: metadata.ogTitle || domain,
+          // Prefer meta description over OG description
+          description: metadata.description || metadata.ogDescription,
+          image: metadata.ogImage ? metadata.ogImage.url : undefined,
+        }
+      : defaultMetadata;
+  } catch (error) {
+    // Return default metadata in case of failure
+    return defaultMetadata;
+  }
 }
