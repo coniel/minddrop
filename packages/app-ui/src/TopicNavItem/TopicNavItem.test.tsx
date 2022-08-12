fd8c54b76f78f0dbb15c899bd70961e6494f00fe
coniel <oscar@coniel.com>
2022-08-12 03:59:19 +0300

    Make topics nav items sortable

--- packages/app-ui/src/TopicNavItem/TopicNavItem.test.tsx
+++ packages/app-ui/src/TopicNavItem/TopicNavItem.test.tsx
@@ -7,11 +7,13 @@ import {
   fireEvent,
   waitFor,
 } from '@minddrop/test-utils';
-import { TopicNavItem } from './TopicNavItem';
-import { setup, cleanup, core } from '../test-utils';
 import { i18n } from '@minddrop/i18n';
 import { App } from '@minddrop/app';
 import { Topics, TOPICS_TEST_DATA } from '@minddrop/topics';
+import { DROPS_TEST_DATA } from '@minddrop/drops';
+import { Selection } from '@minddrop/selection';
+import { TopicNavItem } from './TopicNavItem';
+import { setup, cleanup, core } from '../test-utils';
 
 const {
   tSailing,
@@ -21,7 +23,28 @@ const {
   tCoastalNavigationView,
   tCoastalNavigation,
   trail,
+  tUntitled,
+  rootTopicIds,
 } = TOPICS_TEST_DATA;
+const { drop1 } = DROPS_TEST_DATA;
+
+const dataTransferEvent = {
+  preventDefault: jest.fn(),
+  dataTransfer: {
+    types: [],
+    files: [],
+    data: {},
+    getData: (type: string) => dataTransferEvent.dataTransfer.data[type],
+    setData: (type: string, value: string) => {
+      // Add the type to the types list
+      dataTransferEvent.dataTransfer.types.push(type);
+      // Add the data
+      dataTransferEvent.dataTransfer.data[type] = value;
+    },
+  },
+};
+
+const dragEvent = dataTransferEvent as unknown as DragEvent;
 
 describe('<TopicNavItem />', () => {
   beforeEach(setup);
@@ -29,6 +52,10 @@ describe('<TopicNavItem />', () => {
   afterEach(() => {
     cleanupRender();
     cleanup();
+
+    // Reset the drag event
+    dataTransferEvent.dataTransfer.data = [];
+    dataTransferEvent.dataTransfer.types = [];
   });
 
   it('renders the title', () => {
@@ -123,5 +150,329 @@ describe('<TopicNavItem />', () => {
     render(<TopicNavItem trail={[tSailing.id]} />);
 
     screen.getByRole('button', { current: true });
+  });
+
+  describe('top drop target', () => {
+    it('displayed when dragging topics', () => {
+      const { getByTestId } = render(<TopicNavItem trail={[tSailing.id]} />);
+
+      // Should not be visible by default
+      expect(getByTestId('top-drop-target')).not.toHaveClass('displayed');
+
+      act(() => {
+        // Select a topic
+        Selection.select(core, [Selection.item(tUntitled)]);
+        // Initialize a drag event
+        Selection.dragStart(core, dragEvent, 'sort');
+      });
+
+      // Should be visible
+      expect(getByTestId('top-drop-target')).toHaveClass('displayed');
+
+      act(() => {
+        // End the drag event
+        Selection.dragEnd(core, dragEvent);
+      });
+
+      // Should no longer be visible
+      expect(getByTestId('top-drop-target')).not.toHaveClass('displayed');
+    });
+
+    it('not displayed when dragging topics with other items', () => {
+      const { getByTestId } = render(<TopicNavItem trail={[tSailing.id]} />);
+
+      act(() => {
+        // Select a topic and a drop
+        Selection.select(core, [
+          Selection.item(tUntitled),
+          Selection.item(drop1),
+        ]);
+        // Initialize a drag event
+        Selection.dragStart(core, dragEvent, 'sort');
+      });
+
+      // Should not be visible
+      expect(getByTestId('top-drop-target')).not.toHaveClass('displayed');
+    });
+
+    it('not displayed when dragging a topic onto iteslf', () => {
+      const { getByTestId } = render(<TopicNavItem trail={[tSailing.id]} />);
+
+      act(() => {
+        // Select the topic itself
+        Selection.select(core, [Selection.item(tSailing)]);
+        // Initialize a drag event
+        Selection.dragStart(core, dragEvent, 'sort');
+      });
+
+      // Should not be visible
+      expect(getByTestId('top-drop-target')).not.toHaveClass('displayed');
+    });
+
+    it('not displayed when dragging a parent topic over a descendant', () => {
+      // Render a deeply nested subtopic
+      const { getByTestId } = render(
+        <TopicNavItem
+          trail={[tSailing.id, tNavigation.id, tCoastalNavigation.id]}
+        />,
+      );
+
+      act(() => {
+        // Select the topic's grandparent topic
+        Selection.select(core, [Selection.item(tSailing)]);
+        // Initialize a drag event
+        Selection.dragStart(core, dragEvent, 'sort');
+      });
+
+      // Should not be visible
+      expect(getByTestId('top-drop-target')).not.toHaveClass('displayed');
+    });
+
+    it('has `over` styling when dragging over', () => {
+      const { getByTestId } = render(<TopicNavItem trail={[tSailing.id]} />);
+
+      act(() => {
+        // Select a topic
+        Selection.select(core, [Selection.item(tUntitled)]);
+        // Initialize a drag event
+        Selection.dragStart(core, dragEvent, 'sort');
+      });
+
+      // Should not have 'over' styling by default
+      expect(getByTestId('top-drop-target')).not.toHaveClass('over');
+
+      act(() => {
+        // Drag enter the top drop target
+        fireEvent.dragEnter(getByTestId('top-drop-target'));
+      });
+
+      // Should have 'over' styling
+      expect(getByTestId('top-drop-target')).toHaveClass('over');
+
+      act(() => {
+        // Drag leave the top drop target
+        fireEvent.dragLeave(getByTestId('top-drop-target'));
+      });
+
+      // Should not have 'over' styling
+      expect(getByTestId('top-drop-target')).not.toHaveClass('over');
+    });
+
+    it('does nothing if the dropped data does not exclusively consist of topics', () => {
+      const { getByTestId } = render(
+        <TopicNavItem trail={[rootTopicIds[0]]} />,
+      );
+
+      act(() => {
+        // Select a topic and a drop
+        Selection.select(core, [
+          Selection.item(tUntitled),
+          Selection.item(drop1),
+        ]);
+        // Initialize a drag event
+        Selection.dragStart(core, dragEvent, 'sort');
+      });
+
+      act(() => {
+        // Drop onto the top drop target
+        fireEvent.drop(getByTestId('top-drop-target'), dragEvent);
+      });
+
+      // Root topics should not have changed
+      expect(App.getRootTopics().map((topic) => topic.id)).toEqual(
+        rootTopicIds,
+      );
+    });
+
+    describe('root level topic', () => {
+      it('sorts dropped root topics', () => {
+        // Use the first root topic as the drop target and the
+        // second and third root topics as the ones being dragged.
+        const [targetTopic, draggedTopic1, draggedTopic2] = rootTopicIds;
+
+        const { getByTestId } = render(<TopicNavItem trail={[targetTopic]} />);
+
+        act(() => {
+          // Select the two dragged root topics
+          Selection.select(core, [
+            Selection.item(Topics.get(draggedTopic1)),
+            Selection.item(Topics.get(draggedTopic2)),
+          ]);
+          // Initialize a drag event
+          Selection.dragStart(core, dragEvent, 'sort');
+        });
+
+        act(() => {
+          // Drop onto the top drop target
+          fireEvent.drop(getByTestId('top-drop-target'), dragEvent);
+        });
+
+        // Get the updated root topic IDs
+        const sortedRootTopics = App.getRootTopics().map((topic) => topic.id);
+
+        // Dragged topics should be above target topic
+        expect(sortedRootTopics[0]).toBe(draggedTopic1);
+        expect(sortedRootTopics[1]).toBe(draggedTopic2);
+        expect(sortedRootTopics[2]).toBe(targetTopic);
+      });
+
+      it('moves dropped subtopics to the root level', () => {
+        // Use the first root topic as the drop target
+        const [targetTopic] = rootTopicIds;
+        // Use subtopics as the dragged topics
+        const draggedTopic1 = tNavigation;
+        const draggedTopic2 = tBoats;
+
+        const { getByTestId } = render(<TopicNavItem trail={[targetTopic]} />);
+
+        act(() => {
+          // Select the two dragged root topics
+          Selection.select(core, [
+            Selection.item(draggedTopic1, draggedTopic1.parents[0]),
+            Selection.item(draggedTopic2, draggedTopic2.parents[0]),
+          ]);
+          // Initialize a drag event
+          Selection.dragStart(core, dragEvent, 'sort');
+        });
+
+        act(() => {
+          // Drop onto the top drop target
+          fireEvent.drop(getByTestId('top-drop-target'), dragEvent);
+        });
+
+        // Get an updated dragged topics
+        const updatedDraggedTopic = Topics.get(draggedTopic1.id);
+        // Get the updated root topic IDs
+        const updatedRootTopicIds = App.getRootTopics().map(
+          (topic) => topic.id,
+        );
+
+        // Removes the dragged topics from their parent topic
+        expect(updatedDraggedTopic.parents).toEqual([]);
+        // Adds the dragged topics to the root level at the drop point
+        expect(updatedRootTopicIds).toEqual([
+          draggedTopic1.id,
+          draggedTopic2.id,
+          ...rootTopicIds,
+        ]);
+      });
+    });
+
+    describe('subtopic', () => {
+      it('sorts dropped subtopics from the same parent', () => {
+        // Use the first subtopic as the drop target and the
+        // second and third subtopics as the ones being dragged.
+        const [targetTopic, draggedTopic1, draggedTopic2] = tSailing.subtopics;
+
+        const { getByTestId } = render(
+          <TopicNavItem trail={[tSailing.id, targetTopic]} />,
+        );
+
+        act(() => {
+          // Select the two dragged subtopics
+          Selection.select(core, [
+            Selection.item(Topics.get(draggedTopic1), tSailing),
+            Selection.item(Topics.get(draggedTopic2), tSailing),
+          ]);
+          // Initialize a drag event
+          Selection.dragStart(core, dragEvent, 'sort');
+        });
+
+        act(() => {
+          // Drop onto the top drop target
+          fireEvent.drop(getByTestId('top-drop-target'), dragEvent);
+        });
+
+        // Get the updated parent topic
+        const parent = Topics.get(tSailing.id);
+
+        // Dragged topics should be above target topic
+        expect(parent.subtopics[0]).toBe(draggedTopic1);
+        expect(parent.subtopics[1]).toBe(draggedTopic2);
+        expect(parent.subtopics[2]).toBe(targetTopic);
+      });
+
+      it("moves subtopics from other parents into target's parent", () => {
+        // The subtopics to move
+        const [draggedTopic1, draggedTopic2] = tNavigation.subtopics;
+        // The target subtopic
+        const [targetTopic] = tSailing.subtopics;
+
+        const { getByTestId } = render(
+          <TopicNavItem trail={[tSailing.id, targetTopic]} />,
+        );
+
+        act(() => {
+          // Select the two dragged subtopics
+          Selection.select(core, [
+            Selection.item(Topics.get(draggedTopic1), tNavigation),
+            Selection.item(Topics.get(draggedTopic2), tNavigation),
+          ]);
+          // Initialize a drag event
+          Selection.dragStart(core, dragEvent, 'sort');
+        });
+
+        act(() => {
+          // Drop onto the top drop target
+          fireEvent.drop(getByTestId('top-drop-target'), dragEvent);
+        });
+
+        // Get the updated source parent topic
+        const sourceParent = Topics.get(tNavigation.id);
+        // Get the updated target parent topic
+        const targetParent = Topics.get(tSailing.id);
+
+        // Dragged topics should be removed from source parent
+        expect(sourceParent.subtopics.includes(draggedTopic1)).toBeFalsy();
+        // Dragged topics should be added above target topic
+        expect(targetParent.subtopics).toEqual([
+          draggedTopic1,
+          draggedTopic2,
+          ...tSailing.subtopics,
+        ]);
+      });
+
+      it("moves root topics into target's parent", () => {
+        // The root topics to move
+        const [draggedTopic1, draggedTopic2] = rootTopicIds.filter(
+          (id) => id !== tSailing.id,
+        );
+        // The target subtopic
+        const [targetTopic] = tSailing.subtopics;
+
+        const { getByTestId } = render(
+          <TopicNavItem trail={[tSailing.id, targetTopic]} />,
+        );
+
+        act(() => {
+          // Select the two dragged root topics
+          Selection.select(core, [
+            Selection.item(Topics.get(draggedTopic1)),
+            Selection.item(Topics.get(draggedTopic2)),
+          ]);
+          // Initialize a drag event
+          Selection.dragStart(core, dragEvent, 'sort');
+        });
+
+        act(() => {
+          // Drop onto the top drop target
+          fireEvent.drop(getByTestId('top-drop-target'), dragEvent);
+        });
+
+        // Get the updated root topic IDs
+        const rootTopics = App.getRootTopics().map((topic) => topic.id);
+        // Get the updated target parent topic
+        const targetParent = Topics.get(tSailing.id);
+
+        // Dragged topics should be removed from the root level
+        expect(rootTopics.includes(draggedTopic1)).toBeFalsy();
+        // Dragged topics should be added above target topic
+        expect(targetParent.subtopics).toEqual([
+          draggedTopic1,
+          draggedTopic2,
+          ...tSailing.subtopics,
+        ]);
+      });
+    });
   });
 });
--- packages/app-ui/src/TopicNavItem/TopicNavItem.tsx
+++ packages/app-ui/src/TopicNavItem/TopicNavItem.tsx
@@ -7,20 +7,33 @@ import {
   ContextMenuContent,
   Popover,
   PopoverAnchor,
+  Text,
 } from '@minddrop/ui';
+import { useTranslation } from '@minddrop/i18n';
 import { App } from '@minddrop/app';
-import { Topic, useTopic, TopicViewInstanceData } from '@minddrop/topics';
-import { useAppCore, useCurrentView } from '@minddrop/app';
 import {
+  Selection,
+  SelectionItem,
+  useSelection,
   useDraggable,
-  useSelectionContains,
   useIsDragging,
 } from '@minddrop/selection';
+import {
+  Topic,
+  Topics,
+  useTopic,
+  TopicViewInstanceData,
+} from '@minddrop/topics';
+import { useAppCore, useCurrentView } from '@minddrop/app';
 import {
   LocalPersistentStore,
   useLocalPersistentStoreValue,
 } from '@minddrop/persistent-store';
-import { FieldValue, mapPropsToClasses } from '@minddrop/utils';
+import {
+  FieldValue,
+  mapPropsToClasses,
+  createDataInsertFromDataTransfer,
+} from '@minddrop/utils';
 import { RenameTopicPopover } from '../RenameTopicPopover';
 import { TopicMenu } from '../TopicMenu';
 import './TopicNavItem.css';
@@ -33,41 +46,76 @@ export interface TopicNavItemProps
   trail: string[];
 }
 
-export const TopicNavItem: FC<TopicNavItemProps> = ({ trail, ...other }) => {
+export const TopicNavItem: FC<TopicNavItemProps> = ({
+  trail,
+  level = 0,
+  ...other
+}) => {
   const core = useAppCore();
+  const { t } = useTranslation();
   // The topic ID is the last ID in the trail
   const topicId = useMemo(() => trail.slice(-1)[0], [trail]);
+  // Get the topic
   const topic = useTopic(topicId);
-  const [dragOver, setDragOver] = useState<'top' | 'topic' | null>(null);
+  // Check whether topics are being dragged
+  const isDraggingTopics = useIsDragging('topics:topic', true);
+  // Get selected topics
+  const selection = useSelection('topics:topic');
+  // Get the current view instance
+  const { instance } = useCurrentView<TopicViewInstanceData>();
+  // Initialize drag and drop callbacks
   const { onDragStart, onDragEnd } = useDraggable({
     id: topicId,
     resource: 'topics:topic',
+    parent:
+      trail.length > 1
+        ? {
+            resource: 'topics:topic',
+            id: trail.slice(-2)[0],
+          }
+        : undefined,
   });
-  const isDragging = useIsDragging();
-  const selectionContainsTopics = useSelectionContains('topics:topic');
-  const { instance } = useCurrentView<TopicViewInstanceData>();
+  // The location at which a topic selection is
+  // being dragged over this topic.
+  const [dragOver, setDragOver] = useState<'top' | 'topic' | null>(null);
+  // The open state of the renaming popover
+  const [renamePopoverOpen, setRenamePopoverOpen] = useState(false);
+  // Get the list of expanded topics from the local
+  // persistent store.
   const expandedTopics = useLocalPersistentStoreValue<string[]>(
     core,
     'expandedTopics',
     [],
   );
+  // Whether or not a view for this topic is currently open
   const isActive = useMemo(() => {
     if (!instance) {
+      // There is no view instance open
       return false;
     }
 
+    // Check whether the open view instance's topic ID is
+    // that of this topic.
     return instance.topic === topicId;
   }, [instance, topicId]);
-  const [renamePopoverOpen, setRenamePopoverOpen] = useState(false);
+  // Whether or not the topic itself is being dragged
+  const isDraggingSelf = !!selection.find((item) => item.id === topicId);
+  // Whether the dragged topics includes a parent of this topic
+  const isDraggingParent = Topics.isDescendant(
+    topicId,
+    selection.map((item) => item.id),
+  );
 
   useEffect(() => {
-    if (!isDragging || !selectionContainsTopics) {
+    if (isDraggingTopics) {
+      // Reset the drag over location state when drag ends
       setDragOver(null);
     }
-  }, [isDragging, selectionContainsTopics]);
+  }, [isDraggingTopics]);
 
   const handleExpandedChange = useCallback(
     (expanded) => {
+      // Save the expanded state to the local persistent store
       LocalPersistentStore.set(
         core,
         'expandedTopics',
@@ -116,19 +164,177 @@ export const TopicNavItem: FC<TopicNavItemProps> = ({ trail, ...other }) => {
     event.stopPropagation();
   };
 
+  const removeDraggedSubtopicsFromParents = (
+    draggedTopics: SelectionItem[],
+  ): string[] => {
+    // The IDs of the subtopics which were removed
+    // from their parent.
+    const affectedTopics: string[] = [];
+
+    // Group subtopics by parent topic
+    const parentSubtopics = draggedTopics.reduce((map, topicItem) => {
+      // If the topic has not parent or the parent is not
+      // another topic, ignore it.
+      if (!topicItem.parent || topicItem.parent.resource !== 'topics:topic') {
+        return map;
+      }
+
+      // Add the topic ID to the list of affected topics
+      affectedTopics.push(topicItem.id);
+
+      const existingGroup = map[topicItem.parent.id];
+
+      return {
+        ...map,
+        [topicItem.parent.id]: existingGroup
+          ? [...existingGroup, topicItem.id]
+          : [topicItem.id],
+      };
+    }, {});
+
+    // Remove the subtopics from each parent topic
+    Object.keys(parentSubtopics).forEach((parentId) => {
+      Topics.removeSubtopics(core, parentId, parentSubtopics[parentId]);
+    });
+
+    return affectedTopics;
+  };
+
+  const handleRootTopicDropTopTarget = (draggedTopics: SelectionItem[]) => {
+    // Get the topic IDs
+    const draggedTopicIds = draggedTopics.map((item) => item.id);
+
+    // Get the root topic IDs
+    const rootTopicIds = App.getRootTopics().map((topic) => topic.id);
+
+    // Check if the drag was initiated from the root level
+    // or from within a parent topic.
+    const containsOnlyRootTopics = draggedTopics.reduce(
+      (rootOnly, draggedTopic) => (!draggedTopic.parent ? rootOnly : false),
+      true,
+    );
+
+    // Drag event contains only root level topics
+    if (containsOnlyRootTopics) {
+      // Remove the dragged topics from the list of root topics
+      const sorted = rootTopicIds.filter((id) => !draggedTopicIds.includes(id));
+      // Add the dragged topics back in to the list of root topics
+      // above the dropped location.
+      sorted.splice(sorted.indexOf(topicId), 0, ...draggedTopicIds);
+      // Save the sorted root topics
+      App.sortRootTopics(core, sorted);
+      // Stop here
+      return;
+    }
+
+    // Remove the dragged subtopics from their parent topics
+    const moveToRoot = removeDraggedSubtopicsFromParents(draggedTopics);
+
+    // Add the topics to the root level
+    App.addRootTopics(core, moveToRoot, rootTopicIds.indexOf(topicId));
+  };
+
+  const handleSubtopicDropTopTarget = (draggedTopics: SelectionItem[]) => {
+    // Get the parent topic ID
+    const parentId = trail.slice(-2)[0];
+    // Get the parent topic
+    const parent = Topics.get(parentId);
+    // Get the topic IDs
+    const draggedTopicIds = draggedTopics.map((item) => item.id);
+
+    // Check if the drag was initiated from within the topic's
+    // subtopics.
+    const containsOnlySubtopics = draggedTopics.reduce(
+      (subtopicOnly, draggedTopic) =>
+        // Has a parent
+        draggedTopic.parent &&
+        // Parent is a topic
+        draggedTopic.parent.resource === 'topics:topic' &&
+        // Parent ID is equal to target parent ID
+        draggedTopic.parent.id === parentId
+          ? subtopicOnly
+          : false,
+      true,
+    );
+
+    // Drag event contains only this topic's subtopics
+    if (containsOnlySubtopics) {
+      // Remove the dragged topics from the list of subtopics
+      const sorted = parent.subtopics.filter(
+        (id) => !draggedTopicIds.includes(id),
+      );
+      // Add the dragged topics back in to the list of subtopics
+      // above the dropped location.
+      sorted.splice(sorted.indexOf(topicId), 0, ...draggedTopicIds);
+      // Save the sorted subtopics
+      Topics.sortSubtopics(core, parentId, sorted);
+      // Stop here
+      return;
+    }
+
+    // Remove the dragged subtopics from their parent topics
+    const moveToParent = removeDraggedSubtopicsFromParents(draggedTopics);
+
+    // Add the dragged topics to the parent topic
+    Topics.addSubtopics(
+      core,
+      parentId,
+      moveToParent,
+      parent.subtopics.indexOf(topicId),
+    );
+
+    // Get dragged root level topic IDs
+    const draggedRootTopics = draggedTopics
+      .filter((item) => !item.parent)
+      .map((item) => item.id);
+
+    App.moveRootTopicsToParentTopic(
+      core,
+      parentId,
+      draggedRootTopics,
+      parent.subtopics.indexOf(topicId),
+    );
+  };
+
+  const handleDropTopTarget = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setDragOver(null);
+
+    // Get selection from the event data transfer
+    const selection = Selection.getFromDataInsert(
+      createDataInsertFromDataTransfer(event.dataTransfer),
+    );
+    // Get topics from the selection
+    const draggedTopics = Selection.filter(selection, 'topics:topic');
+
+    if (selection.length !== draggedTopics.length) {
+      // If the selection is not exclusively topics, stop here
+      return;
+    }
+
+    if (trail.length === 1) {
+      // Topic is a root level topic
+      handleRootTopicDropTopTarget(draggedTopics);
+    } else {
+      handleSubtopicDropTopTarget(draggedTopics);
+    }
+  };
+
   return (
     <div className="topic-nav-item-container">
       <div
         className={mapPropsToClasses(
           {
             over: dragOver === 'top',
-            displayed: isDragging && selectionContainsTopics,
+            displayed: isDraggingTopics && !isDraggingSelf && !isDraggingParent,
           },
           'top-drop-target',
         )}
         onDragEnter={handleDragEnterTopTarget}
         onDragOver={handleDragOver}
         onDragLeave={handleDragLeave}
+        onDrop={handleDropTopTarget}
+        data-testid="top-drop-target"
       />
       <Popover open={renamePopoverOpen} onOpenChange={null}>
         <ContextMenu>
@@ -151,6 +357,16 @@ export const TopicNavItem: FC<TopicNavItemProps> = ({ trail, ...other }) => {
                     trail={[...trail, subtopicId]}
                   />
                 ))}
+                {topic.subtopics.length === 0 && (
+                  <Text
+                    as="div"
+                    color="light"
+                    className="helper-text"
+                    style={{ paddingLeft: level * 16 }}
+                  >
+                    {t('noSubtopics')}
+                  </Text>
+                )}
               </TopicNavItemPrimitive>
             </PopoverAnchor>
           </ContextMenuTrigger>
