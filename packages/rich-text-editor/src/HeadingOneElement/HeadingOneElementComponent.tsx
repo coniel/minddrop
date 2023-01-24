9a162e3b5ea75cd76ba7818a5242d1ea52f50778
coniel <oscar@coniel.com>
2023-01-24 22:07:32 +0200

    Add placeholder text to existing elements

--- packages/rich-text-editor/src/HeadingOneElement/HeadingOneElementComponent.tsx
+++ packages/rich-text-editor/src/HeadingOneElement/HeadingOneElementComponent.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { ElementPlaceholderText } from '../ElementPlaceholderText';
 import { RTElementProps } from '../types';
 import './HeadingOneElementComponent.css';
 
 export const HeadingOneElementComponent: React.FC<RTElementProps> = ({
   children,
   attributes,
+  element,
 }) => (
   <h1 className="heading-1-element" {...attributes}>
+    <ElementPlaceholderText element={element} text="Heading 1" />
     {children}
   </h1>
 );
--- packages/rich-text-editor/src/ParagraphElement/ParagraphElementComponent.tsx
+++ packages/rich-text-editor/src/ParagraphElement/ParagraphElementComponent.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { ElementPlaceholderText } from '../ElementPlaceholderText';
 import { RTElementProps } from '../types';
 import './ParagraphElementComponent.css';
 
 export const ParagraphElementComponent: React.FC<RTElementProps> = ({
   children,
   attributes,
+  element,
 }) => (
   <p className="paragraph-element" {...attributes}>
+    <ElementPlaceholderText
+      onlyWhenFocused
+      element={element}
+      text="Type '/' for commands"
+    />
     {children}
   </p>
 );
