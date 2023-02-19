02c0ed9f038434cc04ace3a17f2ab3a553def29a
coniel <oscar@coniel.com>
2023-02-19 21:46:58 +0200

    Drop support for multiple trigger characters

--- packages/rich-text-editor/src/types/RTTriggerCharacterConfig.types.ts
+++ packages/rich-text-editor/src/types/RTTriggerCharacterConfig.types.ts
@@ -2,12 +2,12 @@ import React from 'react';
 
 export interface RTTriggerCharacterConfig {
   /**
-   * The characters which trigger the action.
+   * The character which trigger the action.
    */
-  characters: string[];
+  character: string;
 
   /**
-   * The callback fired when a charater is typed
+   * The callback fired when the trigger charater is typed
    * into the editor.
    */
   action: (event: React.KeyboardEvent<HTMLDivElement>) => void;
