6d9ab2c0bcbdd5fd02a8baf7db53aff448596482
coniel <oscar@coniel.com>
2023-02-23 20:59:46 +0200

    Setup ui package testing

---
+++ packages/ui/Button.test.tsx
@@ -0,0 +1,9 @@
+import { it } from "vitest";
+import { render } from "@testing-library/react";
+import { Button } from "./Button";
+
+it("renders", () => {
+  const { getByText } = render(<Button />);
+
+  getByText("Boop");
+});
