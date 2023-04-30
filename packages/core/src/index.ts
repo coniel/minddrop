95c1851f5d4f3ccadc30cd25f395fd04bc19cb5e
coniel <oscar@coniel.com>
2023-04-30 23:43:53 +0300

    Export errors

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,7 @@
 export * from './constants';
 export * from './CoreProvider';
 export * from './createItemStore';
+export * from './errors';
 export * from './initializeCore';
 export * from './PersistentConfigsStore';
 export * from './FileSystem';
