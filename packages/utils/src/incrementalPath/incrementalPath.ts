3a94c0272091fef4b2d45d6014ffb81ba770a5e5
coniel <oscar@coniel.com>
2023-05-01 02:39:31 +0300

    Add incrementalPath util

---
+++ packages/utils/src/incrementalPath/incrementalPath.ts
@@ -0,0 +1,35 @@
+import { Fs } from '@minddrop/core';
+
+/**
+ * Adds a numerix suffix to the given file path if the file
+ * path already exists.
+ */
+export async function incrementalPath(
+  path: string,
+  increment?: number,
+): Promise<string> {
+  const filename = path.split('/').slice(-1)[0];
+  const extension = filename.includes('.')
+    ? `.${filename.split('.').slice(-1)[0]}`
+    : '';
+  const pathWithoutExtension = path.slice(0, path.length - extension.length);
+
+  // Create the incremented file name by adding the increment
+  // suffix in between the filename and file extension.
+  let incrementedPath = increment
+    ? `${pathWithoutExtension} ${increment}${extension}`
+    : path;
+
+  const exists = await Fs.exists(incrementedPath);
+
+  // If the path exists, recursively increment the suffix
+  // until reaching a unique path.
+  if (exists) {
+    incrementedPath = await incrementalPath(
+      path,
+      increment ? increment + 1 : 1,
+    );
+  }
+
+  return incrementedPath;
+}
