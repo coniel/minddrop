32993d23bfb49a9fe10612c3b73abde9b2963c8d
coniel <oscar@coniel.com>
2023-07-01 21:28:43 +0300

    Set up storybook

---
+++ apps/storybook/.storybook/main.js
@@ -0,0 +1,18 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ['../../../packages/ui/**/*.stories.@(ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-dark-mode',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
