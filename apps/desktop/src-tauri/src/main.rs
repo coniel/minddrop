a54c71d079e142bf29fca7a89836e2a3b4fe10f0
coniel <oscar@coniel.com>
2023-07-02 23:46:11 +0300

    Configure Mac OS window

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -3,15 +3,38 @@
     windows_subsystem = "windows"
 )]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+#[cfg(target_os = "macos")]
+#[macro_use]
+extern crate objc;
+
+use tauri::{Manager, WindowEvent};
+use window_ext::WindowExt;
+
+mod window_ext;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let win = app.get_window("main").unwrap();
+            win.set_transparent_titlebar(true);
+            win.position_traffic_lights(14.0, 16.0);
+            Ok(())
+        })
+        .on_window_event(|e| {
+
+            let apply_offset = || {
+                let win = e.window();
+                win.position_traffic_lights(14.0, 16.0);
+                
+            };
+
+            match e.event() {
+                WindowEvent::Resized(..) => apply_offset(),
+                WindowEvent::ThemeChanged(..) => apply_offset(),
+                _ => {}
+            }
+
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
