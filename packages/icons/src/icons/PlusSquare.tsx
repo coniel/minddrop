eea17c146677166eca278c61690ad2dc46ec2f1d
coniel <oscar@coniel.com>
2023-07-16 23:34:09 +0300

    Add icons

---
+++ packages/icons/src/icons/PlusSquare.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+
+export const PlusSquare: FC<React.HTMLAttributes<SVGElement>> = (props) => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M18 3H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3zm1 15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1z"
+      fill="inherit"
+    />
+    <path
+      d="M15 11h-2V9a1 1 0 0 0-2 0v2H9a1 1 0 0 0 0 2h2v2a1 1 0 0 0 2 0v-2h2a1 1 0 0 0 0-2z"
+      fill="inherit"
+    />
+  </svg>
+);
