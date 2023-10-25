48d56e8d27fed1a1a68b0a0f034afc0cd6a732f2
coniel <oscar@coniel.com>
2023-10-25 23:53:04 +0300

    Export mdast types

--- packages/markdown/src/types/markdown.types.ts
+++ packages/markdown/src/types/markdown.types.ts
@@ -1,1 +1,28 @@
-export type { RootContent } from 'mdast';
+import type * as mdast from 'mdast';
+
+export type {
+  RootContent,
+  PhrasingContent,
+  ListItem as MdastListItem,
+} from 'mdast';
+
+export type MdastBuilder<T extends string> = (
+  node: T extends mdast.Content['type']
+    ? Extract<
+        mdast.Content,
+        {
+          type: T;
+        }
+      >
+    : unknown,
+  next: (children: any[]) => any,
+) => object | undefined;
+
+export type OverridedMdastBuilders = {
+  [key in mdast.Content['type']]?: MdastBuilder<key>;
+} & (
+  | {
+      [key: string]: MdastBuilder<typeof key>;
+    }
+  | {}
+);
