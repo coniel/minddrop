15724776fcb2277960d3106d8a90d168accd30e8
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-10-31 20:18:45 +0200

    feat(workspaces): export workspaces test data

--- packages/workspaces/src/index.ts
+++ packages/workspaces/src/index.ts
@@ -3,3 +3,4 @@ export * from './types';
 export * from './Workspaces';
 export * from './useWorkspaces';
 export * from './useWorkspace';
+export * as WORKSPACES_TEST_DATA from './test-utils/workspaces.data';
