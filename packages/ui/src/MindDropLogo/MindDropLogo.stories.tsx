b0cc21ff025b78b653285557a7273c304fe313d7
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-11-02 00:43:10 +0200

    feat(ui): add MindDropLogo component

---
+++ packages/ui/src/MindDropLogo/MindDropLogo.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { MindDropLogo } from './MindDropLogo';
+
+export default {
+  title: 'ui/MindDropLogo',
+  component: MindDropLogo,
+};
+
+export const Default: React.FC = () => (
+  <div>
+    <MindDropLogo />
+  </div>
+);
