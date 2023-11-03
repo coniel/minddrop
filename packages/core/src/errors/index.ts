227b4bfecbff46b1a2003024eba7b6e343831277
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-11-03 23:11:59 +0200

    feat(ui): add additional error types

--- packages/core/src/errors/index.ts
+++ packages/core/src/errors/index.ts
@@ -2,3 +2,5 @@ export * from './FileNotFoundError';
 export * from './InvalidParameterError';
 export * from './JsonParseError';
 export * from './NotFoundError';
+export * from './InvalidPathError';
+export * from './PathConflictError';
