ec57fc73245e37392d9c0b2d7d5d9bd07d663aa1
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-11-04 02:13:14 +0200

    fix(ui): add missing Dialog sub component exports

--- packages/ui/src/Dialog/index.ts
+++ packages/ui/src/Dialog/index.ts
@@ -1,1 +1,6 @@
 export * from './Dialog';
+export * from './DialogClose';
+export * from './DialogContent';
+export * from './DialogDescription';
+export * from './DialogTitle';
+export * from './DialogTrigger';
