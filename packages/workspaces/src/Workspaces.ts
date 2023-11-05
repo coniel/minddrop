e98347ab7a566b475023578ee4e61e162a70a592
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-11-05 18:32:03 +0200

    refactor(workspaces): get rid of WorkspacesApi type

--- packages/workspaces/src/Workspaces.ts
+++ packages/workspaces/src/Workspaces.ts
@@ -1,32 +1,3 @@
-import { WorkspacesApi } from './types';
-import { addWorkspace } from './addWorkspace';
-import { createWorkspace } from './createWorkspace';
-import { getWorkspaces } from './getWorkspaces';
-import { getWorkspacesConfig } from './getWorkspacesConfig';
-import { hasValidWorkspace } from './hasValidWorkspace';
-import { hasWorkspacesConfig } from './hasWorkspacesConfig';
-import { loadWorkspaces } from './loadWorkspaces';
-import { workspaceExists } from './workspaceExists';
-import { WorkspacesStore } from './WorkspacesStore';
-import { getWorkspace } from './getWorkspace';
-import { removeWorkspace } from './removeWorkspace';
-import { deleteWorkspace } from './deleteWorkspace';
-import { moveWorkspace } from './moveWorkspace';
-import { renameWorkspace } from './renameWorkspace';
+import * as WorkspacesApi from './WorkspacesApi';
 
-export const Workspaces: WorkspacesApi = {
-  load: loadWorkspaces,
-  add: addWorkspace,
-  create: createWorkspace,
-  getAll: getWorkspaces,
-  get: getWorkspace,
-  remove: removeWorkspace,
-  delete: deleteWorkspace,
-  move: moveWorkspace,
-  rename: renameWorkspace,
-  getConfig: getWorkspacesConfig,
-  hasConfig: hasWorkspacesConfig,
-  hasValidWorkspace: hasValidWorkspace,
-  exists: workspaceExists,
-  _clear: () => WorkspacesStore.getState().clear(),
-};
+export const Workspaces = WorkspacesApi;
