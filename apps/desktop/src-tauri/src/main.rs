7592aad30a4922570fe867ee8594434236b83956
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-11-05 23:12:25 +0200

    feat(apps/desktop): add show_in_folder tauri command

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -37,7 +37,7 @@ fn main() {
             }
 
         })
-        .invoke_handler(tauri::generate_handler![move_to_trash])
+        .invoke_handler(tauri::generate_handler![move_to_trash, show_in_folder])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -47,4 +47,57 @@ fn move_to_trash(path: String) -> Result<String, String> {
     trash::delete(path).map_err(|err| err.to_string())?;
 
     Ok("Deleted".to_string())
+}
+
+#[cfg(target_os = "linux")]
+use std::{fs::metadata, path::PathBuf};
+use std::process::Command;
+#[cfg(target_os = "linux")]
+use fork::{daemon, Fork}; // dep: fork = "0.1"
+
+#[tauri::command]
+fn show_in_folder(path: String) {
+  #[cfg(target_os = "windows")]
+  {
+    Command::new("explorer")
+        .args(["/select,", &path]) // The comma after select is not a typo
+        .spawn()
+        .unwrap();
+  }
+
+  #[cfg(target_os = "linux")]
+  {
+    if path.contains(",") {
+      // see https://gitlab.freedesktop.org/dbus/dbus/-/issues/76
+      let new_path = match metadata(&path).unwrap().is_dir() {
+        true => path,
+        false => {
+          let mut path2 = PathBuf::from(path);
+          path2.pop();
+          path2.into_os_string().into_string().unwrap()
+        }
+      };
+      Command::new("xdg-open")
+          .arg(&new_path)
+          .spawn()
+          .unwrap();
+    } else {
+      if let Ok(Fork::Child) = daemon(false, false) {
+        Command::new("dbus-send")
+            .args(["--session", "--dest=org.freedesktop.FileManager1", "--type=method_call",
+                  "/org/freedesktop/FileManager1", "org.freedesktop.FileManager1.ShowItems",
+                  format!("array:string:\"file://{path}\"").as_str(), "string:\"\""])
+            .spawn()
+            .unwrap();
+      }
+    }
+  }
+
+  #[cfg(target_os = "macos")]
+  {
+    Command::new("open")
+        .args(["-R", &path])
+        .spawn()
+        .unwrap();
+  }
 }
