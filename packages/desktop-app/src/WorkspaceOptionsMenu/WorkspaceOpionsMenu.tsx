37ec7b30140c401f6c089bb62812b2c4e793b690
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-11-11 03:57:48 +0200

    feat(desktop-app): create WorkspaceOptionsMenu component

---
+++ packages/desktop-app/src/WorkspaceOptionsMenu/WorkspaceOpionsMenu.tsx
@@ -0,0 +1,116 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuProps,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@minddrop/ui';
+import { useTranslation } from '@minddrop/i18n';
+import './WorkspaceOptionsMenu.css';
+
+export interface WorkspaceOptionsMenuProps extends DropdownMenuProps {
+  /**
+   * The workspace path.
+   */
+  path: string;
+
+  /**
+   * The menu trigger button.
+   */
+  children: React.ReactNode;
+
+  /**
+   * Callback fired when the rename option is selected.
+   */
+  onSelectRename?(event: Event): void;
+
+  /**
+   * Callback fired when the change icon option is selected.
+   */
+  onSelectChangeIcon?(event: Event): void;
+
+  /**
+   * Callback fired when the move option is selected.
+   */
+  onSelectMove?(event: Event): void;
+
+  /**
+   * Callback fired when the remove option is selected.
+   */
+  onSelectRemove?(event: Event): void;
+
+  /**
+   * Callback fired when the delete option is selected.
+   */
+  onSelectDelete?(event: Event): void;
+
+  /**
+   * Callback fired when the reveal in file explorer
+   * option is selected.
+   */
+  onSelectRevealInFileExplorer?(event: Event): void;
+}
+
+export const WorkspaceOptionsMenu: React.FC<WorkspaceOptionsMenuProps> = ({
+  path,
+  children,
+  onSelectRename,
+  onSelectChangeIcon,
+  onSelectMove,
+  onSelectRemove,
+  onSelectDelete,
+  onSelectRevealInFileExplorer,
+  ...other
+}) => {
+  const { t } = useTranslation('workspaces.actions');
+
+  return (
+    <DropdownMenu {...other}>
+      <DropdownMenuTrigger>{children}</DropdownMenuTrigger>
+      <DropdownMenuPortal>
+        <DropdownMenuContent align="start" className="workspace-options-menu">
+          <DropdownMenuItem
+            icon="edit"
+            label={t('rename.action')}
+            onSelect={onSelectRename}
+          />
+          <DropdownMenuItem
+            icon="star"
+            label={t('changeIcon')}
+            onSelect={onSelectChangeIcon}
+          />
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            icon="corner-down-right"
+            label={t('move.action')}
+            tooltipTitle={t('move.tooltip.title')}
+            tooltipDescription={t('move.tooltip.description')}
+            onSelect={onSelectMove}
+          />
+          <DropdownMenuItem
+            icon="close-circle"
+            label={t('remove.action')}
+            tooltipTitle={t('remove.tooltip.title')}
+            tooltipDescription={t('remove.tooltip.description')}
+            onSelect={onSelectRemove}
+          />
+          <DropdownMenuItem
+            icon="trash"
+            label={t('delete.action')}
+            tooltipTitle={t('delete.tooltip.title')}
+            tooltipDescription={t('delete.tooltip.description')}
+            onSelect={onSelectDelete}
+          />
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            icon="folder"
+            label={t('revealInFileExplorer.macos')}
+            onSelect={onSelectRevealInFileExplorer}
+          />
+        </DropdownMenuContent>
+      </DropdownMenuPortal>
+    </DropdownMenu>
+  );
+};
---
+++ packages/desktop-app/src/WorkspaceOptionsMenu/WorkspaceOptionsMenu.test.tsx
@@ -0,0 +1,96 @@
+import { describe, beforeEach, afterEach, it, expect, vi } from 'vitest';
+import { cleanup } from '../test-utils';
+import { render, userEvent } from '@minddrop/test-utils';
+import {
+  WorkspaceOptionsMenu,
+  WorkspaceOptionsMenuProps,
+} from './WorkspaceOpionsMenu';
+
+const WORKSPACE_PATH = 'path/to/Workspace';
+
+const renderMenu = (props?: Partial<WorkspaceOptionsMenuProps>) => {
+  return render(
+    <WorkspaceOptionsMenu open path={WORKSPACE_PATH} {...props}>
+      <div />
+    </WorkspaceOptionsMenu>,
+    { translationKeyPrefix: 'workspaces.actions' },
+  );
+};
+
+describe('<WorkspaceOptionsMenu />', () => {
+  afterEach(cleanup);
+
+  it('calls onSelectRename', async () => {
+    const onSelectRename = vi.fn();
+
+    const { getByTranslatedText } = renderMenu({ onSelectRename });
+
+    // Click the rename menu item
+    await userEvent.click(getByTranslatedText('rename.action'));
+
+    // Should call the onSelectRename callback
+    expect(onSelectRename).toHaveBeenCalled();
+  });
+
+  it('calls onSelectChangeIcon', async () => {
+    const onSelectChangeIcon = vi.fn();
+
+    const { getByTranslatedText } = renderMenu({ onSelectChangeIcon });
+
+    // Click the change menu item
+    await userEvent.click(getByTranslatedText('changeIcon'));
+
+    // Should call the onSelectChangeIcon callback
+    expect(onSelectChangeIcon).toHaveBeenCalled();
+  });
+
+  it('calls onSelectMove', async () => {
+    const onSelectMove = vi.fn();
+
+    const { getByTranslatedText } = renderMenu({ onSelectMove });
+
+    // Click the move menu item
+    await userEvent.click(getByTranslatedText('move.action'));
+
+    // Should call the onSelectMove callback
+    expect(onSelectMove).toHaveBeenCalled();
+  });
+
+  it('calls onSelectRemove', async () => {
+    const onSelectRemove = vi.fn();
+
+    const { getByTranslatedText } = renderMenu({ onSelectRemove });
+
+    // Click the remove menu item
+    await userEvent.click(getByTranslatedText('remove.action'));
+
+    // Should call the onSelectRemove callback
+    expect(onSelectRemove).toHaveBeenCalled();
+  });
+
+  it('calls onSelectDelete', async () => {
+    const onSelectDelete = vi.fn();
+
+    const { getByTranslatedText } = renderMenu({ onSelectDelete });
+
+    // Click the delete menu item
+    await userEvent.click(getByTranslatedText('delete.action'));
+
+    // Should call the onSelectDelete callback
+    expect(onSelectDelete).toHaveBeenCalled();
+  });
+
+  it('calls onSelectRevealInFileExplorer', async () => {
+    const onSelectRevealInFileExplorer = vi.fn();
+
+    const { getByTranslatedText } = renderMenu({
+      onSelectRevealInFileExplorer,
+    });
+
+    // Click the reveal in file explorer menu item
+    await userEvent.click(getByTranslatedText('revealInFileExplorer.macos'));
+
+    // Should call the onSelectRevealInFileExplorer callback
+    expect(onSelectRevealInFileExplorer).toHaveBeenCalled();
+  });
+});
