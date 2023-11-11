c6c8c10682bef312552c6dc78ffd7b28d72f5f88
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-11-12 00:28:16 +0200

    feat(workspaces): add writeWorkspaceConfig function

--- packages/workspaces/src/test-utils/setup-tests.ts
+++ packages/workspaces/src/test-utils/setup-tests.ts
@@ -1,11 +1,14 @@
 import { initializeCore } from '@minddrop/core';
+import { vi } from 'vitest';
 import { WorkspacesStore } from '../WorkspacesStore';
+import { workspace1 } from './workspaces.data';
 
 export const core = initializeCore({ extensionId: 'minddrop:workspaces' });
 
 export function setup() {}
 
 export function cleanup() {
+  vi.clearAllMocks();
   core.removeAllEventListeners();
   WorkspacesStore.getState().clear();
 }
