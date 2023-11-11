ffc5ac9cdaf437b2d130e2e72de44cc7748edaf5
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-11-11 04:09:48 +0200

    feat(desktop-app): add promptMoveWorkspace function

---
+++ packages/desktop-app/src/api/promptMoveWorkspace/promptMoveWorkspace.test.ts
@@ -0,0 +1,39 @@
+import { describe, beforeEach, afterEach, it, expect, vi } from 'vitest';
+import { promptMoveWorkspace } from './promptMoveWorkspace';
+import { Workspaces } from '@minddrop/workspaces';
+
+const WORKSPACE_PATH = 'path/to/Workspace';
+
+let openResponse: string | null;
+
+vi.mock('@tauri-apps/api/dialog', () => ({
+  open: () => openResponse,
+}));
+
+describe('promptMoveWorkspace', () => {
+  it('moves the workspace when a dir is selected', async () => {
+    vi.spyOn(Workspaces, 'move');
+
+    // Pretend dir was selected
+    openResponse = 'new/path';
+
+    // Run the prompt
+    await promptMoveWorkspace(WORKSPACE_PATH);
+
+    // Should move the workspace to the new path
+    expect(Workspaces.move).toHaveBeenCalledWith(WORKSPACE_PATH, openResponse);
+  });
+
+  it('does nothing if no dir was selected', async () => {
+    vi.spyOn(Workspaces, 'move');
+
+    // Pretend dir selection was canceled
+    openResponse = null;
+
+    // Run the prompt
+    await promptMoveWorkspace(WORKSPACE_PATH);
+
+    // Should not attempt to move the workspace
+    expect(Workspaces.move).not.toHaveBeenCalled();
+  });
+});
