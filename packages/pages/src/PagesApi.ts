6484be772f6ff5e957c959bbc9934581ec23ec66
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-11-12 15:42:26 +0200

    feat(pages): export isWrapped as an API method

--- packages/pages/src/PagesApi.ts
+++ packages/pages/src/PagesApi.ts
@@ -2,3 +2,4 @@ export { getPage as get } from './getPage';
 export { createPage as create } from './createPage';
 export { loadPages as load } from './loadPages';
 export { setPageIcon as setIcon } from './setPageIcon';
+export { isWrapped } from './utils';
