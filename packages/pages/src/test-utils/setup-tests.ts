239c8000802c5a5bb6b11af62777be2c05a81a18
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-11-12 08:32:33 +0200

    feat(pages): add Pages.create API method

--- packages/pages/src/test-utils/setup-tests.ts
+++ packages/pages/src/test-utils/setup-tests.ts
@@ -1,10 +1,14 @@
 import { vi } from 'vitest';
 import { PagesStore } from '../PagesStore';
+import { Events } from '@minddrop/events';
 
 export function setup() {}
 
 export function cleanup() {
   vi.clearAllMocks();
+
+  // Clear all event listeners
+  Events._clearAll();
 
   // Clear the pages store
   PagesStore.getState().clear();
