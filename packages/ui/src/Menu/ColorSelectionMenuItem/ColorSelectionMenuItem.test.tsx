f4f7383afc915082547326ca6fa8600467e28e30
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-11-12 04:23:39 +0200

    test(ui): fix ColorSelectionMenuItem test

--- packages/ui/src/Menu/ColorSelectionMenuItem/ColorSelectionMenuItem.test.tsx
+++ packages/ui/src/Menu/ColorSelectionMenuItem/ColorSelectionMenuItem.test.tsx
@@ -1,21 +1,21 @@
 import { describe, afterEach, it, expect } from 'vitest';
-import { render, cleanup, screen } from '@minddrop/test-utils';
+import { render, cleanup } from '@minddrop/test-utils';
 import { ColorSelectionMenuItem } from './ColorSelectionMenuItem';
-import { i18n } from '@minddrop/i18n';
 
 describe('<ColorSelectionMenuItem />', () => {
   afterEach(cleanup);
 
   it('renders the color name', () => {
-    render(<ColorSelectionMenuItem color="red" />);
+    const { getByTranslatedText } = render(
+      <ColorSelectionMenuItem color="red" />,
+    );
 
-    const colorName = i18n.t('colorRed');
-    screen.getByText(colorName);
+    getByTranslatedText('color.red');
   });
 
   it('renders the appropriate icon color', () => {
-    render(<ColorSelectionMenuItem color="red" />);
+    const { getByTestId } = render(<ColorSelectionMenuItem color="red" />);
 
-    expect(screen.getByTestId('icon').className).toContain('color-red');
+    expect(getByTestId('icon').className).toContain('color-red');
   });
 });
