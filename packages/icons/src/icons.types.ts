373a174d6ca3e34f1c41fd3d2c621e76cc419465
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-11-12 04:24:45 +0200

    feat(icons): add UserIcon types

--- packages/icons/src/icons.types.ts
+++ packages/icons/src/icons.types.ts
@@ -1,6 +1,8 @@
 import { UiIcons } from './ui-icons.min';
 import { ContentIcons } from './content-icons.min';
 import { HTMLProps } from 'react';
+import { EmojiSkinTone } from './emoji';
+import { ContentColor } from '@minddrop/core';
 
 export type UiIconName = keyof typeof UiIcons;
 export type ContentIconName = keyof typeof ContentIcons;
@@ -13,3 +15,25 @@ export type ContentIconSet = Record<
   ContentIconName,
   React.ComponentType<HTMLProps<SVGSVGElement>>
 >;
+
+export enum UserIconType {
+  Default = 'default',
+  Emoji = 'emoji',
+  ContentIcon = 'content-icon',
+}
+
+export type UserIconDefault = { type: UserIconType.Default };
+
+export type UserIconEmoji = {
+  type: UserIconType.Emoji;
+  icon: string;
+  skinTone: EmojiSkinTone;
+};
+
+export type UserIconContentIcon = {
+  type: UserIconType.ContentIcon;
+  icon: ContentIconName;
+  color: ContentColor;
+};
+
+export type UserIcon = UserIconDefault | UserIconEmoji | UserIconContentIcon;
