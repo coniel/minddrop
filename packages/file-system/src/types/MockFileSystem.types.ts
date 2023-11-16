cc4dcb6a75cf3e947af85f238a44b280af796e78
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-11-16 17:29:37 +0200

    feat(file-system): add support for file/dir options in mock FS

--- packages/file-system/src/types/MockFileSystem.types.ts
+++ packages/file-system/src/types/MockFileSystem.types.ts
@@ -1,21 +1,34 @@
 import { FileEntry } from './FileEntry.types';
 import { FileSystem } from './FileSystem.types';
+import { FsDirOptions } from './FsDirOptions.types';
+import { FsFileOptions } from './FsFileOptions.types';
 
 export interface MockFileSystem {
   MockFs: FileSystem;
-  clearMockFileSystem(): void;
-  resetMockFileSystem(): void;
+  clear(): void;
+  reset(): void;
   clearTrash(): void;
-  setFiles(files: MockFileDescriptor[]): void;
-  addFiles(files: MockFileDescriptor[]): void;
+  setFiles(files: (MockFileDescriptor | string)[]): void;
+  addFiles(files: (MockFileDescriptor | string)[]): void;
   getFiles(): FileEntry[];
   getTrash(): FileEntry[];
   printFiles(): void;
-  exists(path: string): boolean;
-  readTextFile(path: string): string;
-  writeTextFile(path: string, textContent: string): void;
+  exists(path: string, options?: FsFileOptions | FsDirOptions): boolean;
+  existsInTrash(path: string, options?: FsFileOptions | FsDirOptions): boolean;
+  readTextFile(path: string, options?: FsFileOptions | FsDirOptions): string;
+  writeTextFile(
+    path: string,
+    textContent: string,
+    options?: FsFileOptions | FsDirOptions,
+  ): void;
+  removeFile(path: string, options?: FsFileOptions | FsDirOptions): void;
+  removeDir(path: string, options?: FsFileOptions | FsDirOptions): void;
 }
 
 export type MockFsRoot = Record<string, FileEntry>;
 
-export type MockFileDescriptor = string | { path: string; textContent: string };
+export type MockFileDescriptor = {
+  path: string;
+  textContent?: string;
+  options?: FsFileOptions | FsDirOptions;
+};
