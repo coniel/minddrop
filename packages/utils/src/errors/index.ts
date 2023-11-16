b8483668b20e57ce2535e2f346439636b86e5bc0
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-11-16 17:40:04 +0200

    feat(utils): add JsonParseError error type

--- packages/utils/src/errors/index.ts
+++ packages/utils/src/errors/index.ts
@@ -2,4 +2,5 @@ export * from './AdapterNotRegisteredError';
 export * from './InvalidParameterError';
 export * from './InvalidSchemaError';
 export * from './InvalidValidatorError';
+export * from './JsonParseError';
 export * from './ValidationError';
