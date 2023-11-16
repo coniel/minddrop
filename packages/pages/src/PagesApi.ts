f6fbc76e56dbad1f7991c8c1f02530dacc6216a3
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-11-16 18:25:59 +0200

    feat(pages): add wrap pages API function

--- packages/pages/src/PagesApi.ts
+++ packages/pages/src/PagesApi.ts
@@ -2,4 +2,5 @@ export { getPage as get } from './getPage';
 export { createPage as create } from './createPage';
 export { loadPages as load } from './loadPages';
 export { setPageIcon as setIcon } from './setPageIcon';
+export { wrapPage as wrap } from './wrapPage';
 export { isWrapped } from './utils';
