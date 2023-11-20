340409a896ee04c416e58749c505af93907ae4da
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-11-20 23:44:32 +0200

    feat(pages): add Pages.rename function

--- packages/pages/src/PagesApi.ts
+++ packages/pages/src/PagesApi.ts
@@ -4,3 +4,4 @@ export { loadPages as load } from './loadPages';
 export { setPageIcon as setIcon } from './setPageIcon';
 export { wrapPage as wrap } from './wrapPage';
 export { isWrapped } from './utils';
+export { renamePage as rename } from './renamePage';
