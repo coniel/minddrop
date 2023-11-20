5eb301c2dff6339953ec6c75e2cbab8eacf5b4b5
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-11-21 01:40:54 +0200

    fix(pages): rename getChildPages function to match file name

--- packages/pages/src/utils/getChildPages/getChildPages.test.ts
+++ packages/pages/src/utils/getChildPages/getChildPages.test.ts
@@ -1,6 +1,6 @@
 import { describe, beforeEach, afterEach, it, expect } from 'vitest';
 import { setup, cleanup, page1 } from '../../test-utils';
-import { getPageChildren } from './getChildPages';
+import { getChildPages } from './getChildPages';
 import { Page } from '../../types';
 
 const PAGES: Page[] = [
@@ -17,14 +17,14 @@ describe('getPageChildren', () => {
   afterEach(cleanup);
 
   it('returns the children of a page', () => {
-    expect(getPageChildren(PAGES[0].path, PAGES)).toEqual([PAGES[1], PAGES[2]]);
+    expect(getChildPages(PAGES[0].path, PAGES)).toEqual([PAGES[1], PAGES[2]]);
   });
 
   it('returns empty array if page has no children', () => {
-    expect(getPageChildren(PAGES[4].path, PAGES)).toEqual([]);
+    expect(getChildPages(PAGES[4].path, PAGES)).toEqual([]);
   });
 
   it('returns the children of a directory', () => {
-    expect(getPageChildren('path/to', PAGES)).toEqual([PAGES[0], PAGES[4]]);
+    expect(getChildPages('path/to', PAGES)).toEqual([PAGES[0], PAGES[4]]);
   });
 });
