3a3dd175e1dd506d9cdf592df0b905681c8b69ae
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2023-12-03 01:04:23 +0200

    feat(ui): add ContentListItem component

---
+++ packages/ui/src/ContentListItem/ContentListItem.test.tsx
@@ -0,0 +1,52 @@
+import { describe, afterEach, it, expect, vi } from 'vitest';
+import { render, cleanup, screen, userEvent } from '@minddrop/test-utils';
+import { ContentListItem, ContentListItemProps } from './ContentListItem';
+
+function renderTest(props?: Partial<ContentListItemProps>) {
+  return render(<ContentListItem label="Test" {...props} />);
+}
+
+describe('<ContentListItem />', () => {
+  afterEach(cleanup);
+
+  it('renders the label', () => {
+    const { getByText } = renderTest({ label: 'Test' });
+
+    // Should render the label
+    getByText('Test');
+  });
+
+  it('renders the icon', () => {
+    const { getByText } = renderTest({ icon: 'A' });
+
+    // Should render the icon
+    getByText('A');
+  });
+
+  it('renders children collapsed', async () => {
+    const { getByText, getByTranslatedLabelText, queryByText } = renderTest({
+      children: 'Children',
+    });
+
+    // Should not render children initially
+    expect(queryByText('Children')).toBeNull();
+
+    // Click toggle icon to open chidlren
+    await userEvent.click(getByTranslatedLabelText('expand'));
+
+    // Should render the children
+    getByText('Children');
+  });
+
+  it('calls onClick when clicking the label', async () => {
+    const onClick = vi.fn();
+
+    const { getByText } = renderTest({ onClick });
+
+    // Click the label
+    await userEvent.click(getByText('Test'));
+
+    // Should call onClick
+    expect(onClick).toHaveBeenCalled();
+  });
+});
