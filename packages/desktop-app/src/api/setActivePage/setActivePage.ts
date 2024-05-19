cb590cceeb9f3ca7daa814f67adaed6254a79744
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2024-05-19 19:10:53 +0300

    chore: document setActivePage function

--- packages/desktop-app/src/api/setActivePage/setActivePage.ts
+++ packages/desktop-app/src/api/setActivePage/setActivePage.ts
@@ -1,7 +1,9 @@
 import { AppUiState } from '../../AppUiState';
 
 /**
- * Does something useful.
+ * Sets the given path as the active page.
+ *
+ * @param path The path to set as the active page.
  */
 export function setActivePage(path: string): void {
   AppUiState.set('view', 'page');
