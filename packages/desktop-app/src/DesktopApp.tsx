10f5c3d4ba65b88f7a73bd2da032573d70332903
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2024-05-21 19:20:12 +0300

    feat(desktop-app): create PageView

--- packages/desktop-app/src/DesktopApp.tsx
+++ packages/desktop-app/src/DesktopApp.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { IconsProvider } from '@minddrop/icons';
 import { CoreProvider } from '@minddrop/core';
 import { AppSidebar } from './components';
-import { useCurrentView } from './AppUiState';
+import { useCurrentPath, useCurrentView } from './AppUiState';
 import { ShowWindowOnRendered } from './utils';
+import { PageView } from './views';
 import './DesktopApp.css';
 
 export const DesktopApp: React.FC = () => {
   const view = useCurrentView();
+  const path = useCurrentPath();
 
   if (!view) {
     return null;
@@ -20,6 +22,7 @@ export const DesktopApp: React.FC = () => {
           <AppSidebar />
           <div className="app-content">
             <div data-tauri-drag-region className="app-drag-handle" />
+            {view === 'page' && path && <PageView path={path} />}
           </div>
         </div>
         <ShowWindowOnRendered />
