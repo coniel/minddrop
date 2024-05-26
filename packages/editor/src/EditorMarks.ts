4093cf4357d5e1cfb49d9e23b8a19cb1cc3d4f5b
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2024-05-27 00:58:27 +0300

    feat(editor): add registerDefaults to EditorMarks

--- packages/editor/src/EditorMarks.ts
+++ packages/editor/src/EditorMarks.ts
@@ -1,2 +1,3 @@
 export { registerMarkConfig as register } from './registerMarkConfig';
 export { unregisterMarkConfig as unregister } from './unregisterMarkConfig';
+export { registerDefaultMarks as registerDefaults } from './utils';
