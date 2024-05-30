79c138e0101ca0b02691b1e898e12da0f6afe02a
Oscar Coniel <oscar@Oscars-MacBook-Pro.local>
2024-05-30 23:08:16 +0300

    feat(editor)

--- packages/editor/src/utils/registerDefaulElements/registerDefaultElements.ts
+++ packages/editor/src/utils/registerDefaulElements/registerDefaultElements.ts
@@ -1,10 +1,16 @@
-import { HeadingOneElementConfig } from '../../default-element-configs/HeadingOneElement';
-import { ParagraphElementConfig } from '../../default-element-configs/ParagraphElement';
-import { ToDoElementConfig } from '../../default-element-configs/ToDoElement';
+import {
+  HeadingOneElementConfig,
+  HeadingThreeElementConfig,
+  HeadingTwoElementConfig,
+  ParagraphElementConfig,
+  ToDoElementConfig,
+} from '../../default-element-configs';
 import { registerElementConfig } from '../../registerElementConfig';
 
 const configs = [
   HeadingOneElementConfig,
+  HeadingTwoElementConfig,
+  HeadingThreeElementConfig,
   ParagraphElementConfig,
   ToDoElementConfig,
 ];
