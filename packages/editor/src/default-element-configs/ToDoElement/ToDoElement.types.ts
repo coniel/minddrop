944933d50c440f25ce4205c1d38baf63522d8f82
Oscar Coniel <oscar@coniel.com>
2024-06-04 23:18:04 +0300

    feat(editor): add nestingLevel property to ToDoElement

--- packages/editor/src/default-element-configs/ToDoElement/ToDoElement.types.ts
+++ packages/editor/src/default-element-configs/ToDoElement/ToDoElement.types.ts
@@ -5,6 +5,11 @@ export interface ToDoElementData {
    * `true` when the item is checked.
    */
   done: boolean;
+
+  /**
+   * The nesting level of the to-do item.
+   */
+  nestingLevel?: number;
 }
 
 export type ToDoElement = BlockElement<ToDoElementData>;
