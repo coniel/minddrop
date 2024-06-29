627fe296c7b858f7d7284ef5c49b3bd7e7a14ff1
Oscar Coniel <oscar@coniel.com>
2024-06-30 01:41:21 +0300

    feat(desktop-app): open new document path when wrapping open document

--- packages/desktop-app/src/DesktopApp.tsx
+++ packages/desktop-app/src/DesktopApp.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { IconsProvider } from '@minddrop/icons';
 import { DocumentView } from '@minddrop/documents';
+import { Events } from '@minddrop/events';
 import { AppSidebar } from './components';
 import { useCurrentPath, useCurrentView } from './AppUiState';
 import { ShowWindowOnRendered } from './utils';
+import { setActiveDocument } from './api';
 import './DesktopApp.css';
 
 export const DesktopApp: React.FC = () => {
   const view = useCurrentView();
   const path = useCurrentPath();
+
+  useEffect(() => {
+    Events.addListener(
+      'documents:document:wrap',
+      'reopen-document-view-on-wrapped',
+      ({ data }) => {
+        if (path === data.oldPath) {
+          setActiveDocument(data.newPath);
+        }
+      },
+    );
+
+    return () => {
+      Events.removeListener(
+        'documents:document:wrap',
+        'reopen-document-view-on-wrapped',
+      );
+    };
+  }, [path]);
 
   return (
     <IconsProvider>
