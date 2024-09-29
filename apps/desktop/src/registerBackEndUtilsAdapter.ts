b23d51fcd678e87cd740f9d474ec03c98d33be5c
Oscar Coniel <oscar@coniel.com>
2024-09-30 02:04:38 +0300

    feat(apps/desktop): add open back-end util

--- apps/desktop/src/registerBackEndUtilsAdapter.ts
+++ apps/desktop/src/registerBackEndUtilsAdapter.ts
@@ -1,7 +1,9 @@
 import { invoke } from '@tauri-apps/api/core';
+import { open } from '@tauri-apps/plugin-shell';
 import { registerBackendUtilsAdapter } from '@minddrop/utils';
 
 registerBackendUtilsAdapter({
+  open,
   getWebpageHtml: async (url: string) => {
     const result = await invoke<string>('webpage_html', {
       url,
