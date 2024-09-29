a8651467ecac6d22c5d4add87551e987519aa25a
Oscar Coniel <oscar@coniel.com>
2024-09-30 02:05:37 +0300

    feat(file-system): export getFileExtension helper

--- packages/file-system/src/FsApi.ts
+++ packages/file-system/src/FsApi.ts
@@ -4,3 +4,4 @@ export { parentDirPath } from './parentDirPath';
 export { getPathSlice as pathSlice } from './getPathSlice';
 export { getFileExtension as getExtension } from './getFileExtension';
 export { removeFileExtension as removeExtension } from './removeFileExtension';
+export { getFileExtension } from './getFileExtension';
