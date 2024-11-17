554e75e62f92730066c19405f17c342faa094358
Oscar Coniel <oscar@coniel.com>
2024-11-18 00:05:15 +0200

    feat(extensions/node-type-text): make text node selectable

--- extensions/node-type-text/src/TextNodeEditorRenderer/TextNodeEditorRenderer.tsx
+++ extensions/node-type-text/src/TextNodeEditorRenderer/TextNodeEditorRenderer.tsx
@@ -2,12 +2,22 @@ import { useCallback, useMemo } from 'react';
 import { Ast, BlockElement } from '@minddrop/ast';
 import { Block } from '@minddrop/blocks';
 import { RichTextEditor } from '@minddrop/editor';
-import { TextNodeRendererProps } from '@minddrop/extension';
+import { TextNodeRendererProps, useApi } from '@minddrop/extension';
+import './TextNodeEditorRenderer.css';
 
 export const TextNodeRenderer: React.FC<TextNodeRendererProps> = ({
   node,
   onChange,
 }) => {
+  const { Utils, Selection } = useApi();
+
+  const parentPath = Utils.useParentDir();
+  const id = `${parentPath}#${node.id}`;
+  const { selected, onClick } = Selection.useSelectable({
+    id,
+    getPlainTextContent: () => node.text,
+  });
+
   const initialContent = useMemo<BlockElement[]>(
     () => Ast.fromMarkdown(node.text),
     [],
@@ -21,12 +31,13 @@ export const TextNodeRenderer: React.FC<TextNodeRendererProps> = ({
   );
 
   return (
-    <Block className="text-node">
+    <Block className="text-node-editor-renderer" selected={selected}>
       <RichTextEditor
         key={node.id}
         initialValue={initialContent}
         onChangeDebounced={onEditorChange}
       />
+      <div className="drag-handle" onMouseDown={onClick} />
     </Block>
   );
 };
