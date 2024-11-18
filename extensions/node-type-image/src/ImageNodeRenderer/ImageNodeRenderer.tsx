9f0e4b7cb50451688f386ba396358c222415d553
Oscar Coniel <oscar@coniel.com>
2024-11-18 22:25:28 +0200

    feat(node-type-image): make nodes selectable and draggable

--- extensions/node-type-image/src/ImageNodeRenderer/ImageNodeRenderer.tsx
+++ extensions/node-type-image/src/ImageNodeRenderer/ImageNodeRenderer.tsx
@@ -1,17 +1,40 @@
-import { FileNodeRendererProps, useApi } from '@minddrop/extension';
+import {
+  FileNodeRendererProps,
+  SelectionItem,
+  useApi,
+} from '@minddrop/extension';
 import { useEffect, useMemo, useState } from 'react';
+import './ImageNodeRenderer.css';
 
 export const ImageNodeRenderer: React.FC<FileNodeRendererProps> = ({
   node,
 }) => {
-  const { Fs, Utils } = useApi();
+  const {
+    Fs,
+    Utils,
+    Selection,
+    Ui: { Block },
+  } = useApi();
   const parentDir = Utils.useParentDir();
+  // Path to the parent document
+  const parentPath = Utils.useParentDir();
+  // Path to the current node considering the parent path
+  // and node ID as a subpath.
+  const path = `${parentPath}#${node.id}`;
   const [imageAvailable, setImageAvailable] = useState(true);
 
   const src = useMemo(
     () => Fs.convertFileSrc(Fs.concatPath(parentDir, node.file)),
     [node.file, parentDir, Fs],
   );
+  const selectionItem: SelectionItem = useMemo(
+    () => ({ id: path, getPlainTextContent: () => node.file }),
+    [path, node.file],
+  );
+  // Make the node selectable
+  const { selected, onClick } = Selection.useSelectable(selectionItem);
+  // Make the node draggable
+  const { onDragStart } = Selection.useDraggable(selectionItem);
 
   // When adding a new image, it occasionally takes a moment
   // for the image to be available. This effect checks if the
@@ -59,10 +82,19 @@ export const ImageNodeRenderer: React.FC<FileNodeRendererProps> = ({
   }
 
   return (
-    <img
-      src={src}
-      alt={node.file}
-      style={{ maxWidth: '100%', maxHeight: '100%' }}
-    />
+    <Block
+      draggable
+      className="image-node-renderer"
+      selected={selected}
+      onDragStart={onDragStart}
+      onClick={onClick}
+    >
+      <img
+        src={src}
+        alt={node.file}
+        style={{ maxWidth: '100%', maxHeight: '100%' }}
+      />
+      <div className="drag-handle" />
+    </Block>
   );
 };
