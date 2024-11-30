29f6e78c7cf5423cdb1521bf65887932f06f8ee4
Oscar Coniel <oscar@coniel.com>
2024-12-01 01:42:23 +0200

    test(blocks): add additional fixtures and export fixtures

--- packages/blocks/src/index.ts
+++ packages/blocks/src/index.ts
@@ -1,3 +1,5 @@
 export * from './types';
 export * from './components';
 export * as Blocks from './Blocks';
+
+export * as BLOCKS_TEST_DATA from './test-utils/blocks.data';
--- packages/blocks/src/test-utils/blocks.data.ts
+++ packages/blocks/src/test-utils/blocks.data.ts
@@ -1,4 +1,33 @@
-import { Block } from '../types';
+import { Block, BlockType, BlockVariant, TextBlockClassifier } from '../types';
+
+export const textBlockConfig: BlockType = {
+  id: 'text',
+  defaultVariant: 'editor',
+  description: {
+    'en-US': {
+      name: 'Text',
+      details: 'A block of text.',
+    },
+  },
+};
+
+export const textBlockVariantConfig: BlockVariant = {
+  blockType: 'text',
+  id: 'editor',
+  component: () => null,
+  description: {
+    'en-US': {
+      name: 'Editor',
+    },
+  },
+};
+
+export const textBlockClassifier: TextBlockClassifier = {
+  id: 'text-classifier',
+  blockType: 'text',
+  match: () => true,
+  initializeData: (text: string) => ({ text }),
+};
 
 export const textBlock: Block = {
   id: 'block-1',
