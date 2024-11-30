75387cd054cc554563a59d615f6ca140586584c2
Oscar Coniel <oscar@coniel.com>
2024-11-30 18:15:07 +0200

    feat(board-view): initialize empty columns section when creating view

--- extensions/board-view/src/BoardView.ts
+++ extensions/board-view/src/BoardView.ts
@@ -1,13 +1,29 @@
 import { MindDropExtension } from '@minddrop/extension';
 import { BoardViewRenderer } from './components';
 import { removeBlocksFromBoard } from './removeBlocksFromBoard';
+import { BoardColumn, BoardColumnsSection } from './types';
 
 export const extension: MindDropExtension = {
-  initialize: async ({ Documents }) => {
+  initialize: async ({ Documents, Utils }) => {
     Documents.registerView({
       id: 'board',
       icon: 'kanban-square',
-      initialize: () => ({ sections: [] }),
+      initialize: () => {
+        const columns: BoardColumn[] = Array.from({ length: 3 }, () => ({
+          id: Utils.uuid(),
+          blocks: [],
+        }));
+
+        const section: BoardColumnsSection = {
+          id: Utils.uuid(),
+          type: 'columns',
+          columns,
+        };
+
+        return {
+          sections: [section],
+        };
+      },
       component: BoardViewRenderer,
       description: {
         'en-US': {
