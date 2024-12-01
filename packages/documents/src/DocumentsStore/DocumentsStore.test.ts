b56ed1a64149c6c96b6686f11482e1ececfe6d3f
Oscar Coniel <oscar@coniel.com>
2024-12-01 14:37:02 +0200

    feat(documents): map view and block parent document

--- packages/documents/src/DocumentsStore/DocumentsStore.test.ts
+++ packages/documents/src/DocumentsStore/DocumentsStore.test.ts
@@ -1,7 +1,16 @@
 import { describe, afterEach, it, expect, beforeEach } from 'vitest';
-import { document1, wrappedDocument, documents } from '../test-utils';
+import {
+  document1,
+  wrappedDocument,
+  documents,
+  document1Blocks,
+} from '../test-utils';
 import { Document } from '../types';
-import { DocumentsStore } from './DocumentsStore';
+import {
+  BlockDocumentMap,
+  DocumentViewDocumentMap,
+  DocumentsStore,
+} from './DocumentsStore';
 
 function loadDocuments() {
   DocumentsStore.getState().load(documents);
@@ -10,6 +19,7 @@ function loadDocuments() {
 describe('DocumentsStore', () => {
   afterEach(() => {
     DocumentsStore.getState().clear();
+    BlockDocumentMap.clear();
   });
 
   describe('load', () => {
@@ -24,6 +34,28 @@ describe('DocumentsStore', () => {
         document1,
         wrappedDocument,
       ]);
+    });
+
+    it('updates the BlockDocumentMap', () => {
+      // Load a document into the store
+      DocumentsStore.getState().load([document1]);
+
+      // BlockDocumentMap should contain entries for the
+      // document's blocks.
+      document1.blocks.forEach((blockId) => {
+        expect(BlockDocumentMap.get(blockId)).toBe(document1.id);
+      });
+    });
+
+    it('updates the DocumentViewDocumentMap', () => {
+      // Load a document into the store
+      DocumentsStore.getState().load([document1]);
+
+      // DocumentViewDocumentMap should contain an entry for the
+      // document's views.
+      document1.views.forEach((viewId) => {
+        expect(DocumentViewDocumentMap.get(viewId)).toBe(document1.id);
+      });
     });
   });
 
@@ -40,6 +72,28 @@ describe('DocumentsStore', () => {
         document1,
         wrappedDocument,
       ]);
+    });
+
+    it('updates the BlockDocumentMap', () => {
+      // Add a document to the store
+      DocumentsStore.getState().add(document1);
+
+      // BlockDocumentMap should contain entries for the
+      // document's blocks>
+      document1.blocks.forEach((blockId) => {
+        expect(BlockDocumentMap.get(blockId)).toBe(document1.id);
+      });
+    });
+
+    it('updates the DocumentViewDocumentMap', () => {
+      // Add a document to the store
+      DocumentsStore.getState().add(document1);
+
+      // DocumentViewDocumentMap should contain an entry for the
+      // document's views.
+      document1.views.forEach((viewId) => {
+        expect(DocumentViewDocumentMap.get(viewId)).toBe(document1.id);
+      });
     });
   });
 
@@ -73,6 +127,34 @@ describe('DocumentsStore', () => {
       // Documents state should remain unchanged
       expect(DocumentsStore.getState().documents).toEqual(initialState);
     });
+
+    it('updates the BlockDocumentMap', () => {
+      // Update a document
+      DocumentsStore.getState().update(document1.id, {
+        blocks: [document1Blocks[0].id, 'new-block'],
+      });
+
+      // Should remove removed blocks from the map
+      expect(BlockDocumentMap.get(document1Blocks[1].id)).toBeUndefined();
+      // Should add new blocks to the map
+      expect(BlockDocumentMap.get(document1Blocks[0].id)).toBe(document1.id);
+      expect(BlockDocumentMap.get('new-block')).toBe(document1.id);
+    });
+
+    it('updates the DocumentViewDocumentMap', () => {
+      // Update a document
+      DocumentsStore.getState().update(document1.id, {
+        views: [document1.views[0], 'new-view'],
+      });
+
+      // Should remove removed views from the map
+      expect(DocumentViewDocumentMap.get(document1.views[1])).toBeUndefined();
+      // Should add new views to the map
+      expect(DocumentViewDocumentMap.get(document1.views[0])).toBe(
+        document1.id,
+      );
+      expect(DocumentViewDocumentMap.get('new-view')).toBe(document1.id);
+    });
   });
 
   describe('remove', () => {
@@ -91,6 +173,28 @@ describe('DocumentsStore', () => {
           (document) => document.id === document1.id,
         ),
       ).toBeUndefined();
+    });
+
+    it('updates the BlockDocumentMap', () => {
+      // Remove a document
+      DocumentsStore.getState().remove(document1.id);
+
+      // BlockDocumentMap should no longer have entries
+      // for the removed document.
+      document1.blocks.forEach((blockId) => {
+        expect(BlockDocumentMap.get(blockId)).toBeUndefined();
+      });
+    });
+
+    it('updates the DocumentViewDocumentMap', () => {
+      // Remove a document
+      DocumentsStore.getState().remove(document1.id);
+
+      // DocumentViewDocumentMap should no longer have entries
+      // for the removed document.
+      document1.views.forEach((viewId) => {
+        expect(DocumentViewDocumentMap.get(viewId)).toBeUndefined();
+      });
     });
   });
 });
--- packages/documents/src/DocumentsStore/DocumentsStore.ts
+++ packages/documents/src/DocumentsStore/DocumentsStore.ts
@@ -37,10 +37,34 @@ export const DocumentsStore = create<DocumentsStore>()((set) => ({
   documents: [],
 
   load: (documents) =>
-    set((state) => ({ documents: [...state.documents, ...documents] })),
+    set((state) => {
+      documents.forEach((document) => {
+        // Add block entries to the BlockDocumentMap
+        document.blocks.forEach((blockId) => {
+          BlockDocumentMap.set(blockId, document.id);
+        });
+
+        // Add view entries to the DocumentViewDocumentMap
+        document.views.forEach((viewId) => {
+          DocumentViewDocumentMap.set(viewId, document.id);
+        });
+      });
+
+      return { documents: [...state.documents, ...documents] };
+    }),
 
   add: (document) =>
     set((state) => {
+      // Add block entries to the BlockDocumentMap
+      document.blocks.forEach((blockId) => {
+        BlockDocumentMap.set(blockId, document.id);
+      });
+
+      // Add view entries to the DocumentViewDocumentMap
+      document.views.forEach((viewId) => {
+        DocumentViewDocumentMap.set(viewId, document.id);
+      });
+
       return {
         documents: [...state.documents, document],
       };
@@ -55,17 +79,63 @@ export const DocumentsStore = create<DocumentsStore>()((set) => ({
         return {};
       }
 
-      documents[index] = { ...documents[index], ...data };
+      const oldDocument = documents[index];
+      const newDocument = { ...oldDocument, ...data };
+
+      // Remove old block entries from the BlockDocumentMap
+      oldDocument.blocks.forEach((blockId) => {
+        BlockDocumentMap.delete(blockId);
+      });
+
+      // Add new block entries to the BlockDocumentMap
+      newDocument.blocks.forEach((blockId) => {
+        BlockDocumentMap.set(blockId, id);
+      });
+
+      // Remove old view entries from the DocumentViewDocumentMap
+      oldDocument.views.forEach((viewId) => {
+        DocumentViewDocumentMap.delete(viewId);
+      });
+
+      // Add new view entries to the DocumentViewDocumentMap
+      newDocument.views.forEach((viewId) => {
+        DocumentViewDocumentMap.set(viewId, id);
+      });
+
+      documents[index] = newDocument;
 
       return { documents };
     }),
 
   remove: (id) =>
     set((state) => {
+      const document = state.documents.find((document) => document.id === id);
+
+      if (document) {
+        // Remove block entries from the BlockDocumentMap
+        document.blocks.forEach((blockId) => {
+          BlockDocumentMap.delete(blockId);
+        });
+
+        // Remove view entries from the DocumentViewDocumentMap
+        document.views.forEach((viewId) => {
+          DocumentViewDocumentMap.delete(viewId);
+        });
+      }
+
       return {
         documents: state.documents.filter((document) => id !== document.id),
       };
     }),
 
-  clear: () => set({ documents: [] }),
+  clear: () =>
+    set(() => {
+      BlockDocumentMap.clear();
+      DocumentViewDocumentMap.clear();
+
+      return { documents: [] };
+    }),
 }));
+
+export const BlockDocumentMap = new Map<string, string>();
+export const DocumentViewDocumentMap = new Map<string, string>();
