1b1c19cf8873486a9041617b6f23fd86c645eb22
Oscar Coniel <oscar@coniel.com>
2024-12-02 03:05:57 +0200

    feat(image-block): use useImageSrc hook to load image

--- extensions/image-block/src/Image/Image.tsx
+++ extensions/image-block/src/Image/Image.tsx
@@ -1,25 +1,25 @@
 import { BlockVariantProps, SelectionItem, useApi } from '@minddrop/extension';
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import './Image.css';
 
-export const ImageCard: React.FC<BlockVariantProps> = ({
-  block,
-  deleteBlock,
-}) => {
+export const ImageCard: React.FC<BlockVariantProps> = (props) => {
+  if (props.block.file) {
+    return <HasImageFile {...props} />;
+  }
+
+  return <div>No image file</div>;
+};
+
+const HasImageFile: React.FC<BlockVariantProps> = ({ block, deleteBlock }) => {
   const {
-    Fs,
+    Fs: { useImageSrc, concatPath },
     Selection,
     Ui: { BlockContainer },
     Utils,
   } = useApi();
   const parentDir = Utils.useParentDir();
-  const [imageAvailable, setImageAvailable] = useState(true);
+  const src = useImageSrc(concatPath(parentDir, block.file!));
 
-  const src = useMemo(
-    () =>
-      block.file ? Fs.convertFileSrc(Fs.concatPath(parentDir, block.file)) : '',
-    [block.file, parentDir, Fs],
-  );
   const selectionItem: SelectionItem = useMemo(
     () => ({
       id: block.id,
@@ -33,48 +33,7 @@ export const ImageCard: React.FC<BlockVariantProps> = ({
   // Make the block draggable
   const { onDragStart } = Selection.useDraggable(selectionItem);
 
-  // When adding a new image, it occasionally takes a moment
-  // for the image to be available. This effect checks if the
-  // image is available. If not, it will try again after a
-  // timeout.
-  useEffect(() => {
-    var tester = new Image();
-
-    function imageFound() {
-      setImageAvailable(true);
-
-      tester.removeEventListener('load', imageFound);
-      tester.removeEventListener('error', imageNotFound);
-    }
-
-    function imageNotFound() {
-      setImageAvailable(false);
-
-      setTimeout(() => {
-        setImageAvailable(true);
-        tester.removeEventListener('load', imageFound);
-        tester.removeEventListener('error', imageNotFound);
-      });
-    }
-
-    tester.addEventListener('load', imageFound);
-    tester.addEventListener('error', imageNotFound);
-
-    if (src) {
-      tester.src = src;
-    }
-
-    return () => {
-      tester.removeEventListener('load', imageFound);
-      tester.removeEventListener('error', imageNotFound);
-    };
-  }, [src]);
-
   if (!src) {
-    return null;
-  }
-
-  if (!imageAvailable) {
     return null;
   }
 
