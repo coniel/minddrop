2a9a8de4895bde958ceaab2f11115cbead974a98
Oscar Coniel <oscar@coniel.com>
2024-12-08 06:12:07 +0200

    fix(extensions): remove old export

--- packages/extensions/src/Ui.ts
+++ packages/extensions/src/Ui.ts
@@ -1,9 +1,7 @@
 import * as UiElements from '@minddrop/ui-elements';
-import { DocumentTitleField } from '@minddrop/documents';
 import * as UiDesktop from '@minddrop/ui-desktop';
 
 export const Ui = {
   ...UiElements,
   ...UiDesktop,
-  DocumentTitleField,
 };
