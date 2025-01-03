dfd4bdd558082a4b102b4452a7501a258a04464b
Oscar Coniel <oscar@coniel.com>
2025-01-03 22:22:05 +0200

    feat(ui-elements): export DialogPortal and DialogOverlay

--- packages/ui-elements/src/Dialog/Dialog.tsx
+++ packages/ui-elements/src/Dialog/Dialog.tsx
@@ -4,3 +4,5 @@ import './Dialog.css';
 export type DialogRootProps = DialogPrimitive.DialogProps;
 
 export const Dialog = DialogPrimitive.Root;
+export const DialogPortal = DialogPrimitive.Portal;
+export const DialogOverlay = DialogPrimitive.Overlay;
