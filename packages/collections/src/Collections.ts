900fcfc5d20586018a8e6eaf541affc1644f8e5a
Oscar Coniel <oscar@coniel.com>
2025-03-31 18:23:14 +0200

    feat(collections): add writeCollectionsConfig

--- packages/collections/src/Collections.ts
+++ packages/collections/src/Collections.ts
@@ -2,3 +2,4 @@ export { createCollection as create } from './createCollection';
 export { loadCollections as load } from './loadCollections';
 export { hasCollectionsConfig as hasConfig } from './hasCollectionsConfig';
 export { getCollectionsConfig as getConfig } from './getCollectionsConfig';
+export { writeCollectionsConfig as writeConfig } from './writeCollectionsConfig';
---
+++ packages/collections/src/writeCollectionsConfig/writeCollectionsConfig.test.ts
@@ -0,0 +1,45 @@
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+import { initializeMockFileSystem } from '@minddrop/file-system';
+import { CollectionsStore } from '../CollectionsStore';
+import { getCollectionsConfig } from '../getCollectionsConfig';
+import {
+  cleanup,
+  collectionsConfig,
+  itemsCollectionConfig,
+  setup,
+  workspcesConfigFileDescriptor,
+} from '../test-utils';
+import { writeCollectionsConfig } from './writeCollectionsConfig';
+
+const MockFs = initializeMockFileSystem([
+  // Collections config file
+  {
+    ...workspcesConfigFileDescriptor,
+    textContent: JSON.stringify({ ...collectionsConfig, paths: [] }),
+  },
+]);
+
+describe('writeCollectionsConfig', () => {
+  beforeEach(() => {
+    setup();
+
+    // Load collections into the store
+    CollectionsStore.getState().load([itemsCollectionConfig]);
+
+    // Reset mock file system
+    MockFs.reset();
+  });
+
+  afterEach(cleanup);
+
+  it('writes collection paths to the config file', async () => {
+    // Write current collections state to config file
+    await writeCollectionsConfig();
+
+    // Get collections config
+    const config = await getCollectionsConfig();
+
+    // Should contain collection paths
+    expect(config.paths).toEqual([itemsCollectionConfig.path]);
+  });
+});
