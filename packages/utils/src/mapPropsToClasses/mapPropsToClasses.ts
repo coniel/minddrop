b2bda9b6f37793c2bf0742ab68188fcad335415c
Oscar Coniel <oscar@coniel.com>
2025-03-31 17:40:53 +0200

    feat(utils): add support for number values in mapPropsToClasses

--- packages/utils/src/mapPropsToClasses/mapPropsToClasses.ts
+++ packages/utils/src/mapPropsToClasses/mapPropsToClasses.ts
@@ -4,21 +4,22 @@ import { toKebabCase } from '../toKebabCase';
  * Maps a component's props to class names.
  *
  * Boolean props are mapped as [key] if true, ignored if false.
- * String props are mapped as [key]-[value].
+ * String and number props are mapped as [key]-[value].
  * className prop is appended to the generated className.
  * rootClass is prepended to the generated className.
  * camelCase prop names are converted into kebab-case.
  */
 export function mapPropsToClasses(
-  props: Record<string, string | boolean | undefined>,
+  props: Record<string, string | number | boolean | undefined>,
   rootClass: string = '',
 ): string {
   let className = rootClass;
+  const { className: _, ...rest } = props;
 
-  Object.keys(props).forEach((key) => {
-    if (props[key] && key !== 'className') {
-      if (typeof props[key] === 'string') {
-        className = `${className} ${key}-${props[key]}`;
+  Object.entries(rest).forEach(([key, value]) => {
+    if (value || value === 0) {
+      if (typeof value === 'string' || typeof value === 'number') {
+        className = `${className} ${key}-${value}`;
       } else {
         className = `${className} ${toKebabCase(key)}`;
       }
