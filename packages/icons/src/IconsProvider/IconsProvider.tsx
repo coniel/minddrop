978771915c9ef99b4a983ef1d45d943f489cca2b
Oscar Coniel <oscar@coniel.com>
2025-04-04 19:59:11 +0200

    icons: add emoji skin tone to IconProvider

--- packages/icons/src/IconsProvider/IconsProvider.tsx
+++ packages/icons/src/IconsProvider/IconsProvider.tsx
@@ -2,16 +2,21 @@ import { createContext } from '@minddrop/utils';
 import { EvaIconSvg, EvaIconSvgProps } from '../EvaIconSvg';
 import { LucidIconSvgProps, LucideIconSvg } from '../LucideIconSvg';
 import { ContentIcons as ContentIconsChildren } from '../content-icons.min';
+import { EmojiSkinTone } from '../emoji';
 import { ContentIconSet, UiIconSet } from '../icons.types';
 import { UiIcons as UiIconsChildren } from '../ui-icons.min';
 
 export interface IconsProviderProps {
   children: React.ReactNode;
+  defaultEmojiSkinTone: EmojiSkinTone;
+  onDefaultEmojiSkinToneChange: (skinTone: EmojiSkinTone) => void;
 }
 
 export interface IconsProviderContext {
   UiIcons: UiIconSet;
   ContentIcons: ContentIconSet;
+  defaultEmojiSkinTone: EmojiSkinTone;
+  onDefaultEmojiSkinToneChange: (skinTone: EmojiSkinTone) => void;
 }
 
 const UiIcons = Object.entries(UiIconsChildren).reduce(
@@ -36,8 +41,21 @@ const ContentIcons = Object.entries(ContentIconsChildren).reduce(
 
 const [hook, Provider, Consumer] = createContext<IconsProviderContext>();
 
-export const IconsProvider: React.FC<IconsProviderProps> = ({ children }) => (
-  <Provider value={{ UiIcons, ContentIcons }}>{children}</Provider>
+export const IconsProvider: React.FC<IconsProviderProps> = ({
+  children,
+  defaultEmojiSkinTone,
+  onDefaultEmojiSkinToneChange,
+}) => (
+  <Provider
+    value={{
+      UiIcons,
+      ContentIcons,
+      defaultEmojiSkinTone,
+      onDefaultEmojiSkinToneChange,
+    }}
+  >
+    {children}
+  </Provider>
 );
 
 export const useIcons = hook;
