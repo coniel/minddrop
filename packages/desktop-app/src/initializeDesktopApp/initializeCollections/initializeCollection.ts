70f7c906153f28bf4a2f242e9a0fb3f8f611cef3
Oscar Coniel <oscar@coniel.com>
2025-04-14 23:26:08 +0300

    desktop-app: fix collection entries being loaded twice

--- packages/desktop-app/src/initializeDesktopApp/initializeCollections/initializeCollection.ts
+++ packages/desktop-app/src/initializeDesktopApp/initializeCollections/initializeCollection.ts
@@ -2,6 +2,8 @@ import { Collections } from '@minddrop/collections';
 import { FileNotFoundError } from '@minddrop/file-system';
 import { JsonParseError } from '@minddrop/utils';
 
+let ran = false;
+
 /**
  * Initializes collections by loading them from the collections
  * config file.
@@ -10,9 +12,23 @@ import { JsonParseError } from '@minddrop/utils';
  * config file will be re-initialized.
  */
 export async function initializeCollections() {
+  // Workaround for react strict mode causing the initialize function
+  // to be called twice.
+  if (ran) {
+    return;
+  }
+
+  ran = true;
+
   try {
     // Load collections from the collections config file
     await Collections.load();
+
+    const collections = Collections.getAll();
+
+    await Promise.all(
+      collections.map((collection) => Collections.loadEntries(collection.path)),
+    );
   } catch (error) {
     // Failed to load collections config file. Open the
     // 'create-first-collection' view which will re-initialize
