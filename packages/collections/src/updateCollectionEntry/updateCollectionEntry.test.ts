62118bf7b22b2a5665399e98504084390d5ef1f0
Oscar Coniel <oscar@coniel.com>
2025-04-16 02:02:24 +0300

    collections: add updateCollectionEntry

---
+++ packages/collections/src/updateCollectionEntry/updateCollectionEntry.test.ts
@@ -0,0 +1,119 @@
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+import { Events } from '@minddrop/events';
+import { CollectionEntriesStore } from '../CollectionEntriesStore';
+import { CollectionTypeConfigsStore } from '../CollectionTypeConfigsStore';
+import {
+  CollectionEntryNotFoundError,
+  CollectionNotFoundError,
+} from '../errors';
+import {
+  cleanup,
+  itemsCollection,
+  itemsEntry1,
+  markdownCollectionTypeConfig,
+  setup,
+} from '../test-utils';
+import { CollectionTypeConfig } from '../types';
+import { updateCollectionEntry } from './updateCollectionEntry';
+
+const collectionTypeConfig: CollectionTypeConfig = {
+  ...markdownCollectionTypeConfig,
+  updateEntry: async (_, entry) => ({
+    ...entry,
+    metadata: {
+      ...entry.metadata,
+      updated: true,
+    },
+  }),
+};
+
+const update = { properties: { foo: 'bar' } };
+const updatedEntry = {
+  ...itemsEntry1,
+  properties: update.properties,
+  metadata: {
+    ...itemsEntry1.metadata,
+    lastModified: expect.any(Date),
+    updated: true,
+  },
+};
+
+describe('updateCollectionEntry', () => {
+  beforeEach(() => {
+    setup({
+      loadCollections: true,
+      loadCollectionEntries: true,
+    });
+
+    CollectionTypeConfigsStore.add(collectionTypeConfig);
+  });
+
+  afterEach(cleanup);
+
+  it('throws if the collection does not exist', () => {
+    expect(() =>
+      updateCollectionEntry('non-existent-collection', itemsEntry1.path, {}),
+    ).rejects.toThrow(CollectionNotFoundError);
+  });
+
+  it('throws if the entry does not exist', () => {
+    expect(() =>
+      updateCollectionEntry(itemsCollection.path, 'non-existent-entry', {}),
+    ).rejects.toThrow(CollectionEntryNotFoundError);
+  });
+
+  it('throws if the entry is not found', async () => {
+    await expect(
+      updateCollectionEntry(itemsCollection.path, 'non-existent-entry', update),
+    ).rejects.toThrow(CollectionEntryNotFoundError);
+  });
+
+  it('merges updates into the entry', async () => {
+    const updated = await updateCollectionEntry(
+      itemsCollection.path,
+      itemsEntry1.path,
+      update,
+    );
+
+    expect(updated).toEqual(updatedEntry);
+  });
+
+  it('returns the updated entry', async () => {
+    const updated = await updateCollectionEntry(
+      itemsCollection.path,
+      itemsEntry1.path,
+      update,
+    );
+
+    expect(updated).toEqual(updatedEntry);
+  });
+
+  it('updates the lastModified timestamp in metadata', async () => {
+    const updated = await updateCollectionEntry(
+      itemsCollection.path,
+      itemsEntry1.path,
+      update,
+    );
+
+    expect(updated.metadata.lastModified).not.toEqual(
+      itemsEntry1.metadata.lastModified,
+    );
+  });
+
+  it('updates the entry in the store', async () => {
+    await updateCollectionEntry(itemsCollection.path, itemsEntry1.path, update);
+
+    expect(CollectionEntriesStore.get(itemsEntry1.path)).toEqual(updatedEntry);
+  });
+
+  it('dispatches a entry update event', async () =>
+    new Promise<void>((done) => {
+      Events.addListener('collections:entry:update', 'test', (payload) => {
+        // Payload data should be updated entry
+        expect(payload.data).toEqual(updatedEntry);
+        done();
+      });
+
+      updateCollectionEntry(itemsCollection.path, itemsEntry1.path, update);
+    }));
+});
---
+++ packages/collections/src/updateCollectionEntry/updateCollectionEntry.ts
@@ -0,0 +1,56 @@
+import { Events } from '@minddrop/events';
+import { updateCollectionEntry as updateInStore } from '../CollectionEntriesStore';
+import { getCollection } from '../getCollection';
+import { getCollectionEntry } from '../getCollectionEntry';
+import { getCollectionTypeConfig } from '../getCollectionTypeConfig';
+import { CollectionEntry } from '../types';
+
+/**
+ * Updates a collection entry.
+ *
+ * @param collectionPath - The path of the collection.
+ * @param entryPath - The path of the entry.
+ * @param updates - The updates to apply to the entry.
+ * @returns The updated collection entry.
+ *
+ * @throws {CollectionNotFoundError} If the collection does not exist.
+ * @throws {CollectionTypeConfigNotFoundError} If the collection type config does not exist.
+ * @throws {CollectionEntryNotFoundError} If the entry does not exist.
+ *
+ * @dispatches collections:entry:update
+ */
+export async function updateCollectionEntry(
+  collectionPath: string,
+  entryPath: string,
+  updates: Partial<CollectionEntry>,
+): Promise<CollectionEntry> {
+  // Get the collection
+  const collection = getCollection(collectionPath, true);
+  // Get the collection type config
+  const config = getCollectionTypeConfig(collection.type);
+  // Get the entry
+  const entry = getCollectionEntry(entryPath, true);
+
+  // Merge updates into the existing properties
+  let updatedEntry = {
+    ...entry,
+    ...updates,
+    metadata: {
+      ...entry.metadata,
+      lastModified: new Date(),
+    },
+  };
+
+  // Update the entry files using the collection type config.
+  // The config may further process the entry before saving it,
+  // so we use the returned entry as the updated entry.
+  updatedEntry = await config.updateEntry(entry.path, updatedEntry);
+
+  // Update the entry in the store
+  updateInStore(entry.path, updatedEntry);
+
+  // Dispatch a entry updated event
+  Events.dispatch('collections:entry:update', updatedEntry);
+
+  return updatedEntry;
+}
