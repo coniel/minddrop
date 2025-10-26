3f967d69d38f7a64653b46beca39889b6ad5e3ca
Oscar Coniel <oscar@coniel.com>
2025-10-27 01:45:05 +0200

    templates: fix prompt

--- _templates/ui-component/new/prompt.js
+++ _templates/ui-component/new/prompt.js
@@ -2,15 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 const uiPackagesPath = path.resolve(__dirname, '../../../ui');
-const uiPackages = fs.readdirSync(uiPackagesPath)
+const uiPackages = fs.readdirSync(uiPackagesPath);
 
 function getChoices(componentName) {
-
   const packagePrefix = componentName.replace(/([A-Z])/g, ' $1').trim().split(' ')[0].toLowerCase();
-  const matchedPackage = uiPackages.find(pkg => pkg.startsWith(packagePrefix))
+  const matchedPackage = uiPackages.find(pkg => pkg.startsWith(packagePrefix));
   const otherPackages = uiPackages.filter(pkg => [matchedPackage, 'primitives', 'ui-sandbox'].indexOf(pkg) === -1);
 
-  return [matchedPackage, 'primitives', ...otherPackages];
+  return [matchedPackage, 'primitives', ...otherPackages].filter(package => package);
 }
 
 module.exports = {
