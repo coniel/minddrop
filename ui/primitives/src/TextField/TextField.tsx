c7e75203b23fd6489cfdb43ed92610032e00e260
Oscar Coniel <oscar@coniel.com>
2025-10-29 12:25:22 +0200

    ui-primitives: add TextField prop labels

--- ui/primitives/src/TextField/TextField.tsx
+++ ui/primitives/src/TextField/TextField.tsx
@@ -3,21 +3,52 @@ import { Input } from '@base-ui-components/react/input';
 import { mapPropsToClasses } from '../utils';
 import './TextField.css';
 
-export interface TextFieldProps
-  extends Omit<Field.Root.Props, 'onChange'>,
-    Pick<Input.Props, 'defaultValue' | 'onValueChange' | 'placeholder'> {
+export interface TextFieldProps extends Omit<Field.Root.Props, 'onChange'> {
   /**
    * Additional class name(s) to apply to the field's root element.
    */
   className?: string;
 
+  /**
+   * The label for the text field.
+   */
   label?: string;
 
+  /**
+   * The error message to display below the input.
+   */
   error?: string;
 
+  /**
+   * The description to display below the input.
+   */
   description?: string;
 
+  /**
+   * The change event handler.
+   */
   onChange?: Input.Props['onChange'];
+
+  /**
+   * Callback fired when the input's value changes.
+   * Receives the new value as its only argument.
+   */
+  onValueChange?: Input.Props['onValueChange'];
+
+  /**
+   * The input's placeholder text.
+   */
+  placeholder?: Input.Props['placeholder'];
+
+  /**
+   * The input's default value.
+   */
+  defaultValue?: Input.Props['defaultValue'];
+
+  /**
+   * The input's autoComplete attribute.
+   */
+  autoComplete?: Input.Props['autoComplete'];
 }
 
 export const TextField: React.FC<TextFieldProps> = ({
