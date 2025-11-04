718089e0dd950a518a50a049b75352dd123b2ca0
Oscar Coniel <oscar@coniel.com>
2025-11-04 15:12:30 +0200

    test-utils: add pickEmojiIcon

---
+++ packages/test-utils/src/utils/pickEmojiIcon.ts
@@ -0,0 +1,25 @@
+import { screen, userEvent, waitFor } from '../testing-library-react';
+
+export const emojiIcon = '😀';
+export const emojiIconString = `emoji:${emojiIcon}:1`;
+
+export async function pickEmojiIcon(label: string) {
+  const user = userEvent.setup();
+
+  // Open icon picker
+  await user.click(screen.getByLabelText(label));
+
+  // Wait for icon picker to open
+  await waitFor(() => {
+    screen.getByText('emojiPicker.label');
+  });
+
+  // Select an emoji
+  await user.click(screen.getByText('emojiPicker.label'));
+
+  await waitFor(() => {
+    screen.getByText('Smileys & Emotion');
+  });
+
+  await user.click(screen.getByText(emojiIcon));
+}
