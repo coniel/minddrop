818727576e89e2e43f7ee07223893392c5e76ef5
Oscar Coniel <oscar@coniel.com>
2025-11-14 18:02:52 +0200

    features/app-sidebar: add databases menu to sidebar

--- features/app-sidebar/src/AppSidebar/AppSidebar.tsx
+++ features/app-sidebar/src/AppSidebar/AppSidebar.tsx
@@ -1,63 +1,10 @@
-import { Events } from '@minddrop/events';
-import { ItemTypes } from '@minddrop/item-types';
-import {
-  Button,
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-  MenuItem,
-  MenuLabel,
-} from '@minddrop/ui-primitives';
+import { DatabasesSidebarMenu } from '@minddrop/feature-databases';
 import { Sidebar, SidebarProps } from '../Sidebar';
 
 export const AppSidebar: React.FC<SidebarProps> = ({ ...other }) => {
-  const itemTypes = ItemTypes.useAll();
-
-  function handleAddItemType(event: React.MouseEvent) {
-    event.stopPropagation();
-    Events.dispatch('item-type:new-item-type-dialog:open');
-  }
-
-  function handleOpenItemTypesView(type: string) {
-    Events.dispatch('item-type:view:open', { type });
-  }
-
   return (
     <Sidebar {...other}>
-      <div>
-        <Collapsible defaultOpen>
-          <CollapsibleTrigger
-            nativeButton={false}
-            render={
-              <MenuLabel
-                button
-                label="items.labels.items"
-                actions={
-                  <Button
-                    size="small"
-                    label="itemTypes.actions.add"
-                    variant="text"
-                    startIcon="plus"
-                    onClick={handleAddItemType}
-                  />
-                }
-              />
-            }
-          />
-          <CollapsibleContent>
-            {itemTypes.map((itemType) => (
-              <MenuItem
-                muted
-                contentIcon={itemType.icon || 'content-icon:shapes:inherit'}
-                key={itemType.nameSingular}
-                onClick={() => handleOpenItemTypesView(itemType.nameSingular)}
-              >
-                {itemType.namePlural}
-              </MenuItem>
-            ))}
-          </CollapsibleContent>
-        </Collapsible>
-      </div>
+      <DatabasesSidebarMenu />
     </Sidebar>
   );
 };
