2810d9e005ff4a66632f4808b82e1dde1f7a5045
Oscar Coniel <oscar@coniel.com>
2026-02-07 00:28:05 +0200

    ui/primitives: fix DropdownMenu children type

--- ui/primitives/src/DropdownMenu/DropdownMenuTrigger.tsx
+++ ui/primitives/src/DropdownMenu/DropdownMenuTrigger.tsx
@@ -1,13 +1,11 @@
 import { Menu as DropdownMenuPrimitives } from '@base-ui-components/react/menu';
 import { FC } from 'react';
 
-export type DropdownMenuTriggerProps = DropdownMenuPrimitives.Trigger.Props;
+export type DropdownMenuTriggerProps = DropdownMenuPrimitives.Trigger.Props & {
+  children: DropdownMenuPrimitives.Trigger.Props['render'];
+};
 
 export const DropdownMenuTrigger: FC<DropdownMenuTriggerProps> = ({
   children,
   ...other
-}) => (
-  <DropdownMenuPrimitives.Trigger {...other}>
-    {children}
-  </DropdownMenuPrimitives.Trigger>
-);
+}) => <DropdownMenuPrimitives.Trigger {...other} render={children} />;
