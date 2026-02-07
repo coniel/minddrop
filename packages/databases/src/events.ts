e792301ba14de9522a596f142b78118f10b25270
Oscar Coniel <oscar@coniel.com>
2026-02-07 02:47:49 +0200

    databases: add event data types

--- packages/databases/src/events.ts
+++ packages/databases/src/events.ts
@@ -1,9 +1,16 @@
-import { DatabaseEntry } from './types';
+import { Database, DatabaseEntry } from './types';
 
 // Database events
 export const DatabaseCreatedEvent = 'databases:database:created';
 export const DatabaseUpdatedEvent = 'databases:database:updated';
 export const DatabaseDeletedEvent = 'databases:database:deleted';
+
+export type DatabaseCreatedEventData = Database;
+export interface DatabaseUpdatedEventData {
+  original: Database;
+  updated: Database;
+}
+export type DatabaseDeletedEventData = Database;
 
 // Entry events
 export const DatabaseEntryCreatedEvent = 'databases:entry:created';
@@ -12,8 +19,8 @@ export const DatabaseEntryDeletedEvent = 'databases:entry:deleted';
 export const DatabaseEntryRenamedEvent = 'databases:entry:renamed';
 
 export type DatabaseEntryCreatedEventData = DatabaseEntry;
-
 export interface DatabaseEntryUpdatedEventData {
   original: DatabaseEntry;
   updated: DatabaseEntry;
 }
+export type DatabaseEntryDeletedEventData = DatabaseEntry;
