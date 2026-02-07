75f0c3cb5f936e842f2b9b40cff6e8937b2e6056
Oscar Coniel <oscar@coniel.com>
2026-02-08 00:23:09 +0200

    views: add tests for initializeViews

--- packages/views/src/initializeViews/initializeViews.test.ts
+++ packages/views/src/initializeViews/initializeViews.test.ts
@@ -1,13 +1,25 @@
-import { describe, beforeEach, afterEach, it, expect } from 'vitest';
-import { setup, cleanup } from '../test-utils';
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+import { ViewsStore } from '../ViewsStore';
+import { MockFs, cleanup, setup, views, viewsBasePath } from '../test-utils';
 import { initializeViews } from './initializeViews';
 
 describe('initializeViews', () => {
-  beforeEach(setup);
+  beforeEach(() => setup({ loadViews: false }));
 
   afterEach(cleanup);
 
-  it('does something useful', () => {
-    expect(1).toBe(1);
+  it('loads views from the views directory', async () => {
+    await initializeViews();
+
+    expect(ViewsStore.getAll()).toEqual(views);
+  });
+
+  it('creates the views directory if it does not exist', async () => {
+    // Remove the views directory
+    MockFs.removeFile(viewsBasePath);
+
+    await initializeViews();
+
+    expect(MockFs.exists(viewsBasePath)).toBe(true);
   });
 });
