0feb2e60cf00ec256cec2208d1bfce07be02b5fd
Oscar Coniel <oscar@coniel.com>
2026-02-14 18:28:39 +0200

    features/databases: add basic file drop support

--- features/databases/src/DatabaseView/DatabaseView.tsx
+++ features/databases/src/DatabaseView/DatabaseView.tsx
@@ -1,4 +1,5 @@
-import { DatabaseEntries, Databases } from '@minddrop/databases';
+import { DataTypes, DatabaseEntries, Databases } from '@minddrop/databases';
+import { Fs } from '@minddrop/file-system';
 import {
   ContentIcon,
   Heading,
@@ -23,9 +24,51 @@ export const DatabaseView: React.FC<DatabaseViewProps> = ({ databaseId }) => {
   const entries = DatabaseEntries.useAll(databaseId);
   const [configurationPanelOpen, toggleConfigurationPanel] = useToggle(false);
 
-  function handleClickNewEntry() {
-    DatabaseEntries.create(databaseId);
+  async function handleClickNewEntry() {
+    if (!database) {
+      return;
+    }
+
+    const dataType = DataTypes.get(database.dataType);
+
+    if (dataType.file) {
+      const result = await Fs.openFilePicker({
+        accept: dataType.fileExtensions,
+        multiple: true,
+      });
+
+      if (result) {
+        result.forEach(async (filePath) => {
+          DatabaseEntries.createFromFilePath(database.id, filePath);
+        });
+      }
+    } else {
+      DatabaseEntries.create(database.id);
+    }
   }
+
+  function handleDrop(event: React.DragEvent<HTMLDivElement>) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    if (!database) {
+      return;
+    }
+
+    const { validFiles } = Databases.filterFiles(
+      database.id,
+      Array.from(event.dataTransfer.files),
+    );
+
+    validFiles.forEach(async (file) => {
+      DatabaseEntries.createFromFile(database.id, file);
+    });
+  }
+
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+  };
 
   if (!database) {
     return <div className="database-view">Database not found.</div>;
@@ -33,7 +76,11 @@ export const DatabaseView: React.FC<DatabaseViewProps> = ({ databaseId }) => {
 
   return (
     <div className="database-view">
-      <Panel className="database">
+      <Panel
+        className="database"
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+      >
         <div className="header">
           <div className="title">
             <ContentIcon icon={database.icon} />
