df54c87aa9922836d6923665dba9b9229e5ded6e
Oscar Coniel <oscar@coniel.com>
2026-02-19 02:52:25 +0200

    properties: remove 'storage' from ImagePropertySchema

--- packages/properties/src/schemas/ImagePropertySchema.ts
+++ packages/properties/src/schemas/ImagePropertySchema.ts
@@ -2,12 +2,10 @@ import { PropertySchemaBase } from '../types';
 
 export interface ImagePropertySchema extends PropertySchemaBase {
   type: 'image';
-  storage: 'asset' | 'attachment' | 'item';
 }
 export const ImagePropertySchema: ImagePropertySchema = {
   type: 'image',
   icon: 'content-icon:image:default',
   name: 'properties.image.name',
   description: 'properties.image.description',
-  storage: 'asset',
 };
