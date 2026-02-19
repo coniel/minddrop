89be4bd271a5c83efb3997f4447578b4ba7fd266
Oscar Coniel <oscar@coniel.com>
2026-02-19 17:53:08 +0200

    properties: fix FileExtensionToProperty

--- packages/properties/src/constants.ts
+++ packages/properties/src/constants.ts
@@ -1,8 +1,30 @@
-import { PropertyType } from './types';
+import { FileBasedPropertyType } from './types';
 
-export const FileBasedPropertyTypes: PropertyType[] = ['image', 'file'];
+export const FileBasedPropertyTypes: FileBasedPropertyType[] = [
+  'image',
+  'file',
+];
 
-export const FilePropertySupportedFileExtensions = {
+/**
+ * A [property type]: [file extensions] map of which file extensions
+ * are supported by a given property type.
+ */
+export const FilePropertySupportedFileExtensions: Record<
+  FileBasedPropertyType,
+  string[]
+> = {
   image: ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'svg', 'webp', 'ico'],
   file: [],
 };
+
+/**
+ * A [file extension]: [property type] map of which property type
+ * supports a given file extension.
+ */
+export const FileExtensionToPropertyType = Object.entries(
+  FilePropertySupportedFileExtensions,
+).reduce(
+  (acc, [type, exts]) =>
+    Object.assign(acc, Object.fromEntries(exts.map((e) => [e, type]))),
+  {} as Record<string, FileBasedPropertyType>,
+);
