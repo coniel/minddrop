bc9fb22237fbcbbd7a70fe9078456a56ef850152
Oscar Coniel <oscar@coniel.com>
2026-02-25 20:07:25 +0200

    templates: add dev package option

--- _templates/package/new/prompt.js
+++ _templates/package/new/prompt.js
@@ -3,6 +3,6 @@ module.exports = [
     type: 'select',
     name: 'module',
     message: 'Select a module.',
-    choices: ['packages', 'extensions', 'apps', 'ui', 'dev'],
+    choices: ['packages', 'extensions', 'apps', 'ui', 'views', 'dev'],
   },
 ];
