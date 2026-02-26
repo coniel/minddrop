80863f7dcb2d2cf6b5881ae81df410a05a107927
Oscar Coniel <oscar@coniel.com>
2026-02-26 17:57:12 +0200

    features/app-sidebar: add toolbar

--- features/app-sidebar/src/AppSidebar.tsx
+++ features/app-sidebar/src/AppSidebar.tsx
@@ -1,9 +1,24 @@
+import { Events } from '@minddrop/events';
 import { DatabasesSidebarMenu } from '@minddrop/feature-databases';
+import { OpenDesignStudioEvent } from '@minddrop/feature-design-studio';
+import { Toolbar, ToolbarIconButton } from '@minddrop/ui-primitives';
 import { Sidebar, SidebarProps } from './Sidebar';
 
 export const AppSidebar: React.FC<SidebarProps> = ({ ...other }) => {
+  function handleOpenDesignStudio() {
+    Events.dispatch(OpenDesignStudioEvent, {});
+  }
+
   return (
     <Sidebar {...other}>
+      <Toolbar>
+        <ToolbarIconButton
+          icon="palette"
+          label="designStudio.open"
+          tooltipTitle="designStudio.open"
+          onClick={handleOpenDesignStudio}
+        />
+      </Toolbar>
       <DatabasesSidebarMenu />
     </Sidebar>
   );
