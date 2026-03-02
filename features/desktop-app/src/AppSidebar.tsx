cf8f1421ab1d8afdd051db82a9dff9c66f2cebed
Oscar Coniel <oscar@coniel.com>
2026-03-02 22:50:12 +0200

    features/desktop-app: add theme variant picker to sidebar bottom toolbar

--- features/desktop-app/src/AppSidebar.tsx
+++ features/desktop-app/src/AppSidebar.tsx
@@ -2,6 +2,7 @@ import { Events } from '@minddrop/events';
 import { DatabasesSidebarMenu } from '@minddrop/feature-databases';
 import { OpenDesignStudioEvent } from '@minddrop/feature-designs';
 import { Toolbar, ToolbarIconButton } from '@minddrop/ui-primitives';
+import { ThemeVariantPicker } from '@minddrop/ui-theme';
 import { Sidebar, SidebarProps } from './Sidebar';
 
 export const AppSidebar: React.FC<SidebarProps> = ({ ...other }) => {
@@ -20,6 +21,9 @@ export const AppSidebar: React.FC<SidebarProps> = ({ ...other }) => {
         />
       </Toolbar>
       <DatabasesSidebarMenu />
+      <Toolbar className="app-sidebar-bottom-toolbar">
+        <ThemeVariantPicker />
+      </Toolbar>
     </Sidebar>
   );
 };
