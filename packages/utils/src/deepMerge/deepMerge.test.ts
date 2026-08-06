91a8508fcce651b1b4f339b49b51158939608fd7
Oscar Coniel <oscar@coniel.com>
2026-08-06 23:08:58 +0300

    utils: type the deepMerge test's merged shape

--- packages/utils/src/deepMerge/deepMerge.test.ts
+++ packages/utils/src/deepMerge/deepMerge.test.ts
@@ -6,7 +6,7 @@ describe('deepMerge', () => {
     const target = { a: 1, b: 2, c: { d: 3, e: 4 } };
     const source = { a: 5, d: 3, c: { e: 9, f: 10 } };
 
-    const merged = deepMerge<any>(target, source);
+    const merged = deepMerge<Record<string, unknown>>(target, source);
 
     expect(merged).toEqual({
       a: 5,
