1a7ce28d5a9ec3120f260be0e45a7f84dc2ea37f
Oscar Coniel <oscar@coniel.com>
2026-08-07 20:12:39 +0300

    apps/desktop: delete legacy desktop app

--- apps/desktop/eslint.config.js
+++
@@ -1,9 +0,0 @@
-import { config } from '@minddrop/eslint-config/react-internal';
-
-/** @type {import("eslint").Linter.Config} */
-export default [
-  ...config,
-  {
-    ignores: ['src-tauri/gen/**', 'src-tauri/target/**'],
-  },
-];
