5bac95e4fbfc347954e9ed7bf1d7ff7f61e204bd
Oscar Coniel <oscar@coniel.com>
2026-08-12 10:09:28 +0300

    views: add optional view title and icon

--- packages/views/src/types/View.types.ts
+++ packages/views/src/types/View.types.ts
@@ -1,3 +1,6 @@
+import { TranslationKey } from '@minddrop/i18n';
+import { UiIconName } from '@minddrop/ui-icons';
+
 export interface View {
   /**
    * A unique identifier for the view, matching the `view` field of an
@@ -10,4 +13,18 @@ export interface View {
    */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any -- must accept components with arbitrary props
   component: React.ComponentType<any>;
+
+  /**
+   * The i18n key of the view's label, used by views with a fixed
+   * label (e.g. search). Views labelled by the entity they show
+   * (e.g. a database) leave it unset and provide a title when opened.
+   */
+  title?: TranslationKey;
+
+  /**
+   * The view's icon, used by views with a fixed icon. Views iconed by
+   * the entity they show leave it unset and provide the entity's
+   * content icon when opened.
+   */
+  icon?: UiIconName;
 }
