40defdfc3de0cfe20bab013ea7391fd159bb15f8
Oscar Coniel <oscar@coniel.com>
2026-08-12 10:11:21 +0300

    desktop-app: register the search view as the default view

--- features/desktop-app/src/EmptyView.tsx
+++
@@ -1,5 +0,0 @@
-/**
- * Blank view, registered for `DefaultViewName` and used when there is
- * nothing to show (e.g. a new tab or after exiting the design studio).
- */
-export const EmptyView: React.FC = () => null;
--- features/desktop-app/src/initializeDesktopApp/registerViews.ts
+++ features/desktop-app/src/initializeDesktopApp/registerViews.ts
@@ -1,28 +1,21 @@
-import { DefaultViewName } from '@minddrop/events';
 import { registerCollectionViews } from '@minddrop/feature-collections';
 import { registerDataViewViews } from '@minddrop/feature-data-views';
 import { registerDatabaseViews } from '@minddrop/feature-databases';
 import { registerDesignViews } from '@minddrop/feature-designs';
 import { registerQueryViews } from '@minddrop/feature-queries';
+import { registerSearchViews } from '@minddrop/feature-search';
 import { registerSpaceViews } from '@minddrop/feature-spaces';
-import { Views } from '@minddrop/views';
-import { EmptyView } from '../EmptyView';
 
 /**
  * Registers the views that can be opened by id via `OpenViewEvent`.
  */
 export function registerViews(): void {
-  // Register the blank default view
-  Views.register({
-    type: DefaultViewName,
-    component: EmptyView,
-  });
-
   // Register feature-provided views
   registerCollectionViews();
   registerDataViewViews();
   registerQueryViews();
   registerDatabaseViews();
   registerDesignViews();
+  registerSearchViews();
   registerSpaceViews();
 }
