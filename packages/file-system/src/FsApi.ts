79f9938fb81ea686b74a206a32f6d85aeb2996cd
Oscar Coniel <oscar@coniel.com>
2026-09-01 12:48:48 +0300

    file-system: remove unused pathSlice util

--- packages/file-system/src/FsApi.ts
+++ packages/file-system/src/FsApi.ts
@@ -1,7 +1,6 @@
 export { concatPath } from './concatPath';
 export { fileNameFromPath } from './fileNameFromPath';
 export { parentDirPath } from './parentDirPath';
-export { getPathSlice as pathSlice } from './getPathSlice';
 export { getFileExtension as getExtension } from './getFileExtension';
 export { removeFileExtension as removeExtension } from './removeFileExtension';
 export { getFileExtension } from './getFileExtension';
