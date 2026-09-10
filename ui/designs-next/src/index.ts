0effb4d68b617f1905c00a2ee536ff0e89f137a3
Oscar Coniel <oscar@coniel.com>
2026-09-10 23:52:51 +0300

    ui-designs-next: add font letter band measurement

--- ui/designs-next/src/index.ts
+++ ui/designs-next/src/index.ts
@@ -9,4 +9,6 @@ export * from './DesignPropertyPicker';
 export * from './DesignRenderer';
 export * from './TextContentControls';
 export * from './useElementValue';
+export * from './utils/resolveFontLetterBand';
 export * from './utils/resolveTextSettingsClass';
+export * from './utils/resolveThemeFontStack';
--- ui/designs-next/src/utils/index.ts
+++ ui/designs-next/src/utils/index.ts
@@ -1,5 +1,7 @@
 export * from './formatSnapUnits';
 export * from './resolveElementClass';
+export * from './resolveFontLetterBand';
 export * from './resolveSizeLabelFontSize';
 export * from './resolveTextSettingsClass';
+export * from './resolveThemeFontStack';
 export * from './resolveVerticalStyles';
